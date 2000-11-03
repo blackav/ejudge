c187e739e6cf1b3313cf370967184de8937706de
cher <blackav@gmail.com>
2000-11-03 16:37:00 +0000

    checked in with -k by cher at 2001/01/26 18:00:56

---
+++ fileutl.h
@@ -0,0 +1,57 @@
+/* -*- c -*- */
+/* $Id$ */
+#ifndef __FILEUTL_H__
+#define __FILEUTL_H__
+
+/* Copyright (C) 2000 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ */
+
+#include <stdlib.h>
+
+int   scan_dir(char const *dir, char *result);
+
+/* operation flags */
+enum { SAFE = 1, REMOVE = 2, CONVERT = 4, PIPE = 8 };
+
+int generic_read_file(char **buf, size_t maxsz, size_t *readsz, int flags,
+                      char const *dir, char const *name, char const *sfx);
+int generic_write_file(char const *buf, size_t size, int flags,
+                       char const *dir, char const *name, char const *sfx);
+int generic_copy_file(int sflags,
+                      char const *sdir, char const *sname, char const *ssfx,
+                      int dflags,
+                      char const *ddir, char const *dname, char const *dsfx);
+
+int   clear_directory(char const *path);
+int   make_dir(char const *, int);
+int   make_all_dir(char const *, int);
+
+void  get_uniq_prefix(char *);
+
+int   make_executable(char const *);
+int   make_writable(char const *);
+
+int   check_executable(char const *);
+int   check_readable_dir(char const *);
+int   check_writable_dir(char const *);
+enum { SPOOL_IN, SPOOL_OUT };
+int   check_writable_spool(char const *, int);
+
+int   relaxed_remove(char const *, char const *);
+
+#endif /* __FILEUTL_H__ */
