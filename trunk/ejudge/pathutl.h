7989623161e5449e0758fe0dfb32a24c2c5c0c84
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2001-03-25 14:23:56 +0000

    copyright notice updated

--- trunk/ejudge/pathutl.h
+++ trunk/ejudge/pathutl.h
@@ -3,7 +3,7 @@
 #ifndef __PATHUTL_H__
 #define __PATHUTL_H__
 
-/* Copyright (C) 2000 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2000,2001 Alexander Chernov <cher@ispras.ru> */
 
 /*
  *  This program is free software; you can redistribute it and/or
