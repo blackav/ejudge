9aa4c0a72806961c4f3cd4e18331bb100ad77113
cher <blackav@gmail.com>
2002-03-22 19:43:15 +0000

    copyright notice is updated; `logger.h', `xalloc.h', `osdeps.h' are included from reuse library.

--- clarlog.c
+++ clarlog.c
@@ -1,7 +1,7 @@
 /* -*- c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2000,2001 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2000-2002 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -25,9 +25,10 @@
 
 #include "unix/unix_fileutl.h"
 #include "pathutl.h"
-#include "logger.h"
-#include "xalloc.h"
-#include "osdeps.h"
+
+#include <reuse/logger.h>
+#include <reuse/xalloc.h>
+#include <reuse/osdeps.h>
 
 #include <unistd.h>
 #include <errno.h>
