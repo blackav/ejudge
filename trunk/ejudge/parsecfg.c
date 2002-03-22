8f772d7e5052bbbe3cd7efcbac8210144ee983fe
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2002-03-22 20:39:15 +0000

    copyright notice is updated; `xalloc.h' is included from reuse library include directory.

--- trunk/ejudge/parsecfg.c
+++ trunk/ejudge/parsecfg.c
@@ -1,7 +1,7 @@
 /* -*- c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2000,2001 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2000-2002 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -20,8 +20,9 @@
  */
 
 #include "parsecfg.h"
-#include "xalloc.h"
 #include "pathutl.h"
+
+#include <reuse/xalloc.h>
 
 #include <stdio.h>
 #include <ctype.h>
