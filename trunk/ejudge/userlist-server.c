26047829d213a1b0090b8654a494675fa448080c
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2002-10-08 04:55:58 +0000

    in progress...

--- trunk/ejudge/userlist-server.c
+++ trunk/ejudge/userlist-server.c
@@ -22,6 +22,7 @@
 #include "userlist_cfg.h"
 #include "pathutl.h"
 
+#include <reuse/logger.h>
 #include <reuse/osdeps.h>
 #include <reuse/xalloc.h>
 
@@ -33,6 +34,7 @@
 #include <sys/un.h>
 #include <unistd.h>
 #include <errno.h>
+#include <signal.h>
 
 #if CONF_HAS_LIBINTL - 0 == 1
 #include <libintl.h>
@@ -41,21 +43,30 @@
 #define _(x) x
 #endif
 
+#define CLIENT_TIMEOUT 600
+
 struct client_state
 {
   struct client_state *next;
   struct client_state *prev;
 
+  int id;
   int fd;
   int write_len;
   int written;
   unsigned char *write_buf;
   int read_state;
+  int expected_len;
   int read_len;
   unsigned char *read_buf;
 
   time_t last_time;
   int state;
+
+  // some peer information
+  int peer_pid;
+  int peer_uid;
+  int peer_gid;
 };
 
 static struct userlist_cfg *config;
@@ -63,9 +74,37 @@ static int listen_socket = -1;
 static char *socket_name;
 static struct client_state *first_client;
 static struct client_state *last_client;
+static int serial_id = 1;
+
+static time_t last_flush;
+static unsigned long flush_interval;
 
 static void
 disconnect_client(struct client_state *p)
+{
+  ASSERT(p);
+
+  close(p->fd);
+  if (p->write_buf) xfree(p->write_buf);
+  if (p->read_buf) xfree(p->read_buf);
+
+  if (p->prev) {
+    p->prev->next = p->next;
+  } else {
+    first_client = p->next;
+  }
+  if (p->next) {
+    p->next->prev = p->prev;
+  } else {
+    last_client = p->prev;
+  }
+
+  memset(p, 0, sizeof(*p));
+  xfree(p);
+}
+
+static void
+process_packet(struct client_state *p, int len, unsigned char *pack)
 {
 }
 
@@ -79,6 +118,8 @@ do_work(void)
   fd_set rset, wset;
   struct client_state *p;
   time_t cur_time;
+
+  signal(SIGPIPE, SIG_IGN);
 
   if ((listen_socket = socket(PF_UNIX, SOCK_STREAM, 0)) < 0) {
     err("socket() failed :%s", os_ErrorMsg());
@@ -95,12 +136,14 @@ do_work(void)
   }
   socket_name = config->socket_path;
 
+  /*
   val = 1;
   if (setsockopt(listen_socket, SOL_SOCKET, SO_PASSCRED,
                  &val, sizeof(val)) < 0) {
     err("setsockopt() failed: %s", os_ErrorMsg());
     return 1;
   }
+  */
 
   if (listen(listen_socket, 5) < 0) {
     err("listen() failed: %s", os_ErrorMsg());
@@ -108,6 +151,21 @@ do_work(void)
   }
 
   while (1) {
+    // flush database
+    cur_time = time(0);
+    if (cur_time <= last_flush + flush_interval) {
+      // FIXME: flush database
+    }
+
+    // disconnect idle clients
+    while (1) {
+      for (p = first_client; p; p = p->next)
+        if (p->last_time + CLIENT_TIMEOUT < cur_time) break;
+      if (!p) break;
+      info("%d: timeout, client disconnected", p->id);
+      disconnect_client(p);
+    }
+
     FD_ZERO(&rset);
     FD_ZERO(&wset);
     max_fd = -1;
@@ -135,10 +193,7 @@ do_work(void)
 
     cur_time = time(0);
 
-    if (!val) {
-      // FIXME: disconnect idle clients
-      continue;
-    }
+    if (!val) continue;
 
     if (FD_ISSET(listen_socket, &rset)) {
       int new_fd;
@@ -161,10 +216,182 @@ do_work(void)
         }
         q->fd = new_fd;
         q->last_time = cur_time;
+        q->id = serial_id++;
+
+        val = 1;
+        if (setsockopt(new_fd, SOL_SOCKET, SO_PASSCRED,
+                       &val, sizeof(val)) < 0) {
+          err("%d: setsockopt() failed: %s", q->id, os_ErrorMsg());
+          disconnect_client(q);
+        } else {
+          info("%d: connection accepted", q->id);
+        }
       }
     }
 
+    // check write bit and write
+  restart_write_scan:
     for (p = first_client; p; p = p->next) {
+      if (FD_ISSET(p->fd, &wset)) {
+        int w, l;
+
+        l = p->write_len - p->written;
+        w = write(p->fd, &p->write_buf[p->written], l);
+        if (w <= 0) {
+          err("%d: write() failed: %s", p->id, os_ErrorMsg());
+          disconnect_client(p);
+          goto restart_write_scan; /* UGLY :-( */
+        }
+        p->written += w;
+        if (p->write_len == p->written) {
+          p->written = 0;
+          p->write_len = 0;
+          xfree(p->write_buf);
+          p->write_buf = 0;
+        }
+      }
+    }
+
+    // check read bit and read
+    while (1) {
+      int l, r;
+
+      for (p = first_client; p; p = p->next)
+        if (FD_ISSET(p->fd, &rset)) break;
+      if (!p) break;
+
+      if (p->state == 0) {
+        struct msghdr msg;
+        unsigned char msgbuf[512];
+        struct cmsghdr *pmsg;
+        struct ucred *pcred;
+        struct iovec recv_vec[1];
+        int val;
+
+        // we expect 4 zero bytes and credentials
+        memset(&msg, 0, sizeof(msg));
+        msg.msg_flags = 0; //MSG_DONTWAIT;
+        msg.msg_control = msgbuf;
+        msg.msg_controllen = sizeof(msgbuf);
+        recv_vec[0].iov_base = &val;
+        recv_vec[0].iov_len = 4;
+        msg.msg_iov = recv_vec;
+        msg.msg_iovlen = 1;
+        val = -1;
+        r = recvmsg(p->fd, &msg, 0);
+        if (r < 0) {
+          err("%d: recvmsg failed: %s", p->id, os_ErrorMsg());
+          disconnect_client(p);
+          continue;
+        }
+        if (r != 4) {
+          err("%d: read %d bytes instead of 4", p->id, r);
+          disconnect_client(p);
+          continue;
+        }
+        if (val != 0) {
+          err("%d: expected 4 zero bytes", p->id);
+          disconnect_client(p);
+          continue;
+        }
+        if ((msg.msg_flags & MSG_CTRUNC)) {
+          err("%d: protocol error: control buffer too small", p->id);
+          disconnect_client(p);
+          continue;
+        }
+
+        pmsg = CMSG_FIRSTHDR(&msg);
+        if (!pmsg) {
+          err("%d: empty control data", p->id);
+          disconnect_client(p);
+          continue;
+        }
+        /* cmsg_len, cmsg_level, cmsg_type */
+        if (pmsg->cmsg_level != SOL_SOCKET
+            || pmsg->cmsg_type != SCM_CREDENTIALS
+            || pmsg->cmsg_len != CMSG_LEN(sizeof(*pcred))) {
+          err("%d: protocol error: unexpected control data", p->id);
+          disconnect_client(p);
+          continue;
+        }
+        pcred = (struct ucred*) CMSG_DATA(pmsg);
+        p->peer_pid = pcred->pid;
+        p->peer_uid = pcred->uid;
+        p->peer_gid = pcred->gid;
+        if (CMSG_NXTHDR(&msg, pmsg)) {
+          err("%d: protocol error: unexpected control data", p->id);
+          disconnect_client(p);
+          continue;
+        }
+
+        info("%d: received peer information: %d, %d, %d", p->id,
+             p->peer_pid, p->peer_uid, p->peer_gid);
+
+        p->state = 1;
+        continue;
+      }
+
+      if (p->read_state < 4) {
+        unsigned char rbuf[4];
+
+        memcpy(rbuf, &p->expected_len, 4);
+        l = 4 - p->read_state;
+        r = read(p->fd, &rbuf[p->read_state], l);
+        if (!p->read_state && !r) {
+          info("%d: client closed connection", p->id);
+          disconnect_client(p);
+          continue;
+        }
+        if (!r) {
+          err("%d: unexpected EOF from client", p->id);
+          disconnect_client(p);
+          continue;
+        }
+        if (r < 0) {
+          err("%d: read() failed: %s", p->id, os_ErrorMsg());
+          disconnect_client(p);
+          continue;
+        }
+
+        p->read_state += l;
+        memcpy(&p->expected_len, rbuf, 4);
+        if (p->read_state == 4) {
+          if (p->expected_len <= 0 || p->expected_len > 128 * 1024) {
+            err("%d: protocol error: bad packet length: %d",
+                p->id, p->expected_len);
+            disconnect_client(p);
+            continue;
+          }
+          p->read_len = 0;
+          p->read_buf = (unsigned char*) xcalloc(1, p->expected_len);
+        }
+        FD_CLR(p->fd, &rset);
+        continue;
+      }
+
+      l = p->expected_len - p->read_len;
+      r = read(p->fd, &p->read_buf, l);
+      if (!r) {
+        err("%d: unexpected EOF from client", p->id);
+        disconnect_client(p);
+        continue;
+      }
+      if (r < 0) {
+        err("%d: read() failed: %s", p->id, os_ErrorMsg());
+        disconnect_client(p);
+        continue;
+      }
+
+      p->read_len += r;
+      if (p->expected_len == p->read_len) {
+        process_packet(p, p->expected_len, p->read_buf);
+        p->read_len = 0;
+        p->expected_len = 0;
+        p->read_state = 0;
+        xfree(p->read_buf);
+        p->read_buf = 0;
+      }
+      FD_CLR(p->fd, &rset);
     }
   }
 
