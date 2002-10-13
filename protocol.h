89ca06af50359e0304abe2e44d95f038b60f2813
cher <blackav@gmail.com>
2002-10-13 19:14:48 +0000

    Initial revision

---
+++ protocol.h
@@ -0,0 +1,43 @@
+/* -*- c -*- */
+/* $Id$ */
+
+#ifndef __PROTOCOL_H__
+#define __PROTOCOL_H__
+
+/* Copyright (C) 2002 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <time.h>
+
+#define PROT_SERVE_STATUS_MAGIC (0xe739aa02)
+struct prot_serve_status
+{
+  unsigned int magic;
+  time_t cur_time;
+  time_t start_time;
+  time_t sched_time;
+  time_t duration;
+  time_t stop_time;
+  int total_runs;
+  int total_clars;
+  unsigned char clars_disabled;
+  unsigned char team_clars_disabled;
+  unsigned char standings_frozen;
+};
+
+#endif /* __PROTOCOL_H__ */
