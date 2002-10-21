f3b29e32ab00a0149fb1b12aa0bd311014799462
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2002-10-21 05:29:57 +0000

    new module `expat_iface' is used instead of direct use of the expat   XML parser.

--- trunk/ejudge/userlist_cfg.c
+++ trunk/ejudge/userlist_cfg.c
@@ -20,40 +20,13 @@
  */
 
 #include "userlist_cfg.h"
-#include "nls.h"
-#include "userlist.h"
-#include "utf8_utils.h"
+#include "expat_iface.h"
 #include "pathutl.h"
 
 #include <reuse/xalloc.h>
 #include <reuse/logger.h>
 
-#include <expat.h>
-
 #include <stdio.h>
-#include <string.h>
-
-struct tag_list
-{
-  struct tag_list *next;
-
-  int itag;
-  int a, u;
-  unsigned char *str;
-  void *tree;
-};
-
-struct parser_data
-{
-  int nest;
-  int skipping;
-  int skip_stop;
-  struct tag_list *tag_stack;
-  int err_cntr;
-
-  /* actual data to be parsed */
-  struct userlist_cfg *cfg;
-};
 
 enum
   {
@@ -72,308 +45,110 @@ static char const * const tag_map[] =
   0
 };
 
-static int
-encoding_hnd(void *data, const XML_Char *name, XML_Encoding *info)
+static void *
+tree_alloc_func(int tag)
 {
-  int i, o;
-  unsigned char cb, cu1, cu2;
-  struct nls_table *tab = 0;
-
-  tab = nls_lookup_table(name);
-  if (tab) {
-    for (i = 0; i < 256; i++) {
-      cb = i;
-      tab->char2uni(&cb, &o, &cu1, &cu2);
-      info->map[i] = (cu2 << 8) | cu1;
-    }
-    info->data = 0;
-    info->convert = 0;
-    info->release = 0;
-    return 1;
-  }
-
-  // unsupported encoding
-  return 0;
-}
-
-static void
-handle_attrib(XML_Parser p,
-              struct parser_data *pd, void *tree,
-              unsigned char const *name, int itag,
-              unsigned char const *attrib, unsigned char const *value)
-{
-  switch (itag) {
+  switch (tag) {
   case TG_CONFIG:
-  case TG_FILE:
+    return xcalloc(1, sizeof(struct userlist_cfg));
   case TG_SOCKET:
-    goto invalid_attribute;
-  default:
-    err("Unknown tag: %s", name);
-    pd->err_cntr++;
-    return;
-  }
-  return;
- 
- /*
- invalid_value:
-  fprintf(stderr, "Invalid value of attribute `%s'\n", attrib);
-  return;
- */
-
- invalid_attribute:
-  err("invalid attribute `%s' for tag `%s'", attrib, name);
-}
-
-static void
-start_hnd(void *data, const XML_Char *name, const XML_Char **atts)
-{
-  XML_Parser p = (XML_Parser) data;
-  int cur_val_size = 0, cur_attr_size = 0, val_len, attr_len;
-  char *cur_val = 0, *cur_attr = 0;
-  int cur_tag_size = 0, tag_len;
-  char *cur_tag = 0;
-  struct parser_data *pd = (struct parser_data*) XML_GetUserData(p);
-  struct tag_list *tl = 0;
-  void *tree = 0;
-  int itag = 0;
-
-  /* allocate initial space for attrib and value local buffers */
-  cur_attr_size = 32;
-  cur_attr = (char*) alloca(cur_attr_size);
-  cur_val_size = 32;
-  cur_val = (char*) alloca(cur_val_size);
-
-  /* recode tag to koi8-r */
-  cur_tag_size = 32;
-  tag_len = strlen(name);
-  while (cur_tag_size <= tag_len) {
-    cur_tag_size *= 2;
-  }
-  cur_tag = (char*) alloca(cur_tag_size);
-  str_utf8_to_koi8(cur_tag, cur_tag_size, name);
-
-  if (pd->skipping) {
-    pd->nest++;
-    return;
-  }
-
-  for (itag = 1; tag_map[itag]; itag++)
-    if (!strcmp(cur_tag, tag_map[itag]))
-      break;
-  if (!tag_map[itag]) {
-    err("unknown tag <%s> at line %d, skipping",
-        cur_tag, XML_GetCurrentLineNumber(p));
-    pd->err_cntr++;
-    goto start_skipping;
-  }
-
-  switch (itag) {
-  case TG_CONFIG:
-    if (pd->tag_stack) goto invalid_usage;
-    pd->cfg = xcalloc(1, sizeof(*pd->cfg));
-    tree = pd->cfg;
-    break;
   case TG_FILE:
-    if (!pd->tag_stack || pd->tag_stack->itag != TG_CONFIG)
-      goto invalid_usage;
-    {
-      struct userlist_cfg *uc = (struct userlist_cfg*) pd->tag_stack->tree;
-      if (uc->db_path) {
-        err("%s can be specified only once", cur_tag);
-        pd->err_cntr++;
-        goto start_skipping;
-      }
-      tree = pd->tag_stack->tree;
-    }
-    break;
-  case TG_SOCKET:
-    if (!pd->tag_stack || pd->tag_stack->itag != TG_CONFIG)
-      goto invalid_usage;
-    {
-      struct userlist_cfg *uc = (struct userlist_cfg*) pd->tag_stack->tree;
-      if (uc->socket_path) {
-        err("%s can be specified only once", cur_tag);
-        pd->err_cntr++;
-        goto start_skipping;
-      }
-      tree = pd->tag_stack->tree;      
-    }
-    break;
+    return xcalloc(1, sizeof(struct xml_tree));
   default:
-  invalid_usage:
-    err("Invalid tag `%s' usage", cur_tag);
-    pd->err_cntr++;
-    goto start_skipping;
+    SWERR(("unhandled tag: %d", tag));
   }
-
-  while (*atts) {
-    attr_len = strlen(atts[0]);
-    val_len = strlen(atts[1]);
-    if (attr_len >= cur_attr_size) {
-      while (attr_len >= cur_attr_size) {
-        cur_attr_size *= 2;
-      }
-      cur_attr = (char*) alloca(cur_attr_size);
-    }
-    if (val_len >= cur_val_size) {
-      while (val_len >= cur_val_size) {
-        cur_val_size *= 2;
-      }
-      cur_val = (char*) alloca(cur_val_size);
-    }
-    str_utf8_to_koi8(cur_attr, cur_attr_size, atts[0]);
-    str_utf8_to_koi8(cur_val, cur_val_size, atts[1]);
-    handle_attrib(p, pd, tree, cur_tag, itag, cur_attr, cur_val);
-    atts += 2;
-  }
-
-  tl = (struct tag_list*) xcalloc(1, sizeof(*tl));
-  tl->itag = itag;
-  tl->next = pd->tag_stack;
-  tl->tree = tree;
-  pd->tag_stack = tl;
-  pd->nest++;
-  return;
-
- start_skipping:
-  pd->skipping = 1;
-  pd->nest++;
-  pd->skip_stop = pd->nest;
-}
-
-static void
-end_hnd(void *data, const XML_Char *name)
-{
-  XML_Parser p = (XML_Parser) data;
-  struct parser_data *pd = (struct parser_data*) XML_GetUserData(p);
-  struct tag_list *tl;
-  struct userlist_cfg *uc = 0;
-
-  if (pd->skipping) {
-    pd->nest--;
-    if (pd->nest < pd->skip_stop) {
-      pd->skip_stop = 0;
-      pd->skipping = 0;
-    }
-    return;
-  }
-
-  tl = pd->tag_stack;
-  pd->tag_stack = tl->next;
-  pd->nest--;
-
-  switch (tl->itag) {
-  case TG_CONFIG:
-    break;
-  case TG_FILE:
-    uc = (struct userlist_cfg*) tl->tree;
-    uc->db_path = str_utf8_to_koi8_heap(tl->str);
-    break;
-  case TG_SOCKET:
-    uc = (struct userlist_cfg*) tl->tree;
-    uc->socket_path = str_utf8_to_koi8_heap(tl->str);
-    break;
-  default:
-    err("unexpected closing tag: %s", name);
-    pd->err_cntr++;
-  }
-
-  free(tl->str);
-}
-
-static void
-chardata_hnd(void *data, const XML_Char *s, int len)
-{
-  XML_Parser p = (XML_Parser) data;
-  struct parser_data *pd = (struct parser_data*) XML_GetUserData(p);
-
-  if (!pd->tag_stack) return;
-  if (pd->skipping) return;
-  if (!pd->tag_stack->a) pd->tag_stack->a = 32;
-  while (pd->tag_stack->u + len >= pd->tag_stack->a)
-    pd->tag_stack->a *= 2;
-  pd->tag_stack->str = (char*) xrealloc(pd->tag_stack->str, pd->tag_stack->a);
-  memmove(pd->tag_stack->str + pd->tag_stack->u, s, len);
-  pd->tag_stack->u += len;
-  pd->tag_stack->str[pd->tag_stack->u] = 0;
 }
 
 struct userlist_cfg *
 userlist_cfg_parse(char const *path)
 {
-  XML_Parser p = 0;
-  FILE *f = 0;
-  char buf[512];
-  int len;
-  struct parser_data data;
+  struct xml_tree *tree = 0, *p;
+  struct userlist_cfg *cfg = 0;
 
-  memset(&data, 0, sizeof(data));
-
-  ASSERT(path);
-  if (!(f = fopen(path, "r"))) {
-    err("cannot open input file `%s'", path);
-    goto cleanup_and_exit;
+  tree = xml_build_tree(path, tag_map, 0, tree_alloc_func, 0);
+  if (!tree) return 0;
+  if (tree->tag != TG_CONFIG) {
+    err("%s: %d: top-level tag must be <config>", path, tree->line);
+    goto failed;
   }
-
-  if (!(p = XML_ParserCreate(NULL))) {
-    err("cannot create an XML parser");
-    goto cleanup_and_exit;
-  }
-
-  XML_SetUnknownEncodingHandler(p, encoding_hnd, NULL);
-  XML_SetStartElementHandler(p, start_hnd);
-  XML_SetEndElementHandler(p, end_hnd);
-  XML_SetCharacterDataHandler(p, chardata_hnd);
-  XML_SetUserData(p, &data);
-  XML_UseParserAsHandlerArg(p);
-
-  while (fgets(buf, sizeof(buf), f)) {
-    len = strlen(buf);
-    if (XML_Parse(p, buf, len, 0) == XML_STATUS_ERROR) {
-      err("%s: %d: parse error: %s",
-          path, XML_GetCurrentLineNumber(p),
-          XML_ErrorString(XML_GetErrorCode(p)));
-      goto cleanup_and_exit;
+  cfg = (struct userlist_cfg *) tree;
+  xfree(cfg->b.text);
+  cfg->b.text = 0;
+  for (p = cfg->b.first_down; p; p = p->right) {
+    if (p->tag == TG_FILE) {
+      if (p->first_down) {
+        err("%s: %d: tag <file> cannot contain nested tags", path, p->line);
+        goto failed;
+      }
+      if (cfg->db_path) {
+        err("%s: %d: tag <file> appears more than once", path, p->line);
+        goto failed;
+      }
+      cfg->db_path = p->text;
+    } else if (p->tag == TG_SOCKET) {
+      if (p->first_down) {
+        err("%s: %d: tag <socket> cannot contain nested tags", path, p->line);
+        goto failed;
+      }
+      if (cfg->socket_path) {
+        err("%s: %d: tag <socket> appears more than once", path, p->line);
+        goto failed;
+      }
+      cfg->socket_path = p->text;
+    } else {
+      err("%s: %d: tag <%s> is not valid here", path,p->line,tag_map[p->tag]);
+      goto failed;
     }
   }
 
-  if (ferror(f)) {
-    err("input error");
-    goto cleanup_and_exit;
+  if (!cfg->db_path) {
+    err("%s: tag <file> is not defined", path);
+    goto failed;
   }
-  if (data.err_cntr) goto cleanup_and_exit;
+  if (!cfg->socket_path) {
+    err("%s: tag <socket> is not defined", path);
+    goto failed;
+  }
 
-  XML_ParserFree(p);
-  fclose(f);
-  return data.cfg;
+  userlist_cfg_unparse(cfg, stdout);
+  exit(1);
 
- cleanup_and_exit:
-  if (p) XML_ParserFree(p);
-  if (f) fclose(f);
-  if (data.cfg) userlist_cfg_free(data.cfg);
+ failed:
+  if (tree) userlist_cfg_free((struct userlist_cfg *) tree);
   return 0;
 }
 
 struct userlist_cfg *
 userlist_cfg_free(struct userlist_cfg *cfg)
 {
-  xfree(cfg->db_path);
-  xfree(cfg->socket_path);
-  xfree(cfg);
+  xml_tree_free((struct xml_tree*) cfg, 0, 0);
   return 0;
+}
+
+static void
+fmt_func(FILE *o, struct xml_tree const *p, int s, int n)
+{
+  switch (p->tag) {
+  case TG_CONFIG:
+    if (s == 1 || s == 3) fprintf(o, "\n");
+    break;
+  case TG_FILE:
+  case TG_SOCKET:
+    if (s == 3) fprintf(o, "\n");
+    if (s == 0) fprintf(o, "  ");
+    break;
+  default:
+    SWERR(("unhandled tag %d", p->tag));
+  }
 }
 
 void
 userlist_cfg_unparse(struct userlist_cfg *cfg, FILE *f)
 {
   if (!cfg) return;
-  fprintf(f, "<?xml version=\"1.0\" encoding=\"koi8-r\"?>\n");
-  fprintf(f, "<config>\n");
-  if (cfg->db_path)
-    fprintf(f, "  <file>%s</file>\n", cfg->db_path);
-  if (cfg->socket_path)
-    fprintf(f, "  <socket>%s</socket>\n", cfg->socket_path);
-  fprintf(f, "</config>\n");
+
+  xml_unparse_tree(stdout, (struct xml_tree*) cfg, tag_map, 0, 0, 0,
+                   fmt_func);
 }
 
 /**
