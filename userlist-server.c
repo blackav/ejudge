cfa4402694dd2da0cd56f55391d2721300639094
cher <blackav@gmail.com>
2002-10-28 06:43:30 +0000

    (generate_random_cookie) a new function; (generate_random_password) a new function; merged in Victor Matyukhin's stuff;

--- userlist-server.c
+++ userlist-server.c
@@ -20,7 +20,9 @@
  */
 
 #include "userlist_cfg.h"
+#include "userlist.h"
 #include "pathutl.h"
+#include "base64.h"
 
 #include <reuse/logger.h>
 #include <reuse/osdeps.h>
@@ -32,9 +34,11 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
 #include <signal.h>
+#include <fcntl.h>
 
 #if CONF_HAS_LIBINTL - 0 == 1
 #include <libintl.h>
@@ -70,7 +74,9 @@ struct client_state
 };
 
 static struct userlist_cfg *config;
+static struct userlist_list *users;
 static int listen_socket = -1;
+static int urandom_fd = -1;
 static char *socket_name;
 static struct client_state *first_client;
 static struct client_state *last_client;
@@ -78,6 +84,82 @@ static int serial_id = 1;
 
 static time_t last_flush;
 static unsigned long flush_interval;
+
+static unsigned long long
+generate_random_cookie(void)
+{
+  unsigned long long val = 0;
+  int n, r;
+  char *p;
+
+  ASSERT(urandom_fd >= 0);
+  while (!val) {
+    p = (char*) &val;
+    r = sizeof(val);
+    while (r > 0) {
+      n = read(urandom_fd, p, r);
+      if (n < 0) {
+        err("read from /dev/urandom failed: %s", os_ErrorMsg());
+        exit(1);
+      }
+      if (!n) {
+        err("EOF on /dev/urandom???");
+        exit(1);
+      }
+      p += n;
+      r -= n;
+    }
+    if (!val) {
+      info("got 0 from /dev/urandom");
+    }
+  }
+
+  return val;
+}
+
+static void
+generate_random_password(int size, unsigned char *buf)
+{
+  int rand_bytes;
+  unsigned char *rnd_buf = 0;
+  unsigned char *b64_buf = 0;
+  unsigned char *p;
+  int r, n;
+
+  ASSERT(size > 0 && size <= 128);
+  ASSERT(buf);
+
+  // estimate the number of random bytes to generate
+  rnd_buf = (unsigned char*) alloca(size + 16);
+  b64_buf = (unsigned char *) alloca(size + 16);
+  if (size % 4) {
+    rand_bytes = (size / 4 + 1) * 3;
+  } else {
+    rand_bytes = (size / 4) * 3;
+  }
+
+  // generate the needed number of bytes
+  r = rand_bytes;
+  p = rnd_buf;
+  while (r > 0) {
+    n = read(urandom_fd, p, r);
+    if (n < 0) {
+      err("read from /dev/urandom failed: %s", os_ErrorMsg());
+      exit(1);
+    }
+    if (!n) {
+      err("EOF on /dev/urandom???");
+      exit(1);
+    }
+    p += n;
+    r -= n;
+  }
+
+  // convert to base64
+  base64_encode(rnd_buf, rand_bytes, b64_buf);
+  b64_buf[size] = 0;
+  strcpy(buf, b64_buf);
+}
 
 static void
 disconnect_client(struct client_state *p)
@@ -103,6 +185,210 @@ disconnect_client(struct client_state *p)
   xfree(p);
 }
 
+#if 0
+int send_email(char const *dest, char const *subj, char const *text)
+{
+}
+
+void send_message(struct client_state *p)
+{
+}
+
+static void
+send_reply(struct client_state *p,short answer)
+{
+  p->write_len = sizeof(short);
+  p->write_buf = xmalloc(sizeof(short));
+  *(p->write_buf) = answer;
+  send_message(p);
+}
+
+
+char * mkpasswd()
+
+static void
+create_newuser(struct client_state *p, struct newuser_data * data)
+{
+  struct user_data * user;
+  char * buf;
+
+  data->login = data->buffer;
+  data->email = data->buffer + data->login_length + 1;
+  
+  
+  user = userlist->first;
+  while (user) {
+    if (!strcmp(user_data_get_field(user, TAG_LOGIN),data->login)) {
+      //Login already exists
+      send_reply(p,1);
+      return;
+    }
+    user = user->next;
+  }
+  
+  userlist->last->next = xcalloc(1, sizeof(user_data));
+  userlist->last = userlist->last->next;
+
+  user = userlist->last;
+
+  user->next=nul;
+  user->parent = userlist;
+  
+//!!!Must create user_id
+
+  user->ip = data->origin_ip;
+  user->contest = data->contest_id;
+  user->locale = data->locale_id;
+  user->use_cookies = data->use_cookies;
+  user_data_set_field(user,TAG_LOGIN,data->login);
+  user_data_set_field(user,TAG_EMAIL,data->email);
+
+  user_data_set_field(user,TAG_PASSWORD,mkpasswd());
+
+  buf = (char *) xmalloc(255);
+  sprintf(buf,"You are registered in contest system \n"
+            "http://contest.cmc.msu.ru/cgi-bin/register?contest_id=%d&locale_id=%d\n"
+            "login: %s\n"
+            "password: %s\n",
+            user->contest,user->locale,
+            user->login, user->passwd);
+  send_email(user->email,"Registration in contest system",buf);
+  free(x);
+  send_reply(p,0);
+          
+}
+
+long long generate_cookie()
+{
+  //Must generate cookie
+}
+
+static void
+login_user(struct client_state *p, struct loginuser_data * data)
+{
+  struct user_data * user;
+  struct loginuser_answer * answer;
+ 
+  data->login = data->buffer;
+  data->password = data->buffer + data->login_length + 1;
+                                                         
+  user = userlist->first;
+  while (user) {
+    if (!strcmp(user->login,data->login)) {
+      if (!strcmp(user->passwd,data->password)) {
+        //Login and password correct
+        p->write_len = sizeof(loginuser_answer)+strlen(user->descr);
+        answer = xmalloc(p->write_len);
+        p->write_buf = (unsigned char *)answer;
+
+        user->locale = data->locale_id;
+        user->use_cookies = data->use_cookies;
+        user->ip = data->origin_ip;
+
+        if (data->usecookies==1) {
+          answer->reply = 5;
+          answer->cookie = generate_cookie();
+          //!!!Add time of experied od cookie
+        } else {
+          answer->reply = 6;
+          answer->cookie = 0;
+        }
+        answer->user_id = user->id;
+        answer->name_len = strlen(user->descr);
+        strcopy(answer->name,user->descr);
+        send_message(p);
+        exit;
+       
+      } else {
+        //Incorrect password
+        send_reply(p,3);
+        exit;
+      }
+    }
+    user = user->next;
+  }
+  //Wrong login
+  send_reply(p,2);
+}
+
+static void
+login_cookie(struct client_state *p, struct logincookie_data * data)
+{
+  struct user_data * user;
+  struct logincookie_answer * answer;
+
+  user=userlist->first;
+  while (user) {
+    if ((user->ip==data->origin_ip)&&(user->cookie==data->cookie)) {
+      //!!! Must check time of cookie
+      p->write_len = sizeof(logincookie_answer)+strlen(user->login)+strlen(user->descr)+1;
+      answer = xmalloc(p->write_len);
+      p->write_buf = (unsigned char *)answer;
+      user->locale = data->locale_id;
+      answer->reply = 8;
+      answer->user_id = user->id;
+      answer->login_length = strlen(user->login);
+      answer->name_length = strlen(user->descr);
+      strcopy(answer->buf,user->login);
+      strcopy(answer->buf + strlen(user->login) + 1, user->descr);
+      send_message(p);
+      exit;
+    }
+    user = user->next;
+  }
+  send_reply(p,7);
+}
+
+static void
+logout_user(struct client_state *p, struct logout_data * data)
+{
+  struct user_data * user;
+
+  if (!(data->cookie)) {
+    send_reply(p,0);
+    return;
+  }
+
+  user = userlist->first;
+
+  while (user) {
+    if ((user->ip==data->origin_ip)&&(user->cookie==data->cookie)) {
+      user->cookie = 0;
+      send_reply(p,0);
+      return;
+    }
+    user = user->next;
+  }   
+  send_reply(p,0);
+}
+
+
+  
+
+
+
+static void
+process_packet(struct client_state *p, int len, unsigned char *pack)
+{
+  struct packet_struct * packet;
+  
+  packet = (struct packet_struct *) pack;
+  switch (packet->request) {
+  case INCR_NEWUSER:
+    create_newuser(p, (struct newuser_data *) pack);  
+    break;
+  
+  case INCR_LOGIN:
+    login_user(p, (struct loginuser_data *) pack);  
+    break;
+    
+  case INCR_COOKIE:
+    login_cookie(p, (struct logincookie_data *) pack);  
+    break;
+
+}
+#endif /* 0 */
+
 static void
 process_packet(struct client_state *p, int len, unsigned char *pack)
 {
@@ -120,6 +406,11 @@ do_work(void)
   time_t cur_time;
 
   signal(SIGPIPE, SIG_IGN);
+
+  if((urandom_fd = open("/dev/urandom", O_RDONLY)) < 0) {
+    err("open of /dev/urandom failed: %s", os_ErrorMsg());
+    return 1;
+  }
 
   if ((listen_socket = socket(PF_UNIX, SOCK_STREAM, 0)) < 0) {
     err("socket() failed :%s", os_ErrorMsg());
@@ -270,7 +561,7 @@ do_work(void)
 
         // we expect 4 zero bytes and credentials
         memset(&msg, 0, sizeof(msg));
-        msg.msg_flags = 0; //MSG_DONTWAIT;
+        msg.msg_flags = 0;
         msg.msg_control = msgbuf;
         msg.msg_controllen = sizeof(msgbuf);
         recv_vec[0].iov_base = &val;
@@ -409,7 +700,12 @@ main(int argc, char *argv[])
 
   config = userlist_cfg_parse(argv[1]);
   if (!config) return 1;
+  users = userlist_parse(config->db_path);
+  if(!users) return 1;
 
+  userlist_unparse(users, stdout);
+  return 0;
+#if 0
   code = do_work();
 
   if (socket_name) unlink(socket_name);
@@ -417,6 +713,7 @@ main(int argc, char *argv[])
   //userlist_cfg_unparse(config, stdout);
   userlist_cfg_free(config);
   return code;
+#endif
   
  print_usage:
   printf("Usage: %s config-file\n", argv[0]);
