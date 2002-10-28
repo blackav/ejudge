31ee7045c596cab981adbfeb9e42cf05f7a330e3
cher <blackav@gmail.com>
2002-10-28 06:46:28 +0000

    packet formats are reworked considerably.

--- userlist_proto.h
+++ userlist_proto.h
@@ -26,6 +26,9 @@
 enum
   {
     ULS_REGISTER_NEW = 1,
+    ULS_DO_LOGIN,
+    ULS_CHECK_COOKIE,
+    ULS_DO_LOGOUT,
   };
 
 /* server reply codes */
@@ -42,6 +45,7 @@ enum
     ULS_EMAIL,
   };
 
+/* a generic packet structure */
 struct userlist_packet __attribute__((packed));
 struct userlist_packet
 {
@@ -49,17 +53,63 @@ struct userlist_packet
   char bytes[0];
 };
 
+/* client->server requests packet */
 struct userlist_pk_register_new __attribute__((packed));
 struct userlist_pk_register_new
 {
-  unsigned short id;
-  unsigned long origin_ip;
-  int contest_id;
-  unsigned char locale_id;
-  unsigned char use_cookies;
-  unsigned char login_length;
-  unsigned char email_length;
-  unsigned char data[0];
+  unsigned short request_id;
+  unsigned long  origin_ip;
+  long           contest_id;
+  signed char    locale_id;
+  signed char    use_cookies;
+  unsigned char  login_length;
+  unsigned char  email_length;
+  unsigned char  data[0];
+};
+
+struct userlist_pk_do_login __attribute__((packed));
+struct userlist_pk_do_login
+{
+  unsigned short request_id;
+  unsigned long  origin_ip;
+  long           contest_id;
+  signed char    locale_id;
+  signed char    use_cookies;
+  unsigned char  login_length;
+  unsigned char  password_length;
+  unsigned char  data[0];
+};
+
+struct userlist_pk_check_cookie __attribute__((packed));
+struct userlist_pk_check_cookie
+{
+  unsigned short     request_id;
+  unsigned long      origin_ip;
+  long               contest_id;
+  unsigned long long cookie;
+  signed char        locale_id;
+};
+
+struct userlist_pk_do_logout __attribute__((packed));
+struct userlist_pk_do_logout
+{
+  unsigned short     request_id;
+  unsigned long      origin_ip;
+  long               contest_id;
+  unsigned long long cookie;
+  long               user_id;
+};
+
+/* server->client replies */
+struct userlist_pk_login_ok __attribute__((packed));
+struct userlist_pk_login_ok
+{
+  unsigned short     reply_id;
+  long               user_id;
+  unsigned long long cookie;
+  signed char        locale_id;
+  unsigned char      name_len;
+  char               data[0];
 };
 
 #endif /* __USERLIST_PROTO_H__ */
