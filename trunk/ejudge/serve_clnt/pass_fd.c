3a0d2962d07224c4449711ef787a7336290cbb0d
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2002-11-24 14:39:20 +0000

    Initial revision

---
+++ trunk/ejudge/serve_clnt/pass_fd.c
@@ -0,0 +1,62 @@
+/* -*- mode: c; coding: koi8-r -*- */
+/* $Id$ */
+
+/* Copyright (C) 2002 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "serve_clnt.h"
+#include "protocol.h"
+#include "pathutl.h"
+
+#include <reuse/logger.h>
+#include <reuse/xalloc.h>
+#include <reuse/osdeps.h>
+
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+
+int
+serve_clnt_pass_fd(int sock_fd, int nfd, int *fds)
+{
+  struct prot_serve_packet *out = 0;
+  int out_size = 0;
+  int r;
+
+  out_size = sizeof(*out);
+  out = alloca(out_size);
+  out->id = SRV_CMD_PASS_FD;
+  out->magic = PROT_SERVE_PACKET_MAGIC;
+  if ((r = serve_clnt_send_packet(sock_fd, out_size, out)) < 0) return r;
+  return serve_clnt_do_pass_fd(sock_fd, nfd, fds);
+}
+
+/**
+ * Local variables:
+ *  compile-command: "make"
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ *  eval: (set-language-environment "Cyrillic-KOI8")
+ * End:
+ */
---
+++ trunk/ejudge/serve_clnt/send_packet.c
@@ -0,0 +1,70 @@
+/* -*- mode: c; coding: koi8-r -*- */
+/* $Id$ */
+
+/* Copyright (C) 2002 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "serve_clnt.h"
+#include "protocol.h"
+#include "pathutl.h"
+
+#include <reuse/logger.h>
+#include <reuse/xalloc.h>
+#include <reuse/osdeps.h>
+
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+
+int
+serve_clnt_send_packet(int sock_fd, int size, void const *buf)
+{
+  unsigned char *b;
+  int w, n;
+
+  if (sock_fd < 0) return -SRV_ERR_NOT_CONNECTED;
+  b = (unsigned char*) alloca(size + 4);
+  memcpy(b, &size, 4);
+  memcpy(b + 4, buf, size);
+  w = size + 4;
+
+  while (w > 0) {
+    n = write(sock_fd, b, w);
+    if (n <= 0) {
+      err("serve_clnt_send_packet: write() failed: %s", os_ErrorMsg());
+      return -SRV_ERR_WRITE_TO_SERVER;
+    }
+    w -= n; b += n;
+  }
+  return 0;
+}
+
+/**
+ * Local variables:
+ *  compile-command: "make"
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ *  eval: (set-language-environment "Cyrillic-KOI8")
+ * End:
+ */
