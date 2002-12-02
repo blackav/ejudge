c3c817f1ff079ffc9bb37d11bee321b517dbb66c
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2002-12-02 08:37:23 +0000

    <time.h> is included; <string.h> is included; new status RUN_IGNORED added; (run_open) prototype changed to use `const' qualifier; (run_add_record) `time_t', `size_t' types are used instead of   unsigned long;   `ip' is passed as unsigned long rather than char;   new parameter `sha1' to pass the hash value; (run_start_contest,run_get_start_time) `time_t' type is used; (run_get_record)  `time_t', `size_t' types are used;   `ip' pointer is now `unsigned long *', not `unsigned char*';   new parameter to return the sha1 hash value; (run_get_times) `time_t' type is used; (run_set_duration) the same; (run_get_stop_time) the same; (run_stop_contest) the same; (run_sched_contest) the same; (run_get_total) the same; (run_get_fog_period) the same; (run_get_team_usage) `size_t' type is used; (run_unparse_ip) new function prototype; (run_parse_ip) new function prototype; (run_check_duplicate) new function prototype.

--- trunk/ejudge/runlog.h
+++ trunk/ejudge/runlog.h
@@ -21,6 +21,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <time.h>
+#include <string.h>
+
 #define RUN_MAX_IP_LEN 15
 
 enum
@@ -34,7 +37,8 @@ enum
   RUN_CHECK_FAILED     = 6,
   RUN_PARTIAL          = 7,
   RUN_ACCEPTED         = 8,
-  RUN_MAX_STATUS       = 8,
+  RUN_IGNORED          = 9,
+  RUN_MAX_STATUS       = 9,
   RUN_RUNNING          = 96,
   RUN_COMPILED         = 97,
   RUN_COMPILING        = 98,
@@ -44,37 +48,42 @@ enum
 
 enum { RUN_LOG_CREATE = 1 };
 
-int run_open(char *path, int flags);
-int run_add_record(unsigned long  timestamp, 
-                   unsigned long  size,
-                   char const    *ip,
+int run_open(const char *path, int flags);
+int run_add_record(time_t         timestamp, 
+                   size_t         size,
+                   unsigned long  sha1[5],
+                   unsigned long  ip,
                    int            locale_id,
                    int            team,
                    int            language,
                    int            problem);
-int run_start_contest(unsigned long);
-unsigned long run_get_start_time(void);
+int run_start_contest(time_t);
+time_t run_get_start_time(void);
 int run_change_status(int runid, int newstatus, int newtest, int newscore);
 int run_get_status(int runid);
 int run_get_param(int runid, int *ploc_id, int *plang, int *pprob, int *pstat);
-int run_get_record(int, unsigned long *, unsigned long *,
-                   char *,
+int run_get_record(int, time_t *, size_t *, unsigned long *,
+                   unsigned long *,
                    int *, int *, int *, int *, int *, int *, int *);
 
-void run_get_times(unsigned long *, unsigned long *, unsigned long *,
-                   unsigned long *);
-int  run_set_duration(unsigned long);
+void run_get_times(time_t *, time_t *, time_t *, time_t *);
+int  run_set_duration(time_t);
 
-unsigned long run_get_stop_time(void);
-int           run_stop_contest(unsigned long);
-int           run_sched_contest(unsigned long);
-int           run_get_total(void);
+time_t run_get_stop_time(void);
+int    run_stop_contest(time_t);
+int    run_sched_contest(time_t);
+int    run_get_total(void);
 
-void run_get_team_usage(int, int *, unsigned long*);
+void run_get_team_usage(int, int *, size_t*);
 int  run_get_attempts(int, int *);
 char *run_status_str(int, char *, int);
 
-int run_get_fog_period(unsigned long cur_time, int, int);
+int run_get_fog_period(time_t, int, int);
 void run_reset(void);
+
+unsigned char *run_unparse_ip(unsigned long ip);
+unsigned long run_parse_ip(unsigned char const *buf);
+
+int run_check_duplicate(int run_id);
 
 #endif /* __RUNLOG_H__ */
