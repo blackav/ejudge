156d1ebf2ce9069f4b48b2523ebb31e214fb24e6
cher <blackav@gmail.com>
2002-12-07 06:53:32 +0000

    Initial revision

---
+++ userlist_clnt/get_uid_by_pid.c
@@ -0,0 +1,68 @@
+/* -*- mode: c; coding: koi8-r -*- */
+/* $Id$ */
+
+/* Copyright (C) 2002 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "userlist_clnt/private.h"
+
+int
+userlist_clnt_get_uid_by_pid(struct userlist_clnt *clnt,
+                             int system_uid,
+                             int system_gid,
+                             int system_pid,
+                             int *p_uid,
+                             unsigned long long *p_cookie)
+{
+  struct userlist_pk_get_uid_by_pid *out = 0;
+  struct userlist_pk_uid *in = 0;
+  size_t out_size = 0, in_size = 0;
+  int r;
+
+  out_size = sizeof(*out);
+  out = alloca(out_size);
+  memset(out, 0, out_size);
+  out->request_id = ULS_GET_UID_BY_PID;
+  out->system_uid = system_uid;
+  out->system_gid = system_gid;
+  out->system_pid = system_pid;
+  if ((r = userlist_clnt_send_packet(clnt, out_size, out)) < 0) return r;
+  if ((r = userlist_clnt_recv_packet(clnt, &in_size, (void*) &in)) < 0)
+    return r;
+  if (in_size < sizeof(struct userlist_packet)) {
+    xfree(in);
+    return -ULS_ERR_PROTOCOL;
+  }
+  if (in->reply_id < 0) {
+    r = in->reply_id;
+    xfree(in);
+    return r;
+  }
+  if (in->reply_id != ULS_UID || in_size != sizeof(*in)) {
+    xfree(in);
+    return -ULS_ERR_PROTOCOL;
+  }
+  if (p_uid) *p_uid = in->uid;
+  if (p_cookie) *p_cookie = in->cookie;
+  xfree(in);
+  return ULS_UID;
+}
+
+/**
+ * Local variables:
+ *  compile-command: "make -C .."
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ *  eval: (set-language-environment "Cyrillic-KOI8")
+ * End:
+ */
