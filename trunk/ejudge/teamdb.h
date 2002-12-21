cbffaf74c2078e39bdd9bbdc51000dc03ef58f21
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2002-12-21 19:18:31 +0000

    (teamdb_lookup_login) implemented for the client mode; (teamdb_scramble_passwd) removed; (teamdb_check_scrambled_passwd) removed; (teamdb_check_passwd) removed; (teamdb_set_scrambled_passwd) removed; (teamdb_get_plain_passwd) removed; (teamdb_get_write_passwd) removed; (teamdb_write_teamdb) removed; (teamdb_is_valid_login) removed; (teamdb_is_valid_name) removed; (teamdb_change_login) removed; (teamdb_change_name) removed; (teamdb_add_team) removed; (teamdb_transaction) removed; (teamdb_commit) removed; (teamdb_rollback) removed; (teamdb_regenerate_passwords) parameter changed: now plain file   descriptor is accepted and passed directly to userlist-server; (teamdb_get_uid_by_pid) new parameter `p_ip' added; (struct teamdb_export) fields `scrambled', `passwd' are removed.

--- trunk/ejudge/teamdb.h
+++ trunk/ejudge/teamdb.h
@@ -28,10 +28,7 @@
 /* various team flags */
 enum { TEAM_BANNED = 1, TEAM_INVISIBLE = 2 };
 
-int teamdb_open(char const *, char const *, int);
 int teamdb_open_client(unsigned char const *socket_path, int contest_id);
-int teamdb_write_passwd(char const *);
-int teamdb_write_teamdb(char const *);
 void teamdb_refresh(void);
 
 int teamdb_lookup(int);
@@ -39,37 +36,19 @@ int teamdb_lookup_login(char const *);
 
 char *teamdb_get_login(int);
 char *teamdb_get_name(int);
-int   teamdb_get_plain_password(int, char *, int);
-int   teamdb_scramble_passwd(char const *, char *);
-int   teamdb_check_scrambled_passwd(int, char const *);
-int   teamdb_set_scrambled_passwd(int, char const *);
-int   teamdb_check_passwd(int, char const *);
 int   teamdb_get_max_team_id(void);
 int   teamdb_get_flags(int);
 int   teamdb_get_total_teams(void);
 
-int teamdb_regenerate_passwords(unsigned char const *path);
+int teamdb_regenerate_passwords(int fd);
 
-int   teamdb_is_valid_login(char const *);
-int   teamdb_is_valid_name(char const *);
-int   teamdb_change_login(int, char const *);
-int   teamdb_change_name(int, char const *);
 int   teamdb_toggle_ban(int);
 int   teamdb_toggle_vis(int);
-
-void teamdb_transaction(void);
-void teamdb_commit(void);
-void teamdb_rollback(void);
-
-int teamdb_add_team(int, char const *, char const *, char const *,
-                    int, int, char **);
 
 /* this is export data structure */
 enum {
   TEAMDB_LOGIN_LEN = 64,
   TEAMDB_NAME_LEN = 64,
-  TEAMDB_SCRAMBLED_LEN = 128,
-  TEAMDB_PASSWD_LEN = 128
 };
 
 struct teamdb_export
@@ -78,8 +57,6 @@ struct teamdb_export
   int flags;
   char login[TEAMDB_LOGIN_LEN];
   char name[TEAMDB_NAME_LEN];
-  char scrambled[TEAMDB_SCRAMBLED_LEN];
-  char passwd[TEAMDB_PASSWD_LEN];
 };
 
 int teamdb_export_team(int id, struct teamdb_export *);
@@ -87,6 +64,7 @@ int teamdb_export_team(int id, struct teamdb_export *);
 time_t teamdb_get_archive_time(int uid);
 int    teamdb_set_archive_time(int uid, time_t time);
 
-int teamdb_get_uid_by_pid(int, int, int, int *, int *,unsigned long long *);
+int teamdb_get_uid_by_pid(int, int, int, int *, int *,unsigned long long *,
+                          unsigned long *);
 
 #endif /* __TEAMDB_H__ */
