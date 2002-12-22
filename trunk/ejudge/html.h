a393c8c5aed4cef62339f98e3e3f59e45228bf6a
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2002-12-22 13:13:05 +0000

    (write_runs_table) removed; (write_clars_table) removed; (write_clar_view) removed; (write_team_statistics) removed; (write_judge_allstat) removed; (write_judge_source_view) removed; (write_judge_report_view) removed; (write_judge_standings) removed; (write_judge_teams_view) removed; (write_judge_one_team_view) removed; (new_write_user_runs) prototype changed; (new_write_user_clars) prototype changed; (write_team_page) prototype changed; (write_master_page) parameters `sid_mode', `sid' are added; (write_priv_standings) prototype added; (write_standings_header) prototype added; (do_write_kirov_standings) prototype added; (do_write_standings) prototype added; (write_priv_source) prototype added; (write_priv_report) prototype added; (write_priv_clar) prototype added; (write_priv_users) prototype added; (html_start_form) prototype added; (html_hyperref) prototype added.

--- trunk/ejudge/html.h
+++ trunk/ejudge/html.h
@@ -24,42 +24,79 @@
 #include <stdio.h>
 #include <time.h>
 
-void write_runs_table(int, int, char const *, char const *, int);
-void write_clars_table(int, int, char const *, char const *, int);
-void write_clar_view(int, char const *, char const *, char const *, int);
-void write_team_statistics(int, int, int, char const *, char const *);
 void write_standings(char const *, char const *, char const *, char const *);
 void write_public_log(char const *, char const *, char const *, char const *);
 
-void write_judge_allstat(int, int, int, char const *, char const *);
+void new_write_user_runs(FILE *f, int uid, unsigned int show_flags,
+                         int sid_mode, unsigned long long sid,
+                         unsigned char const *self_url,
+                         unsigned char const *hidden_vars);
 
-void write_judge_source_view(char const *, int);
-void write_judge_report_view(char const *, int);
-void write_judge_standings(char const *);
-void write_judge_teams_view(char const *, int);
-void write_judge_one_team_view(char const *, int);
+void new_write_user_clars(FILE *f, int uid, unsigned int show_flags,
+                          int sid_mode, unsigned long long sid,
+                          unsigned char const *self_url,
+                          unsigned char const *hidden_vars);
 
-void new_write_user_runs(FILE *, int, unsigned int, unsigned char const *);
-void new_write_user_clars(FILE *, int, unsigned int, unsigned char const *);
 int new_write_user_clar(FILE *, int, int);
 int new_write_user_source_view(FILE *, int, int);
 int new_write_user_report_view(FILE *, int, int);
 
-void write_team_page(FILE *f,
-                     int user_id,
-                     int all_runs,
-                     int all_clars,
-                     unsigned char const *simple_form,
-                     unsigned char const *multi_form,
+void write_team_page(FILE *f, int user_id,
+                     int sid_mode, unsigned long long sid,
+                     int all_runs, int all_clars,
+                     unsigned char const *self_url,
+                     unsigned char const *hidden_vars,
                      time_t server_start,
                      time_t server_end);
 
 void write_master_page(FILE *f,
                        int user_id, int priv_level,
+                       int sid_mode, unsigned long long sid,
                        int first_run, int last_run,
                        int first_clar, int last_clar,
                        unsigned char const *self_url,
                        unsigned char const *filter_expr,
                        unsigned char const *hidden_vars);
+
+void write_priv_standings(FILE *f,
+                          int sid_mode,
+                          unsigned long long sid,
+                          unsigned char const *self_url,
+                          unsigned char const *hidden_vars);
+
+void write_standings_header(FILE *, int, unsigned char const *);
+void do_write_kirov_standings(FILE *, int, unsigned char const *);
+void do_write_standings(FILE *, int, unsigned char const *);
+
+int write_priv_source(FILE *f, int user_id, int priv_level,
+                      int sid_mode, unsigned long long sid,
+                      unsigned char const *self_url,
+                      unsigned char const *hidden_vars,
+                      int run_id);
+int write_priv_report(FILE *f, int user_id, int priv_level,
+                      int sid_mode, unsigned long long sid,
+                      unsigned char const *self_url,
+                      unsigned char const *hidden_vars,
+                      int run_id);
+
+int write_priv_clar(FILE *f, int user_id, int priv_level,
+                    int sid_mode, unsigned long long sid,
+                    unsigned char const *self_url,
+                    unsigned char const *hidden_vars,
+                    int clar_id);
+
+int write_priv_users(FILE *f, int user_id, int priv_level,
+                     int sid_mode, unsigned long long sid,
+                     unsigned char const *self_url,
+                     unsigned char const *hidden_vars);
+
+void html_start_form(FILE *f, int mode,
+                     int sid_mode, unsigned long long sid,
+                     unsigned char const *self_url,
+                     unsigned char const *hidden_vars);
+unsigned char *html_hyperref(unsigned char *buf, int size,
+                             int sid_mode, unsigned long long sid,
+                             unsigned char const *self_url,
+                             unsigned char const *format, ...);
 
 #endif /* __HTML_H__ */
