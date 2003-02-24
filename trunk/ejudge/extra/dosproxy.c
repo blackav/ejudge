8ee77a53c835afec6186dfcee66d0fc0612d00e0
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2003-02-24 06:43:38 +0000

    copyright notice updated.

--- trunk/ejudge/extra/dosproxy.c
+++ trunk/ejudge/extra/dosproxy.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002,2003 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
