c242935e434cdd00dd0bdb7f36014a32ed360bbb
cher <blackav@gmail.com>
2003-07-09 17:42:29 +0000

    Initial revision

---
+++ copyright.h
@@ -0,0 +1,28 @@
+/* -*- c -*- */
+/* $Id$ */
+#ifndef __COPYRIGHT_H__
+#define __COPYRIGHT_H__
+
+/* Copyright (C) 2003 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+unsigned char *get_copyright(void);
+
+#endif /* __COPYRIGHT_H__ */
+/**
+ * Local variables:
+ *  compile-command: "make"
+ * End:
+ */
+
