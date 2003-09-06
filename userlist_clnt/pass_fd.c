6d0b7f1c1e928fc6d642a5f1e6f90d87d23f610e
cher <blackav@gmail.com>
2003-09-06 19:51:21 +0000

    copyright notice is updated

--- userlist_clnt/pass_fd.c
+++ userlist_clnt/pass_fd.c
@@ -1,7 +1,7 @@
 /* -*- mode: c; coding: koi8-r -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002,2003 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -35,6 +35,5 @@ userlist_clnt_pass_fd(struct userlist_clnt *clnt, int nfd, int *fds)
  * Local variables:
  *  compile-command: "make -C .."
  *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
- *  eval: (set-language-environment "Cyrillic-KOI8")
  * End:
  */
