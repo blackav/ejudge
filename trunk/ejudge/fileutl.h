e54c2e62ece5e4332da2efa7bb062af0399e5dc3
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2003-10-12 20:06:09 +0000

    copyright notice is updated; (remove_directory_recursively) new function prototype added.

--- trunk/ejudge/fileutl.h
+++ trunk/ejudge/fileutl.h
@@ -3,22 +3,18 @@
 #ifndef __FILEUTL_H__
 #define __FILEUTL_H__
 
-/* Copyright (C) 2000,2001 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2000-2003 Alexander Chernov <cher@ispras.ru> */
 
 /*
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  */
 
 #include <stdlib.h>
@@ -53,5 +49,6 @@ enum { SPOOL_IN, SPOOL_OUT };
 int   check_writable_spool(char const *, int);
 
 int   relaxed_remove(char const *, char const *);
+int   remove_directory_recursively(const unsigned char *path);
 
 #endif /* __FILEUTL_H__ */
