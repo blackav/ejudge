be7c6814b442b3c3efaf3c83d07092d2d6a77f11
cher <blackav@gmail.com>
2003-11-04 05:44:55 +0000

    Initial revision

---
+++ tsc.c
@@ -0,0 +1,71 @@
+/* -*- mode: c -*- */
+/* $Id$ */
+
+/* Copyright (C) 2003 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "tsc.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+
+long long cpu_frequency;
+int
+tsc_init(void)
+{
+  FILE *f = 0;
+  unsigned char buf[1024];
+  size_t len;
+  int lineno = 0, n;
+  unsigned char * path = "/proc/cpuinfo";
+  long double cpuf;
+
+  if (!(f = fopen(path, "r"))) {
+    fprintf(stderr, "%s: cannot open: %s\n", path, strerror(errno));
+    goto failure;
+  }
+  while (fgets(buf, sizeof(buf), f)) {
+    lineno++;
+    len = strlen(buf);
+    if (len > sizeof(buf) - 2) {
+      fprintf(stderr, "%s: %d: line is too long (%zu)\n", path, lineno, len);
+      goto failure;
+    }
+    while (len > 0 && isspace(buf[len - 1])) buf[--len] = 0;
+    if (strncmp(buf, "cpu MHz", 7)) continue;
+    if (sscanf(buf, "cpu MHz : %Lf%n", &cpuf, &n) != 1 || buf[n]) {
+      fprintf(stderr, "%s: %d: cannot parse `cpu MHz' line\n", path, lineno);
+      goto failure;
+    }
+    cpu_frequency = (long long) (cpuf * 1000000.0L);
+    fprintf(stderr, "Detected CPU frequency is %lld\n", cpu_frequency);
+    break;
+  }
+  fclose(f);
+  return 0;
+
+ failure:
+  if (f) fclose(f);
+  return -1;
+}
+
+/**
+ * Local variables:
+ *  compile-command: "make"
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ * End:
+ */
---
+++ tsc.h
@@ -0,0 +1,41 @@
+/* -*- c -*- */
+/* $Id$ */
+#ifndef __TSC_H__
+#define __TSC_H__
+
+/* Copyright (C) 2003 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/* taken from linux/include/asm-i386/msr.h */
+#define rdtsc(low,high) \
+     __asm__ __volatile__("rdtsc" : "=a" (low), "=d" (high))
+
+#define rdtscl(low) \
+     __asm__ __volatile__("rdtsc" : "=a" (low) : : "edx")
+
+#define rdtscll(val) \
+     __asm__ __volatile__("rdtsc" : "=A" (val))
+
+extern long long cpu_frequency;
+
+int tsc_init(void);
+
+#endif /* __TSC_H__ */
+
+/**
+ * Local variables:
+ *  compile-command: "make"
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ * End:
+ */
