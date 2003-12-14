491b4df3d9317665e9a86f08fbf3f6f8bff026ba
cher <blackav@gmail.com>
2003-12-14 10:06:40 +0000

    (serve_clnt_edit_run) two new parameters added: `tests', `score' to   allow editing of the corresponding runlog entry fields.

--- serve_clnt/edit_run.c
+++ serve_clnt/edit_run.c
@@ -27,6 +27,7 @@ int
 serve_clnt_edit_run(int sock_fd, int run_id, int mask,
                     int user_id, int prob_id, int lang_id, int status,
                     int is_imported, int variant, int is_hidden,
+                    int tests, int score,
                     unsigned char const *user_login)
 {
   struct prot_serve_pkt_run_info *out;
@@ -53,6 +54,8 @@ serve_clnt_edit_run(int sock_fd, int run_id, int mask,
   out->is_imported = is_imported;
   out->variant = variant;
   out->is_hidden = is_hidden;
+  out->tests = tests;
+  out->score = score;
   out->user_login_len = user_login_len;
   strcpy(user_login_ptr, user_login);
 
