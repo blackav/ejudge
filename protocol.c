fd2f60094e7c5d33a2bfe829315e0e85f8e22d39
cher <blackav@gmail.com>
2003-12-21 21:42:58 +0000

    message for SRV_ERR_READONLY_RUN is added.

--- protocol.c
+++ protocol.c
@@ -60,6 +60,7 @@ static unsigned char const * const error_map[] =
   _("invalid contest duration"),
   _("invalid run status"),
   _("allowed only in virtual contest"),
+  _("the run is read-only"),
 };
 #undef _
 
