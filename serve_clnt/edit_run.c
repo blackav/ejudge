5e242cedd92e2829f3020928a0de9906e7acb5bb
cher <blackav@gmail.com>
2003-12-21 21:41:09 +0000

    (serve_clnt_edit_run) new parameter `is_readonly' is added. This   parameter is passed to the server.

--- serve_clnt/edit_run.c
+++ serve_clnt/edit_run.c
@@ -27,7 +27,7 @@ int
 serve_clnt_edit_run(int sock_fd, int run_id, int mask,
                     int user_id, int prob_id, int lang_id, int status,
                     int is_imported, int variant, int is_hidden,
-                    int tests, int score,
+                    int tests, int score, int is_readonly,
                     unsigned char const *user_login)
 {
   struct prot_serve_pkt_run_info *out;
@@ -56,6 +56,7 @@ serve_clnt_edit_run(int sock_fd, int run_id, int mask,
   out->is_hidden = is_hidden;
   out->tests = tests;
   out->score = score;
+  out->is_readonly = is_readonly;
   out->user_login_len = user_login_len;
   strcpy(user_login_ptr, user_login);
 
