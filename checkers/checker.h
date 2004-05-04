29eda6dedcf821088f1a4cae0aeb2d4e5eb94222
cher <blackav@gmail.com>
2004-05-04 20:03:17 +0000

    copyright notice is added.

--- checkers/checker.h
+++ checkers/checker.h
@@ -1,6 +1,20 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
+/* Copyright (C) 2003,2004 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef NEED_CORR
 #error You must define NEED_CORR macro
 #endif /* NEED_CORR */
@@ -31,5 +45,3 @@ main(int argc, char **argv)
   checker_do_init(argc, argv, NEED_CORR, NEED_INFO, NEED_TGZ);
   return checker_main(argc, argv);
 }
-
-
