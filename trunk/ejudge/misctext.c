6aafaa3d0385084d7b1a924e081a33cfcd6095e5
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2004-05-04 21:01:20 +0000

    copyright notice is updated; "config.h" is included.

--- trunk/ejudge/misctext.c
+++ trunk/ejudge/misctext.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2000-2003 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2000-2004 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -14,6 +14,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+
+#include "config.h"
 
 #include "misctext.h"
 #include "base64.h"
