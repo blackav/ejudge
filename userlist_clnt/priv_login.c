42e5eef368802ecbbdeaf424fe547ba693d4f669
cher <blackav@gmail.com>
2004-05-04 20:42:44 +0000

    copyright notice is updated; (userlist_clnt_priv_login) a GNU C 3.3 warning about breaking strict   aliasing rules in -Wall -O2 mode is fixed.

--- userlist_clnt/priv_login.c
+++ userlist_clnt/priv_login.c
@@ -1,7 +1,7 @@
 /* -*- mode: c; coding: koi8-r -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002,2003 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2004 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -36,6 +36,7 @@ userlist_clnt_priv_login(struct userlist_clnt *clnt,
   struct userlist_pk_login_ok *in = 0;
   unsigned char *login_ptr, *passwd_ptr, *name_ptr;
   int out_size = 0, in_size = 0, r, login_len, passwd_len;
+  void *void_in = 0;
 
   if (!login || !*login) return -ULS_ERR_INVALID_LOGIN;
   if (!passwd || !*passwd) return -ULS_ERR_INVALID_PASSWORD;
@@ -57,8 +58,9 @@ userlist_clnt_priv_login(struct userlist_clnt *clnt,
   strcpy(login_ptr, login);
   strcpy(passwd_ptr, passwd);
   if ((r = userlist_clnt_send_packet(clnt, out_size, out)) < 0) return r;
-  if ((r = userlist_clnt_recv_packet(clnt, &in_size, (void**) &in)) < 0)
+  if ((r = userlist_clnt_recv_packet(clnt, &in_size, &void_in)) < 0)
     return r;
+  in = void_in;
   if (in->reply_id < 0) {
     r = in->reply_id;
     goto cleanup;
