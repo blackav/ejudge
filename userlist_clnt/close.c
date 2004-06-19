2d8930368baa8c25f626cd945180d8d80b4c9cf0
cher <blackav@gmail.com>
2004-06-19 21:58:16 +0000

    The copyright notice is updated; The `koi8-r' charset specification is removed from the file header.

--- userlist_clnt/close.c
+++ userlist_clnt/close.c
@@ -1,7 +1,7 @@
-/* -*- mode: c; coding: koi8-r -*- */
+/* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002-2003 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2004 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
