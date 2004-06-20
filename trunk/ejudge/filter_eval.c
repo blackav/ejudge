d628bb699a0bb28044c9fc8c2d6b8c261c9b6cb2
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2004-06-20 06:20:33 +0000

    The copyright notice is updated; The emacs' "Cyrillic-KOI8" language environment setting is removed.

--- trunk/ejudge/filter_eval.c
+++ trunk/ejudge/filter_eval.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002,2003 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2004 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -419,6 +419,5 @@ filter_tree_bool_eval(struct filter_env *env,
  * Local variables:
  *  compile-command: "make"
  *  c-font-lock-extra-types: ("\\sw+_t" "FILE" "va_list" "jmp_buf")
- *  eval: (set-language-environment "Cyrillic-KOI8")
  * End:
  */
