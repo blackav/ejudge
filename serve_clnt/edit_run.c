cac0cba4f3aa370dc50221d2896a623c989b4160
cher <blackav@gmail.com>
2004-09-27 19:27:45 +0000

    (serve_clnt_edit_run) the old function is renamed to the function   `do_serve_clnt_edit_run' and generalized to accept `cmd',   `run_size', `run_src', `ip' parameters; (serve_clnt_edit_run) the new function is just a wrapper around   `do_serve_clnt_edit_run'; (serve_clnt_new_run) a new function to pass to the server   `SRV_CMD_NEW_RUN' command.

--- serve_clnt/edit_run.c
+++ serve_clnt/edit_run.c
@@ -23,28 +23,43 @@
 #include <reuse/xalloc.h>
 #include <reuse/osdeps.h>
 
-int
-serve_clnt_edit_run(int sock_fd, int run_id, int mask,
-                    int user_id, int prob_id, int lang_id, int status,
-                    int is_imported, int variant, int is_hidden,
-                    int tests, int score, int is_readonly, int pages,
-                    unsigned char const *user_login)
+static int
+do_serve_clnt_edit_run(int sock_fd, int cmd, int run_id, int mask,
+                       int user_id, int prob_id, int lang_id, int status,
+                       int is_imported, int variant, int is_hidden,
+                       int tests, int score, int is_readonly, int pages,
+                       unsigned long ip, int run_size,
+                       unsigned char const *user_login,
+                       unsigned char const *run_src)
 {
   struct prot_serve_pkt_run_info *out;
   struct prot_serve_packet *in = 0;
   size_t out_size, in_size = 0, user_login_len;
-  unsigned char *user_login_ptr;
+  unsigned char *user_login_ptr, *run_src_ptr;
   int r;
   void *void_in;
 
   if (sock_fd < 0) return -SRV_ERR_NOT_CONNECTED;
   if (!user_login) user_login = "";
+  if (!run_src) {
+    run_src = "";
+    run_size = 0;
+  }
+  if (run_size < 0) {
+    err("serve_clnt_edit_run: negative source length");
+    return -SRV_ERR_PROTOCOL;
+  }
   user_login_len = strlen(user_login);
-  out_size = sizeof(*out) + user_login_len;
+  out_size = sizeof(*out) + user_login_len + run_size;
   out = alloca(out_size);
   memset(out, 0, out_size);
   user_login_ptr = out->data;
-  out->b.id = SRV_CMD_EDIT_RUN;
+  run_src_ptr = user_login_ptr + user_login_len + 1;
+  if (cmd != SRV_CMD_EDIT_RUN && cmd != SRV_CMD_NEW_RUN) {
+    err("serve_clnt_edit_run: invalid command %d", cmd);
+    return -SRV_ERR_PROTOCOL;
+  }
+  out->b.id = cmd;
   out->b.magic = PROT_SERVE_PACKET_MAGIC;
   out->run_id = run_id;
   out->mask = mask;
@@ -59,8 +74,11 @@ serve_clnt_edit_run(int sock_fd, int run_id, int mask,
   out->score = score;
   out->is_readonly = is_readonly;
   out->pages = pages;
+  out->ip = ip;
   out->user_login_len = user_login_len;
+  out->run_src_len = run_size;
   strcpy(user_login_ptr, user_login);
+  memcpy(run_src_ptr, run_src, run_size);
 
   if ((r = serve_clnt_send_packet(sock_fd, out_size, out)) < 0) {
     return r;
@@ -87,6 +105,36 @@ serve_clnt_edit_run(int sock_fd, int run_id, int mask,
   xfree(in);
   return SRV_RPL_OK;
 
+}
+
+int
+serve_clnt_edit_run(int sock_fd, int run_id, int mask,
+                    int user_id, int prob_id, int lang_id, int status,
+                    int is_imported, int variant, int is_hidden,
+                    int tests, int score, int is_readonly, int pages,
+                    unsigned char const *user_login)
+{
+  return do_serve_clnt_edit_run(sock_fd, SRV_CMD_EDIT_RUN, run_id, mask,
+                                user_id, prob_id, lang_id, status,
+                                is_imported, variant, is_hidden,
+                                tests, score, is_readonly, pages, 0, 0,
+                                user_login, 0);
+}
+
+int
+serve_clnt_new_run(int sock_fd, int mask,
+                   int user_id, int prob_id, int lang_id, int status,
+                   int is_imported, int variant, int is_hidden,
+                   int tests, int score, int is_readonly, int pages,
+                   unsigned long ip, int run_size,
+                   unsigned char const *user_login,
+                   unsigned char const *run_src)
+{
+  return do_serve_clnt_edit_run(sock_fd, SRV_CMD_NEW_RUN, 0, mask,
+                                user_id, prob_id, lang_id, status,
+                                is_imported, variant, is_hidden,
+                                tests, score, is_readonly, pages, ip,
+                                run_size, user_login, run_src);
 }
 
 /**
