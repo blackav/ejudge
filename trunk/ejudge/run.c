8e4bcfd35653575cb7099ef7cbeff06d698fdcb8
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2004-09-27 19:49:46 +0000

    (main) call of `prepare' function is updated as the function   prototype is changed.

--- trunk/ejudge/run.c
+++ trunk/ejudge/run.c
@@ -1788,7 +1788,8 @@ main(int argc, char *argv[])
   }
   if (i >= argc) goto print_usage;
 
-  if (prepare(argv[i], p_flags, PREPARE_RUN, cpp_opts) < 0) return 1;
+  if (prepare(argv[i], p_flags, PREPARE_RUN, cpp_opts, managed_mode_flag) < 0)
+    return 1;
   if (T_flag) {
     print_configuration(stdout);
     return 0;
