390f72803fbd4d5b421e5d45eb5b41b6ea9c6ea3
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2004-11-13 10:16:46 +0000

    initial revision

---
+++ trunk/ejudge/win32/cr_serialize.c
@@ -0,0 +1,48 @@
+/* -*- mode: c -*- */
+/* $Id$ */
+
+/* Copyright (C) 2004 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "cr_serialize.h"
+#include "prepare.h"
+#include "pathutl.h"
+
+#include <reuse/osdeps.h>
+#include <reuse/logger.h>
+
+int
+cr_serialize_init(void)
+{
+  SWERR(("Not implemented"));
+}
+
+int
+cr_serialize_lock(void)
+{
+  SWERR(("Not implemented"));
+}
+
+int
+cr_serialize_unlock(void)
+{
+  SWERR(("Not implemented"));
+}
+
+/**
+ * Local variables:
+ *  compile-command: "make -C .."
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ * End:
+ */
