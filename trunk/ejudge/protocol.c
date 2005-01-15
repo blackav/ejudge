d941c56fba33a54ff40b82f913be0fd467cc92cf
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-01-15 10:24:02 +0000

    copyright notice is updated; error text for TRANSIENT_RUNS error is added.

--- trunk/ejudge/protocol.c
+++ trunk/ejudge/protocol.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002-2004 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2005 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -67,6 +67,7 @@ static unsigned char const * const error_map[] =
   _("this language is disabled for this problem"),
   _("the requested file does not exist"),
   _("invalid filter expression"),
+  _("there are transient runs"),
 };
 #undef _
 
