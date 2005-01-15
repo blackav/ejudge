ec326bef8a538d3cd6d74174809089b17161921a
cher <blackav@gmail.com>
2005-01-15 08:04:30 +0000

    copyright notice is updated; (serve_clnt_master_page) added new parameter `mode_clar', which is   then passed to the server.

--- serve_clnt/master_page.c
+++ serve_clnt/master_page.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002-2004 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2005 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -47,6 +47,7 @@ serve_clnt_master_page(int sock_fd,
                        int sid_mode,
                        int first_run,
                        int last_run,
+                       int mode_clar,
                        int first_clar,
                        int last_clar,
                        unsigned char const *self_url,
@@ -90,6 +91,7 @@ serve_clnt_master_page(int sock_fd,
   out->sid_mode = sid_mode;
   out->first_run = first_run;
   out->last_run = last_run;
+  out->mode_clar = mode_clar;
   out->first_clar = first_clar;
   out->last_clar = last_clar;
   out->self_url_len = self_url_len;
