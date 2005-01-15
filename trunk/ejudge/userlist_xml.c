ee8376ce60540df5fa9b8fae9e63f8e280a1a32a
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-01-15 08:05:34 +0000

    copyright notice is updated; (parse_passwd) empty passwords are allowed, empty passwords are   always assumed to be in the plain text form.

--- trunk/ejudge/userlist_xml.c
+++ trunk/ejudge/userlist_xml.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002-2004 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2005 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -487,10 +487,13 @@ parse_passwd(char const *path, struct xml_tree *t)
     nested_tag(path, t);
     return 0;
   }
+  if (!t->text) t->text = xstrdup("");
+  /*
   if (!t->text || !*t->text) {
     empty_tag(path, t);
     return 0;
   }
+  */
 
   for (a = t->first; a; a = a->next) {
     if (a->tag != USERLIST_A_METHOD) {
@@ -515,6 +518,7 @@ parse_passwd(char const *path, struct xml_tree *t)
     }
     */
   }
+  if (!*t->text) pwd->method = USERLIST_PWD_PLAIN;
   return pwd;
 }
 
