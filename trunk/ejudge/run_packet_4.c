802618c55a06700de4e4e52ff480de121b7c6798
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-03-27 08:37:37 +0000

    basically implemented, not yet tested.

--- trunk/ejudge/run_packet_4.c
+++ trunk/ejudge/run_packet_4.c
@@ -32,6 +32,79 @@ int
 run_reply_packet_read(size_t in_size, const void *in_data,
                       struct run_reply_packet **p_out_data)
 {
+  struct run_reply_packet *pout = 0;
+  const struct run_reply_bin_packet *pin = (const struct run_reply_bin_packet*)in_data;
+  int errcode = 0, version;
+  size_t packet_len;
+
+  if (in_size != sizeof(*pin)) {
+    errcode = 1;
+    goto failed;
+  }
+  packet_len = cvt_bin_to_host_32(pin->packet_len);
+  if (packet_len != in_size) {
+    errcode = 2;
+    goto failed;
+  }
+  version = cvt_bin_to_host_32(pin->version);
+  if (version != 1) {
+    errcode = 3;
+    goto failed;
+  }
+
+  pout = (typeof(pout)) xcalloc(1, sizeof(*pout));
+
+  pout->judge_id = cvt_bin_to_host_32(pin->judge_id);
+  if (pout->judge_id < 0 || pout->judge_id > MAX_JUDGE_ID) {
+    errcode = 4;
+    goto failed;
+  }
+  pout->contest_id = cvt_bin_to_host_32(pin->contest_id);
+  if (pout->contest_id <= 0 || pout->contest_id > MAX_CONTEST_ID) {
+    errcode = 5;
+    goto failed;
+  }
+  pout->run_id = cvt_bin_to_host_32(pin->run_id);
+  if (pout->run_id < 0 || pout->run_id > MAX_RUN_ID) {
+    errcode = 6;
+    goto failed;
+  }
+  pout->status = cvt_bin_to_host_32(pin->status);
+  if (pout->status < 0 || pout->status > RUN_MAX_STATUS) {
+    errcode = 7;
+    goto failed;
+  }
+  pout->failed_test = cvt_bin_to_host_32(pin->failed_test);
+  if (pout->failed_test < -1 || pout->failed_test > MAX_FAILED_TEST) {
+    errcode = 8;
+    goto failed;
+  }
+  pout->score = cvt_bin_to_host_32(pin->score);
+  if (pout->score < 0 || pout->score > MAX_SCORE) {
+    errcode = 9;
+    goto failed;
+  }
+
+  pout->ts1 = cvt_bin_to_host_32(pin->ts1);
+  pout->ts1_us = cvt_bin_to_host_32(pin->ts1_us);
+  pout->ts2 = cvt_bin_to_host_32(pin->ts2);
+  pout->ts2_us = cvt_bin_to_host_32(pin->ts2_us);
+  pout->ts3 = cvt_bin_to_host_32(pin->ts3);
+  pout->ts3_us = cvt_bin_to_host_32(pin->ts3_us);
+  pout->ts4 = cvt_bin_to_host_32(pin->ts4);
+  pout->ts4_us = cvt_bin_to_host_32(pin->ts4_us);
+  pout->ts5 = cvt_bin_to_host_32(pin->ts5);
+  pout->ts5_us = cvt_bin_to_host_32(pin->ts5_us);
+  pout->ts6 = cvt_bin_to_host_32(pin->ts6);
+  pout->ts6_us = cvt_bin_to_host_32(pin->ts6_us);
+
+  *p_out_data = pout;
+  return 0;
+
+ failed:
+  err("run_reply_packet_read: error %d", errcode);
+  run_reply_packet_free(pout);
+  return -1;
 }
 
 /**
