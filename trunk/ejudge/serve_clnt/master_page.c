8a496426ddcbe12eb216b442b1865d4aa4e6f45b
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-05-02 16:36:08 +0000

    (serve_clnt_master_page) `sid_mode' parameter is removed since the   corresponding packet field is removed.

--- trunk/ejudge/serve_clnt/master_page.c
+++ trunk/ejudge/serve_clnt/master_page.c
@@ -44,7 +44,6 @@ serve_clnt_master_page(int sock_fd,
                        int locale_id,
                        unsigned long ip,
                        int priv_level,
-                       int sid_mode,
                        int first_run,
                        int last_run,
                        int mode_clar,
@@ -88,7 +87,6 @@ serve_clnt_master_page(int sock_fd,
   out->locale_id = locale_id;
   out->ip = ip;
   out->priv_level = priv_level;
-  out->sid_mode = sid_mode;
   out->first_run = first_run;
   out->last_run = last_run;
   out->mode_clar = mode_clar;
