b797c48d539f1514c96df18b229b524387844b83
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-05-02 14:43:10 +0000

    (run_is_team_report_available) new function prototype; (run_is_report_available) new function prototype.

--- trunk/ejudge/runlog.h
+++ trunk/ejudge/runlog.h
@@ -192,6 +192,8 @@ int run_find(int first_run, int last_run,
 int run_undo_add_record(int run_id);
 int run_is_failed_attempt(int status);
 int run_is_valid_test_status(int status);
+int run_is_team_report_available(int status);
+int run_is_report_available(int status);
 
 int run_status_to_str_short(unsigned char *buf, size_t size, int val);
 int run_str_short_to_status(const unsigned char *str, int *pr);
