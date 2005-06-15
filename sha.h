b893588ff77d521bb4f167cc8b64e323c29eaae9
cher <blackav@gmail.com>
2005-06-15 21:44:16 +0000

    documetation comments are fixed.

--- sha.h
+++ sha.h
@@ -68,7 +68,7 @@ void sha_process_bytes(const void *buffer, size_t len, struct sha_ctx *ctx);
 void sha_init_ctx(struct sha_ctx *ctx);
 
 /* Process the remaining bytes in the buffer and put result from CTX
-   in first 16 bytes following RESBUF.  The result is always in little
+   in first 20 bytes following RESBUF.  The result is always in little
    endian byte order, so that a byte-wise output yields to the wanted
    ASCII representation of the message digest.
 
@@ -76,8 +76,7 @@ void sha_init_ctx(struct sha_ctx *ctx);
    aligned for a 32 bits value.  */
 void *sha_finish_ctx(struct sha_ctx *ctx, void *resbuf);
 
-
-/* Put result from CTX in first 16 bytes following RESBUF.  The result is
+/* Put result from CTX in first 20 bytes following RESBUF.  The result is
    always in little endian byte order, so that a byte-wise output yields
    to the wanted ASCII representation of the message digest.
 
@@ -85,13 +84,12 @@ void *sha_finish_ctx(struct sha_ctx *ctx, void *resbuf);
    aligned for a 32 bits value.  */
 void *sha_read_ctx(const struct sha_ctx *ctx, void *resbuf);
 
-
-/* Compute MD5 message digest for bytes read from STREAM.  The
-   resulting message digest number will be written into the 16 bytes
+/* Compute SHA1 message digest for bytes read from STREAM.  The
+   resulting message digest number will be written into the 20 bytes
    beginning at RESBLOCK.  */
 int sha_stream(FILE *stream, void *resblock);
 
-/* Compute MD5 message digest for LEN bytes beginning at BUFFER.  The
+/* Compute SHA1 message digest for LEN bytes beginning at BUFFER.  The
    result is always in little endian byte order, so that a byte-wise
    output yields to the wanted ASCII representation of the message
    digest.  */
