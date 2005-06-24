e69050eac97bec1350b8794b3b03ad9c757cf6e4
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-06-24 20:58:52 +0000

    initial revision

---
+++ trunk/ejudge/html_start_form.c
@@ -0,0 +1,45 @@
+/* -*- mode: c -*- */
+/* $Id$ */
+
+/* Copyright (C) 2005 Alexander Chernov <cher@unicorn.cmc.msu.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "misctext.h"
+
+#include <reuse/logger.h>
+
+#include <stdio.h>
+
+static const unsigned char * const form_methods[] =
+{
+  "form method=\"GET\" action=",
+  "form method=\"POST\" ENCTYPE=\"application/x-www-form-urlencoded\" action=",
+  "form method=\"POST\" ENCTYPE=\"multipart/form-data\" action=",
+};
+
+void
+html_start_form(FILE *f, int mode, unsigned long long sid,
+                unsigned char const *self_url,
+                unsigned char const *hidden_vars)
+{
+  ASSERT(mode >= 0 && mode <= 2);
+  fprintf(f, "<%s\"%s\">%s", form_methods[mode], self_url, hidden_vars);
+}
+
+/**
+ * Local variables:
+ *  compile-command: "make"
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE" "va_list" "fd_set" "DIR")
+ * End:
+ */
