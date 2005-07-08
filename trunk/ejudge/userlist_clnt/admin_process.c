7d528f93d3ce9386db952ddc8217dcd88f9fc454
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-07-08 19:48:53 +0000

    copyright notice is updated; (userlist_clnt_admin_process) new parameters added:   p_uid - pointer where to store the returned user_id;   p_login - pointer where to store pointer to the user login string;   p_name - pointer where to store pointer to the user name string;   the returned packet is parsed and the data is stored according to   the passed parameters.

--- trunk/ejudge/userlist_clnt/admin_process.c
+++ trunk/ejudge/userlist_clnt/admin_process.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002-2004 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2005 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -18,25 +18,63 @@
 #include "userlist_clnt/private.h"
 
 int
-userlist_clnt_admin_process(struct userlist_clnt *clnt)
+userlist_clnt_admin_process(struct userlist_clnt *clnt,
+                            int *p_uid,
+                            unsigned char **p_login,
+                            unsigned char **p_name)
 {
-  struct userlist_packet *out = 0, *in = 0;
-  size_t out_size = 0, in_size = 0, r;
+  struct userlist_packet *out = 0;
+  struct userlist_pk_uid_2 *in = 0;
+  size_t out_size = 0, in_size = 0, act_login_len, act_name_len, exp_len;
+  int r;
+  void *void_in = 0;
+  unsigned char *login_ptr, *name_ptr;
 
   out_size = sizeof(*out);
   out = alloca(out_size);
   memset(out, 0, out_size);
   out->id = ULS_ADMIN_PROCESS;
   if ((r = userlist_clnt_send_packet(clnt, out_size, out)) < 0) return r;
-  if ((r = userlist_clnt_recv_packet(clnt, &in_size, (void*) &in)) < 0)
+  if ((r = userlist_clnt_recv_packet(clnt, &in_size, &void_in)) < 0)
     return r;
-  if (in_size != sizeof(*in)) {
+  in = void_in;
+  if (in_size < sizeof(*in)) {
     xfree(in);
     return -ULS_ERR_PROTOCOL;
   }
-  r = in->id;
+  if (in->reply_id < 0) {
+    r = in->reply_id;
+    xfree(in);
+    return r;
+  }
+  if (in->reply_id != ULS_UID_2) {
+    xfree(in);
+    return -ULS_ERR_PROTOCOL;
+  }
+
+  login_ptr = in->data;
+  act_login_len = strlen(login_ptr);
+  if (act_login_len != in->login_len) {
+    xfree(in);
+    return -ULS_ERR_PROTOCOL;
+  }
+  name_ptr = login_ptr + act_login_len + 1;
+  act_name_len = strlen(name_ptr);
+  if (act_name_len != in->name_len) {
+    xfree(in);
+    return -ULS_ERR_PROTOCOL;
+  }
+  exp_len = sizeof(*in) + act_login_len + act_name_len;
+  if (exp_len != in_size) {
+    xfree(in);
+    return -ULS_ERR_PROTOCOL;
+  }
+
+  if (p_uid) *p_uid = in->uid;
+  if (p_login) *p_login = strdup(login_ptr);
+  if (p_name) *p_name = strdup(name_ptr);
   xfree(in);
-  return r;
+  return ULS_UID_2;
 }
 
 /**
