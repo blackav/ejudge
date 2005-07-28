37d91b4f0a658f066e6aef101318db6d8ff4c612
cher <blackav@gmail.com>
2005-07-28 05:48:04 +0000

    (userlist_clnt_get_param) new function prototype added.

--- userlist_clnt.h
+++ userlist_clnt.h
@@ -95,6 +95,9 @@ int
 userlist_clnt_get_info(struct userlist_clnt *clnt, int cmd,
                        int uid, unsigned char **p_info);
 int
+userlist_clnt_get_param(struct userlist_clnt *clnt,
+                        int cmd, int contest_id, unsigned char **p_info);
+int
 userlist_clnt_set_info(struct userlist_clnt *clnt,
                        int uid, int contest_id, unsigned char *info);
 int
