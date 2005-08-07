379568149512695272cc718ad1b68031413aa343
cher <blackav@gmail.com>
2005-08-07 08:04:06 +0000

    (xml_parse_ip_mask) added support for default mask "0" (addr == 0   && mask == 0).

--- xml_utils/parse_ip_mask.c
+++ xml_utils/parse_ip_mask.c
@@ -18,6 +18,8 @@
 #include "xml_utils.h"
 #include "pathutl.h"
 
+#include <string.h>
+
 int
 xml_parse_ip_mask(const unsigned char *path, int line, int column,
                   const unsigned char *s,
@@ -27,7 +29,10 @@ xml_parse_ip_mask(const unsigned char *path, int line, int column,
   unsigned int b1, b2, b3, b4, b5;
 
   if (!s) goto failed;
-  if (sscanf(s, "%u.%u.%u.%u/%u %n", &b1, &b2, &b3, &b4, &b5, &n) == 5
+  if (!strcmp(s, "0")) {
+    *p_addr = 0;
+    *p_mask = 0;
+  } else if (sscanf(s, "%u.%u.%u.%u/%u %n", &b1, &b2, &b3, &b4, &b5, &n) == 5
       && !s[n] && b1 <= 255 && b2 <= 255 && b3 <= 255 && b4 <= 255 && b5 <= 32) {
     *p_addr = b1 << 24 | b2 << 16 | b3 << 8 | b4;
     *p_mask = ((unsigned int) -1) << (32 - b5);
