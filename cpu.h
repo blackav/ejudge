9ce3f938f436870191db6aee3bdc1df404774a10
cher <blackav@gmail.com>
2005-09-12 09:20:34 +0000

    initial revision

---
+++ cpu.h
@@ -0,0 +1,22 @@
+/* -*- c -*- */
+/* $Id$ */
+#ifndef __CPU_H__
+#define __CPU_H__
+
+/* Copyright (C) 2005 Alexander Chernov <cher@unicorn.cmc.msu.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+int cpu_get_bogomips(void);
+
+#endif /* __CPU_H__ */
