4bc4422989c0e0772558556311a59c42825d4cf5
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-10-29 18:16:56 +0000

    copyright notice is updated; message for the ULS_ERR_EMAIL_FAILED error is added.

--- trunk/ejudge/userlist_proto.c
+++ trunk/ejudge/userlist_proto.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002-2004 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2005 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -52,6 +52,7 @@ static unsigned char const * const error_map[] =
   _("this field cannot be changed"),
   _("contest deadline exceeded"),
   _("peer closed connection"),
+  _("e-mail sending failed"),
 
   0
 };
