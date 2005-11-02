2fa9ac01d6dfc93e974b639cbc56e48e865aa23a
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-11-02 20:05:06 +0000

    copyright notice is updated; <reuse/integral.h> is included; <sys/uio.h> is included; (userlist_clnt_send_packet) function is rewritten to avoid copying   of the buffer.

--- trunk/ejudge/userlist_clnt/send_packet.c
+++ trunk/ejudge/userlist_clnt/send_packet.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002-2004 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2005 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -17,37 +17,68 @@
 
 #include "userlist_clnt/private.h"
 
+#include <reuse/integral.h>
+
+#include <sys/uio.h>
 #include <errno.h>
 
 int
 userlist_clnt_send_packet(struct userlist_clnt *clnt,
                           size_t size, void const *buf)
 {
-  unsigned char *b;
+  const unsigned char *b;
   int w, n;
+  rint32_t size32;
+  struct iovec vv[2];
 
   ASSERT(clnt);
   ASSERT(size > 0);
   ASSERT(clnt->fd >= 0);
 
-  b = (unsigned char*) alloca(size + 4);
-  memcpy(b, &size, 4);          /* FIXME: non-portable */
-  memcpy(b + 4, buf, size);
-  w = size + 4;
-
-  while (w > 0) {
-    n = write(clnt->fd, b, w);
-    if (n <= 0) {
-      n = errno;
-      err("write() to userlist-server failed: %s", os_ErrorMsg());
-      close(clnt->fd);
-      clnt->fd = -1;
-      if (n == EPIPE) return -ULS_ERR_DISCONNECT;
-      return -ULS_ERR_WRITE_ERROR;
-    }
-    w -= n; b += n;
+  /* the packet length is 4 bytes (signed), so do some sanity check... */
+  if ((size & 0xc000000)) {
+    err("send_packet: packet length exceeds 1GiB");
+    return -ULS_ERR_WRITE_ERROR;
   }
+  size32 = (ruint32_t) size;
+
+  vv[0].iov_base = &size32;
+  vv[0].iov_len = sizeof(size32); /* 4, actually */
+  vv[1].iov_base = (void*) buf;
+  vv[1].iov_len = size;
+
+  n = writev(clnt->fd, vv, 2);
+  if (n == size + 4) return 0;
+  if (n <= 0) goto write_error;
+
+  /* if the fast method did not work out, try the slow one */
+  if (n < 4) {
+    w = 4 - n;
+    b = (const unsigned char*) size32 + n;
+    while (w > 0) {
+      if ((n = write(clnt->fd, b, w)) <= 0) goto write_error;
+      w -= n;
+      b += n;
+    }
+    n = 4;
+  }
+  w = size + 4 - n;
+  b = (const unsigned char*) buf + n - 4;
+  while (w > 0) {
+    if ((n = write(clnt->fd, b, w)) <= 0) goto write_error;
+    w -= n;
+    b += n;
+  }
+
   return 0;
+
+ write_error:
+  n = errno;
+  err("send_packet: write() failed: %s", os_ErrorMsg());
+  close(clnt->fd);
+  clnt->fd = -1;
+  if (n == EPIPE) return -ULS_ERR_DISCONNECT;
+  return -ULS_ERR_WRITE_ERROR;
 }
 
 /**
