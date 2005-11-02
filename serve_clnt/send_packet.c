5ebbae39ea19640b649e0ea9a1325727ae85b088
cher <blackav@gmail.com>
2005-11-02 21:51:29 +0000

    copyright notice is updated; <reuse/integral.h> is included; <sys/uio.h> is included; (serve_clnt_send_packet) `size' parameter now has type `size_t';   the function is rewritten to avoid copying of sending data   into a temporary buffer.

--- serve_clnt/send_packet.c
+++ serve_clnt/send_packet.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002,2004 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2005 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -22,6 +22,7 @@
 #include <reuse/logger.h>
 #include <reuse/xalloc.h>
 #include <reuse/osdeps.h>
+#include <reuse/integral.h>
 
 #include <stdio.h>
 #include <time.h>
@@ -30,31 +31,61 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/uio.h>
+#include <sys/uio.h>
 #include <unistd.h>
 #include <errno.h>
 #include <signal.h>
 
 int
-serve_clnt_send_packet(int sock_fd, int size, void const *buf)
+serve_clnt_send_packet(int sock_fd, size_t size, void const *buf)
 {
-  unsigned char *b;
+  const unsigned char *b;
   int w, n;
+  rint32_t size32;
+  struct iovec vv[2];
 
   if (sock_fd < 0) return -SRV_ERR_NOT_CONNECTED;
-  b = (unsigned char*) alloca(size + 4);
-  memcpy(b, &size, 4);
-  memcpy(b + 4, buf, size);
-  w = size + 4;
 
-  while (w > 0) {
-    n = write(sock_fd, b, w);
-    if (n <= 0) {
-      err("serve_clnt_send_packet: write() failed: %s", os_ErrorMsg());
-      return -SRV_ERR_WRITE_TO_SERVER;
-    }
-    w -= n; b += n;
+  if ((size & 0xc0000000)) {
+    err("send_packet: packet length exceeds 1GiB");
+    return -SRV_ERR_WRITE_TO_SERVER;
   }
+  size32 = (ruint32_t) size;
+
+  vv[0].iov_base = &size32;
+  vv[0].iov_len = sizeof(size32);
+  vv[1].iov_base = (void*) buf;
+  vv[1].iov_len = size;
+
+  n = writev(sock_fd, vv, 2);
+  if (n == size + 4) return 0;
+  if (n <= 0) goto write_error;
+
+  /* if the fast method did not work out, try the slow one */
+  if (n < 4) {
+    w = 4 - n;
+    b = (const unsigned char*) size32 + n;
+    while (w > 0) {
+      if ((n = write(sock_fd, b, w)) <= 0) goto write_error;
+      w -= n;
+      b += n;
+    }
+    n = 4;
+  }
+
+  w = size + 4 - n;
+  b = (const unsigned char*) buf + n - 4;
+  while (w > 0) {
+    if ((n = write(sock_fd, b, w)) <= 0) goto write_error;
+    w -= n;
+    b += n;
+  }
+
   return 0;
+
+ write_error:
+  err("serve_clnt_send_packet: write() failed: %s", os_ErrorMsg());
+  return -SRV_ERR_WRITE_TO_SERVER;
 }
 
 /**
