961da8cdf0a68edd3073f72f03fc3d224abb35d9
cher <blackav@gmail.com>
2005-11-02 20:42:49 +0000

    copyright notice is updated; (userlist_clnt_map_contest) fixes for 64-bit platforms.

--- userlist_clnt/map_contest.c
+++ userlist_clnt/map_contest.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002-2004 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2005 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -25,7 +25,8 @@ userlist_clnt_map_contest(struct userlist_clnt *clnt,
 {
   struct userlist_pk_map_contest *out = 0;
   struct userlist_pk_contest_mapped *in = 0;
-  int out_size = 0, in_size = 0, r;
+  int r;
+  size_t out_size, in_size = 0;
 
   /* FIXME: check args? */
 
