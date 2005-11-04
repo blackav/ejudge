910315d14f793bfe79f1e4954c428d4717ed7d1e
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-11-04 17:57:01 +0000

    *** empty log message ***

---
+++ trunk/ejudge/ej_limits.h
@@ -0,0 +1,35 @@
+/* -*- c -*- */
+/* $Id$ */
+#ifndef __EJ_LIMITS_H__
+#define __EJ_LIMITS_H__
+
+/* Copyright (C) 2005 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/* maximal possible number of a contest */
+#define MAX_CONTEST_ID  999999
+
+/* maximal possible number of a user */
+#define EJ_MAX_USER_ID  999999
+
+/* maximal possible number of a submit */
+#define EJ_MAX_RUN_ID   999999
+
+/* maximal possible number of a message */
+#define EJ_MAX_CLAR_ID  999999
+
+/* maximal directory depth in base32-encoding */
+#define EJ_MAX_32DIGITS 4
+
+#endif /* __EJ_LIMITS_H__ */
