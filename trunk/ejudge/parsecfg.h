7a8ca4775dfabcd6c70185a779a0d5945e91dd48
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-11-16 08:52:26 +0000

    (sarray_unparse_2) new function prototype added; (sarray_parse_2) new function prototype added.

--- trunk/ejudge/parsecfg.h
+++ trunk/ejudge/parsecfg.h
@@ -95,6 +95,8 @@ char **sarray_merge_pf(char **, char **);
 char **sarray_free(char **);
 char **sarray_merge_arr(int, char ***);
 char  *sarray_unparse(char **);
+char  *sarray_unparse_2(char **a);
 int    sarray_parse(const unsigned char *, char ***);
+int    sarray_parse_2(const unsigned char *, char ***);
 
 #endif /* __PARSECFG_H__ */
