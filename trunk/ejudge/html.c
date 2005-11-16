2226739e357f340cf3e56e10c20ed30c5e594ea5
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-11-16 15:27:32 +0000

    (do_write_kirov_standings) `write_kirov_page_table' is moved   to its proper place.

--- trunk/ejudge/html.c
+++ trunk/ejudge/html.c
@@ -2156,11 +2156,11 @@ do_write_kirov_standings(FILE *f,
 
   fputs("</table>\n", f);
 
-  write_kirov_page_table(f, total_pages, current_page, pgrefs,
-                         t_sort, tot_full, tot_score, pg_n1, pg_n2,
-                         pr_attrs, pc_attrs);
-
   if (!client_flag) {
+    write_kirov_page_table(f, total_pages, current_page, pgrefs,
+                           t_sort, tot_full, tot_score, pg_n1, pg_n2,
+                           pr_attrs, pc_attrs);
+
     if (footer_str) {
       process_template(f, footer_str, 0, 0, 0, get_copyright());
     } else {
