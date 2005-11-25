e16ed05509eecf485a9ce6f0c68cb88c89984e3b
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-11-25 14:30:04 +0000

    copyright notice is updated; "errlog.h" is included instead of "pathutl.h".

--- trunk/ejudge/userlist_cfg.c
+++ trunk/ejudge/userlist_cfg.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002,2004 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2002-2005 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -19,7 +19,7 @@
 
 #include "userlist_cfg.h"
 #include "expat_iface.h"
-#include "pathutl.h"
+#include "errlog.h"
 
 #include <reuse/xalloc.h>
 #include <reuse/logger.h>
