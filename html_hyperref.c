6986db3ae24e9ca4b2c3b1edbcc4321b2877b840
cher <blackav@gmail.com>
2005-11-25 14:33:01 +0000

    "mischtml.h" is included instead of "misctext.h".

--- html_hyperref.c
+++ html_hyperref.c
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  */
 
-#include "misctext.h"
+#include "mischtml.h"
 
 #include <stdio.h>
 #include <stdarg.h>
--- html_start_form.c
+++ html_start_form.c
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  */
 
-#include "misctext.h"
+#include "mischtml.h"
 
 #include <reuse/logger.h>
 
