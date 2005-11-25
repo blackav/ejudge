41a878cb0f907178208c7a2467e0879a62e813f3
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2005-11-25 14:08:12 +0000

    Copyright notice is updated; "pathutl.h" is replaced with "errlog.h".

--- trunk/ejudge/super_clnt/simple_cmd.c
+++ trunk/ejudge/super_clnt/simple_cmd.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2004 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2004,2005 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -17,7 +17,7 @@
 
 #include "super_clnt.h"
 #include "super_proto.h"
-#include <pathutl.h>
+#include "errlog.h"
 
 #include <stdlib.h>
 
