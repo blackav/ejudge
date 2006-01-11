406e46a3d6a4e4e4f3be68dfc371aeff9c28a38d
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2006-01-11 13:12:57 +0000

    copyright notice is updated.

--- trunk/ejudge/copyright.c
+++ trunk/ejudge/copyright.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2003-2005 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2003-2006 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -36,7 +36,7 @@ make_copyright(unsigned char *buf, size_t size)
 {
   snprintf(buf, size,
            _("<p class=\"ejudge_copyright\">This is <a href=\"%s\"><b>ejudge</b></a> contest administration system, version %s, compiled %s.</p>\n"
-           "<p class=\"ejudge_copyright\">This program is copyright (C) 2000-2005 Alexander Chernov.</p>\n"
+           "<p class=\"ejudge_copyright\">This program is copyright (C) 2000-2006 Alexander Chernov.</p>\n"
            "<p class=\"ejudge_copyright\">"
            "This program is free software; you can redistribute it and/or modify it under the terms of the <a href=\"http://www.fsf.org/licenses/licenses.html#GPL\">GNU General Public License</a> as published by the <a href=\"http://www.fsf.org\">Free Software Foundation</a>; either version 2 of the License, or (at your option) any later version.</p>\n"),
            "http://www.ejudge.ru",
