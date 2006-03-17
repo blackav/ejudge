a346a62a87e5b1d954a670ecc29de3420f8dd0f2
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2006-03-17 22:19:44 +0000

    copyright notice is updated; (sarray_unparse) function is moved to parsecfg_2.c; (sarray_unparse_2) the same as above.

--- trunk/ejudge/parsecfg.c
+++ trunk/ejudge/parsecfg.c
@@ -1,7 +1,7 @@
 /* -*- c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2000-2005 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2000-2006 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -1320,99 +1320,6 @@ char **sarray_merge_arr(int n, char ***pa)
     }
   }
   return pptr;
-}
-
-char *
-sarray_unparse(char **a)
-{
-  char *out_txt = 0;
-  unsigned char *s, *q;
-  size_t out_len = 0;
-  FILE *out;
-  int i;
-
-  out = open_memstream(&out_txt, &out_len);
-  if (a) {
-    for (i = 0; a[i]; i++) {
-      // VAR[=[value]]
-      if (i > 0) fprintf(out, " ");
-      s = a[i];
-      while (*s && (isalnum(*s) || *s == '_')) s++;
-      if (*s && *s != '=') {
-        // invalid variable name
-        fprintf(out, "invalid_variable_name=");
-        s = a[i];
-      } else if (*s == '=' && (char*) s == a[i]) {
-        fprintf(out, "empty_variable_name=");
-        s++;
-      } else {
-        s = a[i];
-        while (*s && *s != '=') putc_unlocked(*s++, out);
-        if (*s == '=') putc_unlocked(*s++, out);
-      }
-      q = s;
-      while (*q && *q > ' ' && *q < 127 && *q != '\"' && *q != '\\') q++;
-      if (*q) {
-        putc_unlocked('\"', out);
-        for (; *s; s++) {
-          if (*s < ' ') {
-            fprintf(out, "\\%03o", *s);
-          } else if (*s == '\"') {
-            fputs("\\\"", out);
-          } else if (*s == '\\') {
-            fputs("\\\\", out);
-          } else {
-            putc_unlocked(*s, out);
-          }
-        }
-        putc_unlocked('\"', out);
-      } else {
-        while (*s) putc_unlocked(*s++, out);
-      }
-    }
-  }
-  fclose(out);
-  return out_txt;
-}
-
-char *
-sarray_unparse_2(char **a)
-{
-  char *out_txt = 0;
-  unsigned char *s, *q;
-  size_t out_len = 0;
-  FILE *out;
-  int i;
-
-  out = open_memstream(&out_txt, &out_len);
-  if (a) {
-    for (i = 0; a[i]; i++) {
-      // VAR[=[value]]
-      if (i > 0) fprintf(out, " ");
-      s = a[i];
-      q = s;
-      while (*q && *q > ' ' && *q < 127 && *q != '\"' && *q != '\\') q++;
-      if (*q) {
-        putc_unlocked('\"', out);
-        for (; *s; s++) {
-          if (*s < ' ') {
-            fprintf(out, "\\%03o", *s);
-          } else if (*s == '\"') {
-            fputs("\\\"", out);
-          } else if (*s == '\\') {
-            fputs("\\\\", out);
-          } else {
-            putc_unlocked(*s, out);
-          }
-        }
-        putc_unlocked('\"', out);
-      } else {
-        while (*s) putc_unlocked(*s++, out);
-      }
-    }
-  }
-  fclose(out);
-  return out_txt;
 }
 
 int
