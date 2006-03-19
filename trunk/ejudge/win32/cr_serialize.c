17365801d602a94210bf9789a39544ee8f070792
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2006-03-19 09:39:40 +0000

    copyright notice is updated;

--- trunk/ejudge/win32/cr_serialize.c
+++ trunk/ejudge/win32/cr_serialize.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2004,2005 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2004-2006 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
