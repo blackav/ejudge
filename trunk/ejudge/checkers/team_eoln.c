7cbb445243f2f9df15ee5515c474e2afe4e3b1c3
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2006-04-07 07:49:08 +0000

    initial revision

---
+++ trunk/ejudge/checkers/team_eoln.c
@@ -0,0 +1,41 @@
+/* -*- mode: c -*- */
+/* $Id$ */
+
+/* Copyright (C) 2006 Alexander Chernov <cher@ispras.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "checker_internal.h"
+
+void
+checker_team_eoln(int lineno)
+{
+  int c;
+
+  c = getc(f_team);
+  while (c != EOF && c != '\n' && isspace(c)) c = getc(f_team);
+  if (c != EOF && c != '\n') {
+    if (lineno > 0) {
+      fatal_PE("output: end-of-line expected at line %d", lineno);
+    } else {
+      fatal_PE("output: end-of-line expected");
+    }
+  }
+}
+
+/*
+ * Local variables:
+ *  compile-command: "make"
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ * End:
+ */
