66607a66d02f2cc6b894fa266668d2c176c820bc
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2006-05-09 06:28:32 +0000

    copyright notice is updated; (checker_eq_double) Win32 (MinGW) version is implemented.

--- trunk/ejudge/checkers/eq_double.c
+++ trunk/ejudge/checkers/eq_double.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2005 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2005,2006 Alexander Chernov <cher@ispras.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -17,9 +17,47 @@
 
 #include "checker_internal.h"
 
+/*
+ * 63 - sign bit
+ * 52-62 - exponent
+ * 0-51 - mantissa
+ */
 int
 checker_eq_double(double v1, double v2, double eps)
 {
+#if defined __MINGW32__
+#define checker_is_nan(x) (((x) & 0x7ff0000000000000LL) == 0x7ff0000000000000LL && ((x) & 0x000fffffffffffffLL) != 0)
+#define checker_is_inf(x) (((x) & 0x7ff0000000000000LL) == 0x7ff0000000000000LL && ((x) & 0x000fffffffffffffLL) == 0)
+  long long vv1 = *(long long*) &v1;
+  long long vv2 = *(long long*) &v2;
+  long long p1, p2, p;
+
+  if (checker_is_nan(vv1) && checker_is_nan(vv2)) return 1;
+  if (checker_is_nan(vv1) || checker_is_nan(vv2)) return 0;
+  if (checker_is_inf(vv1) && checker_is_inf(vv2)) {
+    if ((vv1 ^ vv2) < 0) return 0;
+    return 1;
+  }
+  if (checker_is_inf(vv1) || checker_is_inf(vv2)) return 0;
+  if (fabs(v1) <= 1.0 && fabs(v2) <= 1.0) {
+    if (fabs(v1 - v2) <= 2*eps) return 1;
+    return 0;
+  }
+  if (!v1 || !v2) return 0;
+  if ((vv1 ^ vv2) < 0) return 0;
+  vv1 &= 0x7fffffffffffffffLL;
+  vv2 &= 0x7fffffffffffffffLL;
+  p = p1 = vv1 & 0x7ff0000000000000LL;
+  if (p > (p2 = vv2 & 0x7ff0000000000000LL)) p = p2;
+  if (p1 - p > 0x0010000000000000LL || p2 - p > 0x001000000000000LL)
+    return 0;
+  vv1 -= (p - 0x3ff0000000000000LL);
+  vv2 -= (p - 0x3ff0000000000000LL);
+  v1 = *(double*) &vv1;
+  v2 = *(double*) &vv2;
+  if (fabs(v1 - v2) <= 2*eps) return 1;
+  return 0;
+#else
   double m1, m2;
   int e1, e2, em;
 
@@ -46,4 +84,5 @@ checker_eq_double(double v1, double v2, double eps)
   m2 = ldexp(m2, e2);
   if (fabs(m1 - m2) <= 2*eps) return 1;
   return 0;
+#endif
 }
