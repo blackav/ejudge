1b449961fedb56ba13bd833d28b2484e170048de
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2006-06-02 16:00:44 +0000

    SSERV_CMD_PROB_CHANGE_SCORING_CHECKER handled.

--- trunk/ejudge/super-serve.c
+++ trunk/ejudge/super-serve.c
@@ -2616,6 +2616,7 @@ cmd_set_value(struct client_state *p, int len,
   case SSERV_CMD_PROB_CLEAR_LONG_NAME:
   case SSERV_CMD_PROB_CHANGE_SUPER:
   case SSERV_CMD_PROB_CHANGE_OUTPUT_ONLY:
+  case SSERV_CMD_PROB_CHANGE_SCORING_CHECKER:
   case SSERV_CMD_PROB_CHANGE_USE_STDIN:
   case SSERV_CMD_PROB_CHANGE_USE_STDOUT:
   case SSERV_CMD_PROB_CHANGE_BINARY_INPUT:
@@ -3118,6 +3119,7 @@ static const struct packet_handler packet_handlers[SSERV_CMD_LAST] =
   [SSERV_CMD_PROB_CLEAR_LONG_NAME] = { cmd_set_value },
   [SSERV_CMD_PROB_CHANGE_SUPER] = { cmd_set_value },
   [SSERV_CMD_PROB_CHANGE_OUTPUT_ONLY] = { cmd_set_value },
+  [SSERV_CMD_PROB_CHANGE_SCORING_CHECKER] = { cmd_set_value },
   [SSERV_CMD_PROB_CHANGE_USE_STDIN] = { cmd_set_value },
   [SSERV_CMD_PROB_CHANGE_USE_STDOUT] = { cmd_set_value },
   [SSERV_CMD_PROB_CHANGE_BINARY_INPUT] = { cmd_set_value },
