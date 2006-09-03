5980c4ef6c2d9a92d2780e648a91512d103a7c51
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2006-09-03 15:03:45 +0000

    initial revision

---
+++ trunk/ejudge/userlist_clnt/get_fd.c
@@ -0,0 +1,32 @@
+/* -*- mode: c -*- */
+/* $Id$ */
+
+/* Copyright (C) 2006 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "userlist_clnt/private.h"
+
+long
+userlist_clnt_get_fd(struct userlist_clnt *clnt)
+{
+  if (!clnt) return -1;
+  return clnt->fd;
+}
+
+/*
+ * Local variables:
+ *  compile-command: "make -C .."
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ * End:
+ */
