f98545df3fbb931cf8f7f9e50ad305351ed5701e
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2006-11-17 12:42:42 +0000

    * files.make: `new_server_clnt/control.c' is added.
    * Makefile.unix.in:
    deps for new-server-control.c are fixed.
    
    * new_server_proto.h:
    `NEW_SRV_CMD_STOP', `NEW_SRV_CMD_RESTART' added.
    
    * new_server_clnt.h:
    (new_server_clnt_control) new function prototype.
    
    * new-server.c:
    (cmd_control) STOP and RESTART commands are handled.
    
    * new-server-control.c: basically completed.

--- trunk/ejudge/new-server-control.c
+++ trunk/ejudge/new-server-control.c
@@ -18,16 +18,135 @@
 #include "config.h"
 #include "settings.h"
 #include "ej_types.h"
+#include "version.h"
 
 #include "ejudge_cfg.h"
+#include "new_server_proto.h"
+#include "new_server_clnt.h"
+
+#include <reuse/osdeps.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
 
 /*
  * usage: new-server-control COMMAND CONFIG
  *   COMMAND is one of `stop', `restart', `status'
  */
 
+static const unsigned char *program_name = "";
+
+static void startup_error(const char *format, ...)
+  __attribute__((format(printf, 1, 2), noreturn));
+static void
+startup_error(const char *format, ...)
+{
+  va_list args;
+  char buf[1024];
+
+  va_start(args, format);
+  vsnprintf(buf, sizeof(buf), format, args);
+  va_end(args);
+
+  fprintf(stderr, "%s: %s\n  Use --help option for help.\n", program_name,
+          buf);
+  exit(1);
+}
+
+static void op_error(const char *format, ...)
+  __attribute__((format(printf, 1, 2), noreturn));
+static void
+op_error(const char *format, ...)
+{
+  va_list args;
+  char buf[1024];
+
+  va_start(args, format);
+  vsnprintf(buf, sizeof(buf), format, args);
+  va_end(args);
+
+  fprintf(stderr, "%s: %s\n", program_name, buf);
+  exit(1);
+}
+
+static void write_help(void) __attribute__((noreturn));
+static void
+write_help(void)
+{
+  printf("%s: new-server control utility\n"
+         "Usage: %s [OPTIONS] COMMAND [EJUDGE-XML-PATH]\n"
+         "  OPTIONS:\n"
+         "    --help    write message and exit\n"
+         "    --version report version and exit\n"
+         "  COMMAND:\n"
+         "    stop      stop the new-server\n"
+         "    restart   restart the new-server\n"
+         /*"    status    report the new-server status\n"*/,
+         program_name, program_name);
+  exit(0);
+}
+static void write_version(void) __attribute__((noreturn));
+static void
+write_version(void)
+{
+  printf("%s %s, compiled %s\n", program_name, compile_version, compile_date);
+  exit(0);
+}
+
 int
 main(int argc, char *argv[])
 {
+  int i = 1, r = 0;
+  const char *command = 0;
+  const char *ejudge_xml_path = 0;
+  struct ejudge_cfg *config = 0;
+  int cmd = 0;
+  new_server_conn_t conn = 0;
+
+  program_name = os_GetBasename(argv[0]);
+  if (argc < 2) startup_error("not enough parameters");
+
+  if (!strcmp(argv[i], "--help")) {
+    write_help();
+  } else if (!strcmp(argv[i], "--version")) {
+    write_version();
+  }
+
+  command = argv[i];
+  i++;
+
+  if (i < argc) {
+    ejudge_xml_path = argv[i];
+    i++;
+  }
+
+  if (i < argc) startup_error("too many parameters");
+
+#if defined EJUDGE_XML_PATH
+  if (!ejudge_xml_path) ejudge_xml_path = EJUDGE_XML_PATH;
+#endif /* EJUDGE_XML_PATH */
+
+  if (!ejudge_xml_path) startup_error("ejudge.xml path is not specified");
+
+  if (!(config = ejudge_cfg_parse(ejudge_xml_path))) return 1;
+
+  if (!strcmp(command, "stop")) {
+    cmd = NEW_SRV_CMD_STOP;
+  } else if (!strcmp(command, "restart")) {
+    cmd = NEW_SRV_CMD_RESTART;
+  } else {
+    startup_error("invalid command");
+  }
+
+  r = new_server_clnt_open("/tmp/new-server-socket", &conn);
+  if (r == -NEW_SRV_ERR_CONNECT_FAILED) op_error("new-server is not running");
+  if (r < 0) op_error("%s", ns_strerror(-r));
+
+  r = new_server_clnt_control(conn, cmd);
+  if (r < 0) op_error("%s", ns_strerror(-r));
+
   return 0;
 }
