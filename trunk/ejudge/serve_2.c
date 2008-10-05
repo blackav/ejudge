34a315d84c4c258a9ff031b439390472add4e873
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2008-10-05 14:25:36 +0000

    * serve_2.c: (serve_run_request) `disable_security' problem configuration variable is supported.

--- trunk/ejudge/serve_2.c
+++ trunk/ejudge/serve_2.c
@@ -1189,6 +1189,7 @@ serve_run_request(serve_state_t state,
   run_pkt->full_archive = state->global->enable_full_archive;
   run_pkt->memory_limit = state->global->enable_memory_limit_error;
   run_pkt->secure_run = state->global->secure_run;
+  if (run_pkt->secure_run && prob->disable_security) run_pkt->secure_run = 0;
   run_pkt->security_violation = state->global->detect_violations;
   get_current_time(&run_pkt->ts4, &run_pkt->ts4_us);
   if (comp_pkt) {
