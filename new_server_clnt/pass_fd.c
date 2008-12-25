c102cf5504f079575755b1ce2cfa2e5bb8e56db0
cher <blackav@gmail.com>
2008-12-25 21:01:01 +0000

    * open.c: copyright notice is updated; "sock_op.h" is included; (new_server_clnt_open) `sock_op_enable_creds', `sock_op_put_creds'   are used.
    * pass_fd.c: copyright notice is updated;
    "sock_op.h" is included;
    (new_server_clnt_pass_fd) `sock_op_put_fds' is used.

--- new_server_clnt/pass_fd.c
+++ new_server_clnt/pass_fd.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2006 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2006-2008 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -16,28 +16,23 @@
  */
 
 #include "new_server_clnt/new_server_clnt_priv.h"
+#include "sock_op.h"
 #include "new_server_proto.h"
 #include "errlog.h"
 
 #include <reuse/osdeps.h>
 
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 
 int
-new_server_clnt_pass_fd(new_server_conn_t conn, int fds_num, const int *fds)
+new_server_clnt_pass_fd(
+        new_server_conn_t conn,
+        int fds_num,
+        const int *fds)
 {
   struct new_server_prot_packet *out = 0;
   int out_size = 0;
   int r;
-
-  struct msghdr msg;
-  unsigned char msgbuf[512];
-  struct cmsghdr *pmsg;
-  struct iovec send_vec[1];
-  int *fd2;
-  int arrsize, val, ret;
 
   if (!conn || conn->fd < 0) return -NEW_SRV_ERR_NOT_CONNECTED;
 
@@ -46,30 +41,8 @@ new_server_clnt_pass_fd(new_server_conn_t conn, int fds_num, const int *fds)
   out->id = NEW_SRV_CMD_PASS_FD;
   out->magic = NEW_SERVER_PROT_PACKET_MAGIC;
   if ((r = new_server_clnt_send_packet(conn, out_size, out)) < 0) return r;
-
-  memset(&msg, 0, sizeof(msg));
-  msg.msg_control = msgbuf;
-  msg.msg_controllen = sizeof(msgbuf);
-  arrsize = sizeof(int) * fds_num;
-  pmsg = CMSG_FIRSTHDR(&msg);
-  fd2 = (int*) CMSG_DATA(pmsg);
-  memcpy(fd2, fds, arrsize);
-  pmsg->cmsg_level = SOL_SOCKET;
-  pmsg->cmsg_type = SCM_RIGHTS;
-  pmsg->cmsg_len = CMSG_LEN(arrsize);
-  msg.msg_controllen = CMSG_SPACE(arrsize);
-  send_vec[0].iov_base = &val;
-  send_vec[0].iov_len = 4;
-  msg.msg_iov = send_vec;
-  msg.msg_iovlen = 1;
-  val = 0;
-  ret = sendmsg(conn->fd, &msg, 0);
-  if (ret < 0) {
-    err("new_server_clnt_pass_fd: sendmsg() failed: %s", os_ErrorMsg());
-    return -NEW_SRV_ERR_WRITE_ERROR;
-  }
-  if (ret != 4) {
-    err("new_server_clnt_pass_fd: sendmsg() short write: %d bytes", ret);
+  if (sock_op_put_fds(conn->fd, fds_num, fds) < 0) {
+    err("new_server_clnt_pass_fd: sock_op_put_fds failed");
     return -NEW_SRV_ERR_WRITE_ERROR;
   }
   return 0;
