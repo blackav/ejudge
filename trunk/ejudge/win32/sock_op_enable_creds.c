f3caaaa082a2616192419197c5e8df0e236e7606
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2008-12-25 20:47:48 +0000

    initial revision

---
+++ trunk/ejudge/win32/sock_op_enable_creds.c
@@ -0,0 +1,40 @@
+/* -*- mode: c -*- */
+/* $Id$ */
+
+/* Copyright (C) 2008 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "sock_op.h"
+
+#include "errlog.h"
+
+#include <reuse/osdeps.h>
+
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int
+sock_op_enable_creds(int sock_fd)
+{
+  abort();
+}
+
+/*
+ * Local variables:
+ *  compile-command: "make -C .."
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ * End:
+ */
---
+++ trunk/ejudge/win32/sock_op_get_fds.c
@@ -0,0 +1,38 @@
+/* -*- mode: c -*- */
+/* $Id$ */
+
+/* Copyright (C) 2008 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "sock_op.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int
+sock_op_get_fds(
+	int sock_fd,
+        int fds_num,
+        int *fds)
+{
+}
+
+/*
+ * Local variables:
+ *  compile-command: "make -C .."
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ * End:
+ */
