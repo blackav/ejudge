ad91d86c26f3e43b219ebe690bf0b092f31bcc01
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2009-05-02 16:06:34 +0000

    initial revision

---
+++ trunk/ejudge/win32/file_perms.c
@@ -0,0 +1,38 @@
+/* -*- mode: c -*- */
+/* $Id$ */
+
+/* Copyright (C) 2009 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "file_perms.h"
+#include "misctext.h"
+
+int
+file_perms_parse_mode(const unsigned char *mode)
+{
+  return -1;
+}
+
+int
+file_perms_parse_group(const unsigned char *group)
+{
+  return -1;
+}
+
+/*
+ * Local variables:
+ *  compile-command: "make -C .."
+ *  c-font-lock-extra-types: ("\\sw+_t" "FILE")
+ * End:
+ */
