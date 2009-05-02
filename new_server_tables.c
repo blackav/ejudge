5876e0b71c30d6ca114d65e1e8ebec1660fd1c0d
cher <blackav@gmail.com>
2009-05-02 18:56:49 +0000

    * new-server.h:  `SUBMIT_RUN_COMMENT_AND_OK' command is added.
    * new_server_html.c:
    (priv_submit_run_comment) `SUBMIT_RUN_COMMENT_AND_OK' command is supported.
    
    * new_server_html_2.c:
    (ns_write_priv_source) binary runs may also be commented;
      `SUBMIT_RUN_COMMENT_AND_OK' command is supported.
    
    * new_server_tables.c:
    `SUBMIT_RUN_COMMENT_AND_OK' command is supported.

--- new_server_tables.c
+++ new_server_tables.c
@@ -171,6 +171,7 @@ const unsigned char * const ns_submit_button_labels[NEW_SRV_ACTION_LAST]=
   [NEW_SRV_ACTION_SET_PRIORITIES] = __("Set priorities"),
   [NEW_SRV_ACTION_PRIV_SUBMIT_RUN_COMMENT_AND_IGNORE] = __("Send run comment and ignore run"),
   [NEW_SRV_ACTION_CHANGE_FINISH_TIME] = __("Change"),
+  [NEW_SRV_ACTION_PRIV_SUBMIT_RUN_COMMENT_AND_OK] = __("Send run comment and OK run"),
 };
 
 const int ns_priv_next_state[NEW_SRV_ACTION_LAST] =
