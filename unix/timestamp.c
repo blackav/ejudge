851e85aff934e393e9c425f0b9e60dcbc95ebe06
cher <blackav@gmail.com>
2011-03-26 09:15:37 +0000

    copyright notice is updated; "reuse_xalloc.h" is included instead of <reuse/xalloc.h>.

--- unix/timestamp.c
+++ unix/timestamp.c
@@ -1,7 +1,7 @@
 /* -*- c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2005 Alexander Chernov <cher@ispras.ru> */
+/* Copyright (C) 2005-2011 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -17,7 +17,8 @@
 
 #include "timestamp.h"
 
-#include <reuse/xalloc.h>
+#include "reuse_xalloc.h"
+
 #include <reuse/logger.h>
 
 #include <time.h>
