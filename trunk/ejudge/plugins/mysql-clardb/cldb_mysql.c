9ab9f8f78023e872c98d60e00b59aa18de362e29
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2011-03-26 09:10:55 +0000

    copyright notice is updated; "reuse_xalloc.h" is included instead of <reuse/xalloc.h>.

--- trunk/ejudge/plugins/mysql-clardb/cldb_mysql.c
+++ trunk/ejudge/plugins/mysql-clardb/cldb_mysql.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2008-2009 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2008-2011 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -28,7 +28,8 @@
 #include "prepare.h"
 #include "compat.h"
 
-#include <reuse/xalloc.h>
+#include "reuse_xalloc.h"
+
 #include <reuse/logger.h>
 
 #include <mysql.h>
