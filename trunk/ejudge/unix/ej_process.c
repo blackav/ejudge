4749c770477a55b91bbf3ae9a3b5927a1ae27b39
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2011-03-26 09:15:37 +0000

    copyright notice is updated; "reuse_xalloc.h" is included instead of <reuse/xalloc.h>.

--- trunk/ejudge/unix/ej_process.c
+++ trunk/ejudge/unix/ej_process.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2005-2010 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2005-2011 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -18,7 +18,7 @@
 #include "ej_process.h"
 #include "compat.h"
 
-#include <reuse/xalloc.h>
+#include "reuse_xalloc.h"
 
 #include <stdio.h>
 #include <string.h>
--- trunk/ejudge/unix/fileutl.c
+++ trunk/ejudge/unix/fileutl.c
@@ -1,7 +1,7 @@
 /* -*- c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2000-2010 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2000-2011 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or
@@ -23,9 +23,10 @@
 #include "errlog.h"
 #include "ej_limits.h"
 
+#include "reuse_xalloc.h"
+
 #include <reuse/logger.h>
 #include <reuse/osdeps.h>
-#include <reuse/xalloc.h>
 
 #include <unistd.h>
 #include <string.h>
