76b2ec27214a349e7faca2a75442d68371db1ee0
cher <blackav@gmail.com>
2011-03-26 09:11:17 +0000

    copyright notice is updated; "reuse_xalloc.h" is included instead of <reuse/xalloc.h>.

--- plugins/mysql-common/common_mysql.c
+++ plugins/mysql-common/common_mysql.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2008 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2008-2011 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -22,7 +22,8 @@
 #include "pathutl.h"
 #include "errlog.h"
 
-#include <reuse/xalloc.h>
+#include "reuse_xalloc.h"
+
 #include <reuse/logger.h>
 #include <reuse/osdeps.h>
 
