c3d502750266e7b47d0aa1ed040daea708e256bb
cher <blackav@gmail.com>
2011-03-27 07:01:28 +0000

    copyright notice is updated; "reuse_osdeps.h" is included instead of <reuse/osdeps.h>.

--- unix/sock_op_get_fds.c
+++ unix/sock_op_get_fds.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2008 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2008-2011 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -19,7 +19,7 @@
 
 #include "errlog.h"
 
-#include <reuse/osdeps.h>
+#include "reuse_osdeps.h"
 
 #include <string.h>
 #include <stdlib.h>
--- unix/sock_op_put_creds.c
+++ unix/sock_op_put_creds.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2008-2009 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2008-2011 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -23,7 +23,7 @@
 #if defined PYTHON
 #include <Python.h>
 #else
-#include <reuse/osdeps.h>
+#include "reuse_osdeps.h"
 #endif
 
 #include <string.h>
