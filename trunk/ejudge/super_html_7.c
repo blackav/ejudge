64d658d0caab92ee376ef555ea4e96abae7938d1
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2011-07-15 19:55:49 +0000

    * serve_state.h: (struct serve_state) field 'contest_id' is added.
    * meta_generic.c:
    (meta_destroy_fields) fixed.
    
    * super_html_7.c, super_proto.c: progress...
    
    * super-serve.c:
    (sid_state_find) parameter 'remote_addr' is added.

--- trunk/ejudge/super_html_7.c
+++ trunk/ejudge/super_html_7.c
@@ -19,7 +19,95 @@
 #include "version.h"
 #include "ej_limits.h"
 
+#include "super-serve.h"
+#include "super_proto.h"
+#include "super_html.h"
+#include "ejudge_cfg.h"
+#include "contests.h"
+#include "mischtml.h"
+#include "xml_utils.h"
+
+#define ARMOR(s)  html_armor_buf(&ab, (s))
+#define FAIL(c) do { retval = -(c); goto cleanup; } while (0)
+
 void
 super_html_7_force_link()
 {
+}
+
+static int
+get_full_caps(const struct super_http_request_info *phr, const struct contest_desc *cnts, opcap_t *pcap)
+{
+  opcap_t caps1 = 0, caps2 = 0;
+
+  opcaps_find(&phr->config->capabilities, phr->login, &caps1);
+  opcaps_find(&cnts->capabilities, phr->login, &caps2);
+  *pcap = caps1 | caps2;
+  return 0;
+}
+
+static int
+check_other_editors(
+        FILE *log_f,
+        FILE *out_f,
+        struct super_http_request_info *phr,
+        int contest_id)
+{
+  unsigned char buf[1024];
+  unsigned char hbuf[1024];
+  const unsigned char *cl = " class=\"b0\"";
+
+  // check if this contest is already edited by anybody else
+  const struct sid_state *other_session = super_serve_sid_state_get_cnts_editor(contest_id);
+  if (other_session) {
+    snprintf(buf, sizeof(buf), "serve-control: %s, the contest is being edited by someone else",
+             phr->html_name);
+    ss_write_html_header(out_f, phr, buf, 0, NULL);
+    fprintf(out_f, "<h1>%s</h1>\n", buf);
+
+    fprintf(out_f, "<ul>");
+    fprintf(out_f, "<li>%s%s</a></li>",
+            html_hyperref(hbuf, sizeof(hbuf), phr->session_id, phr->self_url,
+                          NULL, NULL),
+            "Main page");
+    fprintf(out_f, "</ul>\n");
+
+    fprintf(out_f, "<p>This contest is being edited by another user or in another session.</p>");
+    fprintf(out_f, "<table%s><tr><td%s>%s</td><td%s>%016llx</td></tr>",
+            cl, cl, "Session", cl, other_session->sid);
+    fprintf(out_f, "<tr><td%s>%s</td><td%s>%s</td></tr>",
+            cl, "IP address", cl, xml_unparse_ip(other_session->remote_addr));
+    fprintf(out_f, "<tr><td%s>%s</td><td%s>%s</td></tr></table>\n",
+            cl, "User login", cl, other_session->user_login);
+    ss_write_html_footer(out_f);
+    return -1;
+  }
+
+  // check if this contest is already opened for test editing by anybody else
+  return 0;
+}
+
+int
+super_serve_op_TESTS_MAIN_PAGE(
+        FILE *log_f,
+        FILE *out_f,
+        struct super_http_request_info *phr)
+{
+  int retval = 0;
+  int contest_id = 0;
+  const struct contest_desc *cnts = NULL;
+  opcap_t caps = 0;
+
+  ss_cgi_param_int_opt(phr, "contest_id", &contest_id, 0);
+  if (contest_id <= 0) FAIL(S_ERR_INV_CONTEST);
+  if (contests_get(contest_id, &cnts) < 0 || !cnts) FAIL(S_ERR_INV_CONTEST);
+
+  if (phr->priv_level < PRIV_LEVEL_JUDGE) FAIL(S_ERR_PERM_DENIED);
+  get_full_caps(phr, cnts, &caps);
+  if (opcaps_check(caps, OPCAP_CONTROL_CONTEST) < 0) FAIL(S_ERR_PERM_DENIED);
+
+  if (check_other_editors(log_f, out_f, phr, contest_id) < 0) goto cleanup;
+
+cleanup:
+  return retval;
 }
