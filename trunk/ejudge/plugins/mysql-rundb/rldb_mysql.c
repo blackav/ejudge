5922a5a028fbdcb63d9cafba423aea916daaccf9
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2012-02-07 14:44:54 +0000

    * rldb_mysql.c: copyright notice is updated; gcc 4.6 warning fixed.

--- trunk/ejudge/plugins/mysql-rundb/rldb_mysql.c
+++ trunk/ejudge/plugins/mysql-rundb/rldb_mysql.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2008-2011 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2008-2012 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -1065,6 +1065,7 @@ add_entry_func(
   ASSERT(de->run_id == run_id);
   ASSERT(de->status == RUN_EMPTY);
   ASSERT(de->time > 0);
+  (void) de;
 
   return do_update_entry(cs, run_id, re, flags);
 }
