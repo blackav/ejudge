2cfeba6a3bcf242b3beae22a5364eeff10e04a36
cher <blackav@gmail.com>
2012-04-10 14:11:01 +0000

    initial revision

---
+++ problem_config.c
@@ -0,0 +1,112 @@
+/* -*- c -*- */
+/* $Id$ */
+
+/* Copyright (C) 2012 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "problem_config.h"
+#include "problem_config_meta.h"
+
+#include "meta_generic.h"
+#include "reuse_xalloc.h"
+
+#include <limits.h>
+
+void
+problem_config_section_init(struct generic_section_config *gp)
+{
+  for (int field_id = META_PROBLEM_CONFIG_SECTION_manual_checking;
+       field_id < META_PROBLEM_CONFIG_SECTION_test_checker_env;
+       ++field_id) {
+    // 'B' - ejintbool_t
+    // 'i' - int
+    // 't' - time_t
+    // 'Z' - size_t
+    // 's' - unsigned char *
+    // 'x' - char **
+    // 'X' - ejenvlist_t
+    int type_id = meta_problem_config_section_get_type(field_id);
+    void *vp = meta_problem_config_section_get_ptr_nc((struct problem_config_section*) gp, field_id);
+    if (type_id == 'B') {
+      *(ejintbool_t *) vp = -1;
+    } else if (type_id == 'Z') {
+      *(size_t *) vp = ~((size_t) 0);
+    } else if (type_id == 'i') {
+      int *ip = (int*) vp;
+      if (field_id == META_PROBLEM_CONFIG_SECTION_priority_adjustment) {
+        *ip = INT_MIN;
+      } else {
+        *ip = -1;
+      }
+    }
+  }
+}
+
+struct problem_config_section *
+problem_config_section_alloc(void)
+{
+  struct problem_config_section *p = NULL;
+  XCALLOC(p, 1);
+  problem_config_section_init((struct generic_section_config *) p);
+  return p;
+}
+
+void
+problem_config_section_free(struct generic_section_config *gp)
+{
+  if (gp) {
+    meta_destroy_fields(&meta_problem_config_section_methods, gp);
+    xfree(gp);
+  }
+}
+
+static struct config_section_info problem_config_section_info[] =
+{
+  { "problem", sizeof(struct problem_config_section), NULL, NULL,
+    problem_config_section_init, problem_config_section_free,
+    &meta_problem_config_section_methods },
+
+  { NULL, 0 },
+};
+
+struct problem_config_section *
+problem_config_section_parse_cfg(const unsigned char *path, FILE *f)
+{
+  struct generic_section_config *cfg = parse_param(path, f, problem_config_section_info, 1, 0, 0, NULL);
+  return (struct problem_config_section *) cfg;
+}
+
+struct problem_config_section *
+super_run_in_packet_parse_cfg_str(const unsigned char *path, char *buf, size_t size)
+{
+  FILE *f = fmemopen(buf, size, "r");
+  if (!f) return NULL;
+  // FIXME: parse_param closes 'f'
+  return problem_config_section_parse_cfg(path, f);
+}
+
+static struct problem_config_section default_values;
+
+void
+super_run_in_packet_unparse_cfg(FILE *out_f, const struct problem_config_section *p)
+{
+  if (default_values.manual_checking >= 0) {
+    problem_config_section_init((struct generic_section_config *) p);
+  }
+  if (p) {
+    fprintf(out_f, "# -*- coding: utf-8 -*-\n\n");
+    fprintf(out_f, "\n[problem]\n\n");
+    meta_unparse_cfg(out_f, &meta_problem_config_section_methods, p, &default_values);
+  }
+}
---
+++ problem_config.h
@@ -0,0 +1,185 @@
+/* -*- c -*- */
+/* $Id$ */
+#ifndef __PROBLEM_CONFIG_H__
+#define __PROBLEM_CONFIG_H__
+
+/* Copyright (C) 2012 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+#include "ej_types.h"
+#include "parsecfg.h"
+
+#include <time.h>
+
+#ifndef META_ATTRIB
+#if defined __RCC__
+#undef __attribute__
+#define META_ATTRIB(x) __attribute__(x)
+#else
+#define META_ATTRIB(x)
+#endif /* __RCC__ */
+#endif /* META_ATTRIB */
+
+struct problem_config_section
+{
+  struct generic_section_config g META_ATTRIB((meta_hidden));
+
+  ejintbool_t manual_checking;
+  ejintbool_t check_presentation;
+  ejintbool_t scoring_checker;
+  ejintbool_t use_stdin;
+  ejintbool_t use_stdout;
+  ejintbool_t combined_stdin;
+  ejintbool_t combined_stdout;
+  ejintbool_t binary_input;
+  ejintbool_t binary;
+  ejintbool_t ignore_exit_code;
+  ejintbool_t olympiad_mode;
+  ejintbool_t score_latest;
+  ejintbool_t score_latest_or_unmarked;
+  ejintbool_t use_ac_not_ok;
+  ejintbool_t ignore_prev_ac;
+  ejintbool_t team_enable_rep_view;
+  ejintbool_t team_enable_ce_view;
+  ejintbool_t team_show_judge_report;
+  ejintbool_t ignore_compile_errors;
+  ejintbool_t variable_full_score;
+  ejintbool_t ignore_penalty;
+  ejintbool_t use_corr;
+  ejintbool_t use_info;
+  ejintbool_t use_tgz;
+  ejintbool_t tests_to_accept;
+  ejintbool_t accept_partial;
+  ejintbool_t disable_user_submit;
+  ejintbool_t disable_tab;
+  ejintbool_t restricted_statement;
+  ejintbool_t disable_submit_after_ok;
+  ejintbool_t disable_auto_testing;
+  ejintbool_t disable_testing;
+  ejintbool_t enable_compilation;
+  ejintbool_t skip_testing;
+  ejintbool_t hidden;
+  ejintbool_t stand_hide_time;
+  ejintbool_t advance_to_next;
+  ejintbool_t disable_ctrl_chars;
+  ejintbool_t enable_text_form;
+  ejintbool_t stand_ignore_score;
+  ejintbool_t stand_last_column;
+  ejintbool_t disable_security;
+  ejintbool_t valuer_sets_marked;
+  ejintbool_t ignore_unmarked;
+  ejintbool_t disable_stderr;
+
+  int real_time_limit;
+  int time_limit;
+  int time_limit_millis;
+  int full_score;
+  int full_user_score;
+  int test_score;
+  int run_penalty;
+  int acm_run_penalty;
+  int disqualified_penalty;
+  int min_tests_to_accept;
+  int checker_real_time_limit;
+  int priority_adjustment;
+  int score_multiplier;
+  int prev_runs_to_show;
+  int interactor_time_limit;
+  int max_open_file_count;
+  int max_process_count;
+
+  time_t deadline;
+  time_t start_date;
+
+  size_t max_vm_size;
+  size_t max_data_size;
+  size_t max_stack_size;
+  size_t max_core_size;
+  size_t max_file_size;
+
+  unsigned char *type;
+  unsigned char *long_name;
+  unsigned char *stand_name;
+  unsigned char *internal_name;
+  unsigned char *test_dir;
+  unsigned char *test_sfx;
+  unsigned char *corr_sfx;
+  unsigned char *info_sfx;
+  unsigned char *tgz_sfx;
+  unsigned char *tgzdir_sfx;
+  unsigned char *input_file;
+  unsigned char *output_file;
+  unsigned char *test_score_list;
+  unsigned char *score_tests;
+  unsigned char *standard_checker;
+  unsigned char *spelling;
+  unsigned char *plugin_file;
+  unsigned char *xml_file;
+  unsigned char *stand_attr;
+  unsigned char *source_header;
+  unsigned char *source_footer;
+  unsigned char *test_pat;
+  unsigned char *corr_pat;
+  unsigned char *info_pat;
+  unsigned char *tgz_pat;
+  unsigned char *tgzdir_pat;
+  unsigned char *normalization;
+  unsigned char *check_cmd;
+  unsigned char *valuer_cmd;
+  unsigned char *interactor_cmd;
+  unsigned char *style_checker_cmd;
+  unsigned char *test_checker_cmd;
+  unsigned char *solution_src;
+  unsigned char *solution_cmd;
+  unsigned char *score_bonus;
+  unsigned char *open_tests;
+  unsigned char *final_open_tests;
+  unsigned char *extid;
+
+  char **test_sets;
+  char **date_penalty;
+  char **group_start_date;
+  char **group_deadline;
+  char **disable_language;
+  char **enable_language;
+  char **require;
+  char **lang_time_adj;
+  char **lang_time_adj_millis;
+  char **personal_deadline;
+  char **score_view;
+  char **score_view_text;
+
+  ejenvlist_t lang_compiler_env;
+  ejenvlist_t checker_env;
+  ejenvlist_t valuer_env;
+  ejenvlist_t interactor_env;
+  ejenvlist_t style_checker_env;
+  ejenvlist_t test_checker_env;
+};
+
+void
+problem_config_section_init(struct generic_section_config *gp);
+struct problem_config_section *
+problem_config_section_alloc(void);
+void
+problem_config_section_free(struct generic_section_config *gp);
+struct problem_config_section *
+problem_config_section_parse_cfg(const unsigned char *path, FILE *f);
+struct problem_config_section *
+super_run_in_packet_parse_cfg_str(const unsigned char *path, char *buf, size_t size);
+void
+super_run_in_packet_unparse_cfg(FILE *out_f, const struct problem_config_section *p);
+
+
+#endif /* __PROBLEM_CONFIG_H__ */
