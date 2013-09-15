9ffd4866c3a36a1c9fc7592a7db9ab8908ded46f
cher <blackav@gmail.com>
2013-09-15 20:12:11 +0000

    * ej-polygon.c: support of newer polygon versions is in progress.

--- ej-polygon.c
+++ ej-polygon.c
@@ -23,6 +23,8 @@
 #include "expat_iface.h"
 #include "xml_utils.h"
 #include "problem_config.h"
+#include "list_ops.h"
+#include "misctext.h"
 
 #include "reuse_osdeps.h"
 #include "reuse_xalloc.h"
@@ -69,6 +71,22 @@ enum UpdateState
     STATE_ACTUAL,
 
     STATE_LAST,
+};
+
+struct HtmlAttribute
+{
+    struct HtmlAttribute *prev;
+    struct HtmlAttribute *next;
+    unsigned char *name;
+    unsigned char *value;
+};
+
+struct HtmlElement
+{
+    unsigned char *name;
+    struct HtmlAttribute *first_attr;
+    struct HtmlAttribute *last_attr;
+    int no_body; // 1, if <elem /> case
 };
 
 struct ProblemInfo
@@ -118,6 +136,11 @@ struct RevisionInfo
     unsigned char *standard_url;
     unsigned char *windows_url;
     unsigned char *linux_url;
+};
+
+struct PolygonState
+{
+    unsigned char *ccid; // may be NULL for older versions of Polygon
 };
 
 enum
@@ -209,8 +232,8 @@ struct DownloadInterface
     unsigned char *(*get_page_text)(struct DownloadData *data);
     ssize_t (*get_page_size)(struct DownloadData *data);
     int (*login_page)(struct DownloadData *data);
-    int (*login_action)(struct DownloadData *data);
-    int (*problems_page)(struct DownloadData *data);
+    int (*login_action)(struct DownloadData *data, struct PolygonState *ps);
+    int (*problems_page)(struct DownloadData *data, struct PolygonState *ps);
     int (*problem_info_page)(struct DownloadData *data, struct ProblemInfo *info);
     int (*package_page)(struct DownloadData *data, const unsigned char *edit_session);
     int (*create_full_package)(struct DownloadData *data, const unsigned char *edit_session);
@@ -326,7 +349,7 @@ ends_with(const unsigned char *str, const unsigned char *suffix)
 }
 
 static int
-curl_iface_login_action_func(struct DownloadData *data)
+curl_iface_login_action_func(struct DownloadData *data, struct PolygonState *ps)
 {
     unsigned char url_buf[1024];
     unsigned char param_buf[1024];
@@ -389,7 +412,7 @@ cleanup:
 }
 
 static int
-curl_iface_problems_page_func(struct DownloadData *data)
+curl_iface_problems_page_func(struct DownloadData *data, struct PolygonState *ps)
 {
     unsigned char url_buf[1024];
 
@@ -644,6 +667,23 @@ get_zip_interface(FILE *log_f, const struct polygon_packet *pkt)
 }
 #endif
 
+struct PolygonState *
+polygon_state_create(void)
+{
+    struct PolygonState *ps = NULL;
+    XCALLOC(ps, 1);
+    return ps;
+}
+
+struct PolygonState *
+polygon_state_free(struct PolygonState *ps)
+{
+    if (!ps) return NULL;
+    xfree(ps->ccid);
+    xfree(ps);
+    return NULL;
+}
+
 const unsigned char * update_statuses[] =
 {
     [STATE_NOT_STARTED] = "NOT_STARTED",
@@ -713,6 +753,289 @@ free_revision_info(struct RevisionInfo *info)
         xfree(info->linux_url);
     }
     memset(info, 0, sizeof(*info));
+}
+
+static struct HtmlAttribute *
+html_attribute_create(
+        const unsigned char *ptr,
+        int len,
+        unsigned char *value)
+{
+    struct HtmlAttribute *attr = NULL;
+    XCALLOC(attr, 1);
+    if (len <= 0) {
+        attr->name = xmemdup(ptr, len);
+    } else {
+        attr->name = xstrdup(ptr);
+    }
+    // note, the pre-allocated memory for value is used
+    attr->value = value;
+    for (unsigned char *s = attr->name; *s; ++s) {
+        *s = tolower(*s);
+    }
+    return attr;
+}
+
+static struct HtmlAttribute *
+html_attribute_free(struct HtmlAttribute *attr)
+{
+    if (!attr) return NULL;
+    xfree(attr->name);
+    xfree(attr->value);
+    xfree(attr);
+    return NULL;
+}
+
+static struct HtmlAttribute *
+html_element_find_attribute(struct HtmlElement *elem, const unsigned char *name)
+{
+    if (!elem) return NULL;
+    for (struct HtmlAttribute *p = elem->first_attr; p; p = p->next) {
+        if (!strcmp(p->name, name))
+            return p;
+    }
+    return NULL;
+}
+
+static struct HtmlElement *
+html_element_create(const unsigned char *ptr, int len)
+{
+    struct HtmlElement *elem = NULL;
+    XCALLOC(elem, 1);
+    if (len <= 0) {
+        elem->name = xstrdup(ptr);
+    } else {
+        elem->name = xmemdup(ptr, len);
+    }
+    for (unsigned char *s = elem->name; *s; ++s) {
+        *s = tolower(*s);
+    }
+    return elem;
+}
+
+static struct HtmlElement *
+html_element_free(struct HtmlElement *elem)
+{
+    if (!elem) return NULL;
+    xfree(elem->name);
+    struct HtmlAttribute *p, *q;
+    for (p = elem->first_attr; p; p = q) {
+        q = p->next;
+        html_attribute_free(p);
+    }
+    xfree(elem);
+    return NULL;
+}
+
+static int
+is_xml_element_name_start_char(int c)
+{
+    return isalpha(c) || c == ':' || c == '_';
+}
+static int
+is_xml_element_name_char(int c)
+{
+    return isalnum(c) || c == ':' || c == '_' || c == '-' || c == '.';
+}
+
+struct EntityTableEntry
+{
+    const unsigned char *name;
+    int value;
+};
+static struct EntityTableEntry html_entities[] =
+{
+    { "amp", '&' },
+
+    { NULL, 0 },
+};
+
+static int
+find_html_entity(const unsigned char *beg, const unsigned char *end)
+{
+    int len = (int) (end - beg);
+    unsigned char buf[128], *s = buf;
+    const unsigned char *p = beg;
+
+    if (len <= 0 || len > 127) return -1;
+    while (p != end) *s++ = tolower(*p++);
+    *s = 0;
+    for (int i = 0; html_entities[i].name; ++i) {
+        if (!strcmp(html_entities[i].name, buf)) {
+            return html_entities[i].value;
+        }
+    }
+    return -1;
+}
+
+static int
+convert_html_entity(const unsigned char *beg, const unsigned char *end, int base)
+{
+    int len = (int) (end - beg);
+    unsigned char buf[32];
+    if (len <= 0 || len > 31) return -1;
+    memcpy(buf, beg, len);
+    buf[len] = 0;
+    errno = 0;
+    int value = strtol(buf, NULL, base);
+    if (errno || value < 0 || value >= 0x10000) return -1;
+    return value;
+}
+
+static void
+parse_html_decode(
+        unsigned char *buf,
+        const unsigned char *start,
+        const unsigned char *end)
+{
+    const unsigned char *p = start;
+    unsigned char *out = buf;
+    while (*p && p != end) {
+        if (*p != '&') {
+            *out++ = *p++;
+            continue;
+        }
+        ++p;
+        if (!*p || p == end) {
+            *out++ = '&';
+            continue;
+        }
+        if (*p == '#' && p[1] == 'x') {
+            const unsigned char *q = p;
+            p += 2;
+            while (isxdigit(*p)) ++p;
+            int e = convert_html_entity(q + 2, p, 16);
+            if (e >= 0) {
+                out = ucs4_to_utf8_char(out, e);
+                if (*p == ';') ++p;
+            } else {
+                *out++ = '&';
+                p = q;
+            }
+        } else if (*p == '#') {
+            const unsigned char *q = p;
+            ++p;
+            while (isdigit(*p)) ++p;
+            int e = convert_html_entity(q + 1, p, 10);
+            if (e >= 0) {
+                out = ucs4_to_utf8_char(out, e);
+                if (*p == ';') ++p;
+            } else {
+                *out++ = '&';
+                p = q;
+            }
+        } else if (isalpha(*p)) {
+            // entity expansion is not performed...
+            const unsigned char *q = p;
+            while (isalnum(*p)) ++p;
+            int e = find_html_entity(q, p);
+            if (e >= 0) {
+                out = ucs4_to_utf8_char(out, e);
+                if (*p == ';') ++p;
+            } else {
+                *out++ = '&';
+                p = q;
+            }
+        } else {
+            *out++ = '&';
+            continue;
+        }
+    }
+    *out = 0;
+}
+
+static struct HtmlElement *
+html_element_parse_start(
+        const unsigned char *text,
+        int start_pos,
+        int *p_end_pos)
+{
+    const unsigned char *p = text + start_pos, *q, *r;
+    const unsigned char *value_start, *value_end;
+    struct HtmlElement *elem = NULL;
+    struct HtmlAttribute *attr = NULL;
+    unsigned char *value_buf = NULL;
+
+    while (isspace(*p)) ++p;
+    if (!*p) goto fail;
+    if (*p != '<') goto fail;
+    ++p;
+    while (isspace(*p)) ++p;
+    if (!is_xml_element_name_start_char(*p)) goto fail;
+    q = p;
+    while (is_xml_element_name_char(*p)) ++p;
+    if (!isspace(*p)) goto fail;
+    elem = html_element_create(q, (int) (p - q));
+    while (1) {
+        while (isspace(p)) ++p;
+        if (*p == '>') {
+            ++p;
+            break;
+        }
+        if (*p == '/' && p[1] == '>') {
+            elem->no_body = 1;
+            p += 2;
+            break;
+        }
+        if (!is_xml_element_name_start_char(*p)) goto fail;
+        q = p;
+        while (is_xml_element_name_char(*p)) ++p;
+        r = p;
+        while (isspace(*p)) ++p;
+        if (*p != '=') {
+            // create an empty value attribute
+            // FIXME: check attribute uniqueness
+            attr = html_attribute_create(r, (int) (q - r), NULL);
+            LINK_LAST(attr, elem->first_attr, elem->last_attr, prev, next);
+            attr = NULL;
+            continue;
+        }
+        ++p;
+        while (isspace(*p)) ++p;
+        if (*p == '\'') {
+            ++p;
+            value_start = p;
+            while (*p && *p != '\'') ++p;
+            if (!*p) goto fail;
+            value_end = p;
+            ++p;
+        } else if (*p == '\"') {
+            ++p;
+            value_start = p;
+            while (*p && *p != '\"') ++p;
+            if (!*p) goto fail;
+            value_end = p;
+            ++p;
+        } else if (is_xml_element_name_start_char(*p)) {
+            value_start = p;
+            while (*p && !isspace(*p)) ++p;
+            if (!*p) goto fail;
+            value_end = p;
+        } else {
+            // create an empty value attribute
+            // FIXME: check attribute uniqueness
+            attr = html_attribute_create(r, (int) (q - r), NULL);
+            LINK_LAST(attr, elem->first_attr, elem->last_attr, prev, next);
+            attr = NULL;
+            continue;
+        }
+
+        // value is in [value_start; value_end), not entity-decoded...
+        value_buf = malloc(((int) (value_end - value_start) + 1) * sizeof(*value_buf));
+        if (!value_buf) goto fail;
+        parse_html_decode(value_buf, value_start, value_end);
+        attr = html_attribute_create(r, (int) (q - r), value_buf);
+        LINK_LAST(attr, elem->first_attr, elem->last_attr, prev, next);
+        attr = NULL; value_buf = NULL;
+    }
+    if (p_end_pos) *p_end_pos = (int)(p - text);
+    return elem;
+
+fail:
+    xfree(value_buf);
+    html_element_free(elem);
+    if (p_end_pos) *p_end_pos = (int)(p - text);
+    return NULL;
 }
 
 static int
@@ -906,6 +1229,36 @@ extract_raw_td_content(const unsigned char *s, unsigned char *out, const unsigne
 static time_t
 parse_time(FILE *log_f, const unsigned char *param_name, const unsigned char *buf);
 
+static int
+process_login_page(
+        FILE *log_f,
+        struct PolygonState *ps,
+        struct DownloadData *ddata)
+{
+    int pos = 0;
+    const unsigned char *text = ddata->iface->get_page_text(ddata);
+    const unsigned char *cur;
+    struct HtmlElement *elem = NULL;
+    struct HtmlAttribute *attr;
+
+    while ((cur = strstr(text + pos, "<input"))) {
+        pos = (int)(cur - (text + pos));
+        elem = html_element_parse_start(text, pos, &pos);
+        if (elem) {
+            if ((attr = html_element_find_attribute(elem, "type")) && attr->value && !strcasecmp(attr->value, "hidden")
+                && (attr = html_element_find_attribute(elem, "name")) && attr->value && !strcasecmp(attr->value, "ccid")
+                && (attr = html_element_find_attribute(elem, "value")) && attr->value) {
+                ps->ccid = xstrdup(attr->value);
+                elem = html_element_free(elem);
+                break;
+            }
+            elem = html_element_free(elem);
+        }
+    }
+
+    return 0;
+}
+
 static void
 process_problem_row(
         FILE *log_f,
@@ -1076,6 +1429,7 @@ cleanup:
 static void
 process_problems_page(
         FILE *log_f,
+        struct PolygonState *ps,
         const unsigned char *text,
         struct ProblemInfo *infos,
         int info_count)
@@ -2166,6 +2520,7 @@ check_problem_statuses(
         const struct polygon_packet *pkt,
         const struct DownloadInterface *dif,
         struct DownloadData *ddata,
+        struct PolygonState *ps,
         const struct ZipInterface *zif,
         struct ProblemInfo *infos,
         int info_count)
@@ -2188,6 +2543,7 @@ do_work(
     const struct ZipInterface *zif = NULL;
     struct DownloadData *ddata = NULL;
     int retval = 0;
+    struct PolygonState *ps = NULL;
 
     if (info_count <= 0) {
         fprintf(log_f, "no problems to update\n");
@@ -2227,10 +2583,19 @@ do_work(
         goto done;
     }
 
+    if (!(ps = polygon_state_create())) {
+        fprintf(log_f, "failed to create PolygonState object\n");
+        retval = 1;
+        goto done;
+    }
+
     if ((retval = dif->login_page(ddata)))
         goto done;
 
-    if ((retval = dif->login_action(ddata)))
+    if ((retval = process_login_page(log_f, ps, ddata)))
+        goto done;
+
+    if ((retval = dif->login_action(ddata, ps)))
         goto done;
 
     while (1) {
@@ -2239,9 +2604,9 @@ do_work(
             break;
         }
 
-        process_problems_page(log_f, dif->get_page_text(ddata), infos, info_count);
+        process_problems_page(log_f, ps, dif->get_page_text(ddata), infos, info_count);
 
-        if (!check_problem_statuses(log_f, pkt, dif, ddata, zif, infos, info_count)) {
+        if (!check_problem_statuses(log_f, pkt, dif, ddata, ps, zif, infos, info_count)) {
             // no problems in RUNNING state
             break;
         }
@@ -2253,7 +2618,7 @@ do_work(
             break;
         }
 
-        if ((retval = dif->problems_page(ddata)))
+        if ((retval = dif->problems_page(ddata, ps)))
             goto done;
     }
 
@@ -2261,6 +2626,7 @@ done:;
     if (ddata) {
         if (dif) ddata = dif->cleanup(ddata);
     }
+    ps = polygon_state_free(ps);
     return retval;
 }
 
