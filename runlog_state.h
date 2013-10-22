316a411b36ebd09d948965b67dc62bb958ab399b
cher <blackav@gmail.com>
2013-10-22 23:24:36 +0000

    * runlog_state.h: copyright notice is updated; (struct user_entry) new fields 'run_id_valid', 'run_id_first', 'run_id_last' are added; (struct run_entry_extra) new structure is added; (struct runlog_state) new fields 'run_extra_u', 'run_extra_a', 'run_extras' are added.
    * runlog.c:
    (run_destroy) new fields are handled;
    (run_reset) new fields are handled;
    (extend_run_extras) new function;
    (get_user_entry) user index rebuilding is added.

--- runlog_state.h
+++ runlog_state.h
@@ -4,7 +4,7 @@
 #ifndef __RUNLOG_STATE_H__
 #define __RUNLOG_STATE_H__
 
-/* Copyright (C) 2008-2012 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2008-2013 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -30,14 +30,24 @@ enum
 struct user_entry
 {
   int status;                   /* virtual or real user */
-  int start_time;
-  int stop_time;
+  time_t start_time;
+  time_t stop_time;
+
+  int run_id_valid;             /* 1, if the following two fields are properly computed */
+  int run_id_first;             /* first run_id of that user, -1, if none */
+  int run_id_last;              /* last run_id of that user, -1, if none */
 };
 
 struct user_flags_info_s
 {
   int nuser;
   int *flags;
+};
+
+struct run_entry_extra
+{
+  int prev_user_id;            /* previous run with the same user_id, -1, if none*/
+  int next_user_id;            /* next run with the same user_id, -1, if none */
 };
 
 struct rldb_plugin_iface;
@@ -57,10 +67,14 @@ struct runlog_state
   teamdb_state_t     teamdb_state;
   int ut_size;
   struct user_entry **ut_table;
-  struct user_flags_info_s user_flags;
+
+  struct user_flags_info_s user_flags; // banned/invisible/locked flags for users
 
   int max_user_id;
   int user_count;
+
+  int run_extra_u, run_extra_a;
+  struct run_entry_extra *run_extras; /* run indices */
 
   // the managing plugin information
   struct rldb_plugin_iface *iface;
