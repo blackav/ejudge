5469d92b42b18863534f0b1d933c4605b6152c36
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2014-01-03 17:37:50 +0000

    copyright notice is updated; refactoring of reuse files.

--- trunk/ejudge/userlist_clnt/send_packet.c
+++ trunk/ejudge/userlist_clnt/send_packet.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2002-2011 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2002-2014 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -19,7 +19,7 @@
 
 #include "errlog.h"
 
-#include "reuse_integral.h"
+#include "reuse/integral.h"
 
 #include <sys/uio.h>
 #include <errno.h>
