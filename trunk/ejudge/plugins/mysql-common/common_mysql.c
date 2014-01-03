0de2a4f1c9195b1b96d9f446a5de76e87dc1a25e
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2014-01-03 17:52:54 +0000

    copyright notice is updated; refactoring: reuse include files are moved to another directory.

--- trunk/ejudge/plugins/mysql-common/common_mysql.c
+++ trunk/ejudge/plugins/mysql-common/common_mysql.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2008-2013 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2008-2014 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -22,9 +22,9 @@
 #include "pathutl.h"
 #include "errlog.h"
 
-#include "reuse_xalloc.h"
-#include "reuse_logger.h"
-#include "reuse_osdeps.h"
+#include "reuse/xalloc.h"
+#include "reuse/logger.h"
+#include "reuse/osdeps.h"
 
 #include <string.h>
 #include <ctype.h>
