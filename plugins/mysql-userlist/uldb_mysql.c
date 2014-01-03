2bd83af1abd721f07ff29357bff3e619109e3811
cher <blackav@gmail.com>
2014-01-03 17:54:00 +0000

    copyright notice is updated; refactoring: reuse include files are moved to another directory.

--- plugins/mysql-userlist/uldb_mysql.c
+++ plugins/mysql-userlist/uldb_mysql.c
@@ -1,7 +1,7 @@
 /* -*- mode: c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2006-2013 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2006-2014 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -30,9 +30,9 @@
 #include "../mysql-common/common_mysql.h"
 #include "compat.h"
 
-#include "reuse_xalloc.h"
-#include "reuse_logger.h"
-#include "reuse_osdeps.h"
+#include "reuse/xalloc.h"
+#include "reuse/logger.h"
+#include "reuse/osdeps.h"
 
 #include <mysql.h>
 
