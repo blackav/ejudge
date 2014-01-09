b2e86bd4fd6f6f2834097e00d15a0b7add53337b
cher <blackav@gmail.com>
2014-01-09 21:42:11 +0000

    progress...

--- type_info.c
+++ type_info.c
@@ -18,6 +18,21 @@
 #include "type_info.h"
 
 #include "reuse/xalloc.h"
+#include "reuse/logger.h"
+
+#include <string.h>
+
+typedef struct ValueTreeNode
+{
+    struct ValueTreeNode *left, *right;
+    TypeInfo *value;
+} ValueTreeNode;
+
+typedef struct ValueTree
+{
+    ValueTreeNode *root;
+    int count;
+} ValueTree;
 
 typedef struct SignedIntStorage
 {
@@ -175,6 +190,119 @@ TypeInfo *
 tc_get_long_double(TypeInfo *cntx, long double value)
 {
     // FIXME
+    return NULL;
+}
+
+static int
+vt_count_nodes(ValueTreeNode *root)
+{
+    if (!root) return 0;
+    return 1 + vt_count_nodes(root->left) + vt_count_nodes(root->right);
+}
+
+static int
+vt_add_to_arr(ValueTreeNode *root, ValueTreeNode **nodes, int cur)
+{
+    if (!root) return cur;
+    cur = vt_add_to_arr(root->left, nodes, cur);
+    nodes[cur++] = root;
+    return vt_add_to_arr(root->right, nodes, cur);
+}
+
+static ValueTreeNode *
+vt_build_from_arr(ValueTreeNode **nodes, int low, int high)
+{
+    if (low >= high) return NULL;
+    int mid = (low + high) / 2;
+    nodes[mid]->left = vt_build_from_arr(nodes, low, mid);
+    nodes[mid]->right = vt_build_from_arr(nodes, mid + 1, high);
+    return nodes[mid];
+}
+
+ValueTreeNode *
+vt_build_balanced(ValueTreeNode *root)
+{
+    int count = vt_count_nodes(root);
+    if (count <= 0) return root;
+
+    ValueTreeNode **nodes = xcalloc(count, sizeof(nodes[0]));
+    vt_add_to_arr(root, nodes, 0);
+    ValueTreeNode *new_root = vt_build_from_arr(nodes, 0, count);
+    xfree(nodes);
+    return new_root;
+}
+
+int
+vt_compare(c_value_t *pv1, c_value_t *pv2)
+{
+    ASSERT(pv1);
+    ASSERT(pv2);
+    ASSERT(pv1->tag == pv2->tag);
+
+    switch (pv1->tag) {
+    case C_BOOL:
+        if (pv1->v.ct_bool < pv2->v.ct_bool) return -1;
+        if (pv1->v.ct_bool > pv2->v.ct_bool) return 1;
+        return 0;
+    case C_CHAR:
+        if (pv1->v.ct_char < pv2->v.ct_char) return -1;
+        if (pv1->v.ct_char > pv2->v.ct_char) return 1;
+        return 0;
+    case C_SCHAR:
+        if (pv1->v.ct_schar < pv2->v.ct_schar) return -1;
+        if (pv1->v.ct_schar > pv2->v.ct_schar) return 1;
+        return 0;
+    case C_UCHAR:
+        if (pv1->v.ct_uchar < pv2->v.ct_uchar) return -1;
+        if (pv1->v.ct_uchar > pv2->v.ct_uchar) return 1;
+        return 0;
+    case C_SHORT:
+        if (pv1->v.ct_short < pv2->v.ct_short) return -1;
+        if (pv1->v.ct_short > pv2->v.ct_short) return 1;
+        return 0;
+    case C_USHORT:
+        if (pv1->v.ct_ushort < pv2->v.ct_ushort) return -1;
+        if (pv1->v.ct_ushort > pv2->v.ct_ushort) return 1;
+        return 0;
+    case C_INT:
+        if (pv1->v.ct_int < pv2->v.ct_int) return -1;
+        if (pv1->v.ct_int > pv2->v.ct_int) return 1;
+        return 0;
+    case C_UINT:
+        if (pv1->v.ct_uint < pv2->v.ct_uint) return -1;
+        if (pv1->v.ct_uint > pv2->v.ct_uint) return 1;
+        return 0;
+    case C_LONG:
+        if (pv1->v.ct_lint < pv2->v.ct_lint) return -1;
+        if (pv1->v.ct_lint > pv2->v.ct_lint) return 1;
+        return 0;
+    case C_ULONG:
+        if (pv1->v.ct_ulint < pv2->v.ct_ulint) return -1;
+        if (pv1->v.ct_ulint > pv2->v.ct_ulint) return 1;
+        return 0;
+    case C_LLONG:
+        if (pv1->v.ct_llint < pv2->v.ct_llint) return -1;
+        if (pv1->v.ct_llint > pv2->v.ct_llint) return 1;
+        return 0;
+    case C_ULLONG:
+        if (pv1->v.ct_ullint < pv2->v.ct_ullint) return -1;
+        if (pv1->v.ct_ullint > pv2->v.ct_ullint) return 1;
+        return 0;
+    case C_FLOAT:
+        return memcmp(&pv1->v.ct_float, &pv2->v.ct_float, sizeof(float));
+    case C_DOUBLE:
+        return memcmp(&pv1->v.ct_double, &pv2->v.ct_double, sizeof(double));
+    case C_LDOUBLE:
+        return memcmp(&pv1->v.ct_ldouble, &pv2->v.ct_ldouble, sizeof(long double));
+    default:
+        abort();
+    }
+    return 0;
+}
+
+ValueTreeNode *
+vt_find(ValueTree *pt, const c_value_t *pv)
+{
     return NULL;
 }
 
