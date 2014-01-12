9a89e97c005b6aeaf754159d56a4fd6b72b81c06
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2014-01-12 18:00:51 +0000

    initial revision

---
+++ trunk/ejudge/dwarf_parse.h
@@ -0,0 +1,32 @@
+/* -*- c -*- */
+/* $Id$ */
+#ifndef __DWARF_PARSE_H__
+#define __DWARF_PARSE_H__
+
+/* Copyright (C) 2014 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "type_info.h"
+
+#include <stdio.h>
+
+int dwarf_parse(FILE *log_f, const unsigned char *path, TypeContext *cntx);
+
+#endif /* __DWARF_PARSE_H__ */
+
+/*
+ * Local variables:
+ *  c-basic-offset: 4
+ * End:
+ */
