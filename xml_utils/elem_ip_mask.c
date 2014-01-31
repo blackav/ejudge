79971ae6f21ada6f05c1ff7573c57f851c6ca73d
cher <blackav@gmail.com>
2014-01-31 11:55:30 +0000

    * elem_ip_mask.c: copyright notice is updated; (_xml_elem_ip_mask) function is removed.

--- xml_utils/elem_ip_mask.c
+++ xml_utils/elem_ip_mask.c
@@ -1,7 +1,7 @@
 /* -*- c -*- */
 /* $Id$ */
 
-/* Copyright (C) 2004-2013 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2004-2014 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -19,47 +19,6 @@
 #include "expat_iface.h"
 
 #include <string.h>
-
-int
-_xml_elem_ip_mask(struct xml_tree *tree,
-                 unsigned int *addr_ptr, unsigned int *mask_ptr)
-{
-  unsigned int b1, b2, b3, b4;
-  int n;
-
-  if (!strcmp(tree->text, "0")) {
-    *addr_ptr = 0;
-    *mask_ptr = 0;
-    return 0;
-  }
-  if (sscanf(tree->text, "%u.%u.%u.%u %n", &b1, &b2, &b3, &b4, &n) == 4
-      && !tree->text[n] && b1 <= 255 && b2 <= 255 && b3 <= 255 && b4 <= 255) {
-    *addr_ptr = b1 << 24 | b2 << 16 | b3 << 8 | b4;
-    *mask_ptr = 0xFFFFFFFF;
-    return 0;
-  }
-  if (sscanf(tree->text, "%u.%u.%u. %n", &b1, &b2, &b3, &n) == 3
-      && !tree->text[n] && b1 <= 255 && b2 <= 255 && b3 <= 255) {
-    *addr_ptr = b1 << 24 | b2 << 16 | b3 << 8;
-    *mask_ptr = 0xFFFFFF00;
-    return 0;
-  }
-  if (sscanf(tree->text, "%u.%u. %n", &b1, &b2, &n) == 2
-      && !tree->text[n] && b1 <= 255 && b2 <= 255) {
-    *addr_ptr = b1 << 24 | b2 << 16;
-    *mask_ptr = 0xFFFF0000;
-    return 0;
-  }
-  if (sscanf(tree->text, "%u. %n", &b1, &n) == 1
-      && !tree->text[n] && b1 <= 255) {
-    *addr_ptr = b1 << 24;
-    *mask_ptr = 0xFF000000;
-    return 0;
-  }
-
-  xml_err(tree, "invalid IP-address");
-  return -1;
-}
 
 int
 xml_elem_ipv6_mask(
