54d18e01df7a1f49310a7d07e1228b4000c520e1
cher <cher@13e13086-cd15-0410-aeb9-89833061a40d>
2014-08-01 22:14:47 +0000

    action renamings

--- trunk/ejudge/super_html.c
+++ trunk/ejudge/super_html.c
@@ -934,37 +934,37 @@ print_form_fields(FILE *f, struct contest_desc *cnts,
   struct contest_member *memb;
 
   print_form_fields_3(f, cnts->fields, "Primary registration fields",
-                      SSERV_CMD_EDIT_USER_FIELDS_PAGE,
+                      SSERV_CMD_CNTS_EDIT_USER_FIELDS_PAGE,
                       session_id, row_attrs[0],
                       self_url, hidden_vars, extra_args);
   memb = 0;
   if (cnts->members) memb = cnts->members[CONTEST_M_CONTESTANT];
   print_form_fields_2(f, memb, "\"Contestant\" member parameters",
-                      SSERV_CMD_NEW_CNTS_EDIT_CONTESTANT_FIELDS_PAGE,
+                      SSERV_CMD_CNTS_EDIT_CONTESTANT_FIELDS_PAGE,
                       session_id, row_attrs[1],
                       self_url, hidden_vars, extra_args);
   memb = 0;
   if (cnts->members) memb = cnts->members[CONTEST_M_RESERVE];
   print_form_fields_2(f, memb, "\"Reserve\" member parameters",
-                      SSERV_CMD_NEW_CNTS_EDIT_RESERVE_FIELDS_PAGE,
+                      SSERV_CMD_CNTS_EDIT_RESERVE_FIELDS_PAGE,
                       session_id, row_attrs[0],
                       self_url, hidden_vars, extra_args);
   memb = 0;
   if (cnts->members) memb = cnts->members[CONTEST_M_COACH];
   print_form_fields_2(f, memb, "\"Coach\" member parameters",
-                      SSERV_CMD_NEW_CNTS_EDIT_COACH_FIELDS_PAGE,
+                      SSERV_CMD_CNTS_EDIT_COACH_FIELDS_PAGE,
                       session_id, row_attrs[1],
                       self_url, hidden_vars, extra_args);
   memb = 0;
   if (cnts->members) memb = cnts->members[CONTEST_M_ADVISOR];
   print_form_fields_2(f, memb, "\"Advisor\" member parameters",
-                      SSERV_CMD_NEW_CNTS_EDIT_ADVISOR_FIELDS_PAGE,
+                      SSERV_CMD_CNTS_EDIT_ADVISOR_FIELDS_PAGE,
                       session_id, row_attrs[0],
                       self_url, hidden_vars, extra_args);
   memb = 0;
   if (cnts->members) memb = cnts->members[CONTEST_M_GUEST];
   print_form_fields_2(f, memb, "\"Guest\" member parameters",
-                      SSERV_CMD_NEW_CNTS_EDIT_GUEST_FIELDS_PAGE,
+                      SSERV_CMD_CNTS_EDIT_GUEST_FIELDS_PAGE,
                       session_id, row_attrs[1],
                       self_url, hidden_vars, extra_args);
 }
@@ -1469,28 +1469,28 @@ super_html_edit_contest_page(
 
   if (sstate->show_access_rules) {
     print_access_summary(f, cnts->register_access, "Access to `register' program",
-                         SSERV_CMD_EDIT_REGISTER_ACCESS_PAGE,
+                         SSERV_CMD_CNTS_EDIT_REGISTER_ACCESS_PAGE,
                          session_id, form_row_attrs[row ^= 1],
                          self_url, extra_args);
     print_access_summary(f, cnts->users_access, "Access to `users' program",
-                         SSERV_CMD_EDIT_USERS_ACCESS_PAGE,
+                         SSERV_CMD_CNTS_EDIT_USERS_ACCESS_PAGE,
                          session_id, form_row_attrs[row ^= 1],
                          self_url, extra_args);
     print_access_summary(f, cnts->master_access, "Access to `master' program",
-                         SSERV_CMD_EDIT_MASTER_ACCESS_PAGE,
+                         SSERV_CMD_CNTS_EDIT_MASTER_ACCESS_PAGE,
                          session_id, form_row_attrs[row ^= 1],
                          self_url, extra_args);
     print_access_summary(f, cnts->judge_access, "Access to `judge' program",
-                         SSERV_CMD_EDIT_JUDGE_ACCESS_PAGE,
+                         SSERV_CMD_CNTS_EDIT_JUDGE_ACCESS_PAGE,
                          session_id, form_row_attrs[row ^= 1],
                          self_url, extra_args);
     print_access_summary(f, cnts->team_access, "Access to `team' program",
-                         SSERV_CMD_EDIT_TEAM_ACCESS_PAGE,
+                         SSERV_CMD_CNTS_EDIT_TEAM_ACCESS_PAGE,
                          session_id, form_row_attrs[row ^= 1],
                          self_url, extra_args);
     print_access_summary(f, cnts->serve_control_access,
                          "Access to `serve-control' program",
-                         SSERV_CMD_EDIT_SERVE_CONTROL_ACCESS_PAGE,
+                         SSERV_CMD_CNTS_EDIT_SERVE_CONTROL_ACCESS_PAGE,
                          session_id, form_row_attrs[row ^= 1],
                          self_url, extra_args);
   }
