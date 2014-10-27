48b269a85db5a6be9770a730e356ffd545a9f08b
cher <blackav@gmail.com>
2014-10-27 23:38:37 +0000

    'enable_tokens' problem config var is added

--- ej-import-contest.c
+++ ej-import-contest.c
@@ -661,6 +661,7 @@ merge_problem_section(
         { CNTSPROB_disable_tab, META_PROBLEM_CONFIG_SECTION_disable_tab },
         { CNTSPROB_unrestricted_statement, META_PROBLEM_CONFIG_SECTION_unrestricted_statement },
         { CNTSPROB_hide_file_names, META_PROBLEM_CONFIG_SECTION_hide_file_names },
+        { CNTSPROB_enable_tokens, META_PROBLEM_CONFIG_SECTION_enable_tokens },
         { CNTSPROB_disable_submit_after_ok, META_PROBLEM_CONFIG_SECTION_disable_submit_after_ok },
         { CNTSPROB_disable_auto_testing, META_PROBLEM_CONFIG_SECTION_disable_auto_testing },
         { CNTSPROB_disable_testing, META_PROBLEM_CONFIG_SECTION_disable_testing },
--- super_html_3.c
+++ super_html_3.c
@@ -414,6 +414,7 @@ const unsigned char * const super_serve_help_urls[SSERV_CMD_LAST] =
   [SSERV_CMD_PROB_CHANGE_DISABLE_TAB] = "Serve.cfg:problem:disable_tab",
   [SSERV_CMD_PROB_CHANGE_UNRESTRICTED_STATEMENT] = "Serve.cfg:problem:unrestricted_statement",
   [SSERV_CMD_PROB_CHANGE_HIDE_FILE_NAMES] = "Serve.cfg:problem:hide_file_names",
+  [SSERV_CMD_PROB_CHANGE_ENABLE_TOKENS] = "Serve.cfg:problem:enable_tokens",
   [SSERV_CMD_PROB_CHANGE_DISABLE_SUBMIT_AFTER_OK] = "Serve.cfg:problem:disable_submit_after_ok",
   [SSERV_CMD_PROB_CHANGE_DISABLE_SECURITY] = "Serve.cfg:problem:disable_security",
   [SSERV_CMD_PROB_CHANGE_DISABLE_TESTING] = "Serve.cfg:problem:disable_testing",
@@ -2337,6 +2338,10 @@ super_html_prob_param(struct sid_state *sstate, int cmd,
 
   case SSERV_CMD_PROB_CHANGE_HIDE_FILE_NAMES:
     p_int = &prob->hide_file_names;
+    goto handle_boolean_2;
+
+  case SSERV_CMD_PROB_CHANGE_ENABLE_TOKENS:
+    p_int = &prob->enable_tokens;
     goto handle_boolean_2;
 
   case SSERV_CMD_PROB_CHANGE_DISABLE_SUBMIT_AFTER_OK:
