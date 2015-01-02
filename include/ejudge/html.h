f357cc0b9910b86587fc8fe0a45dc78f190c4b8a
Alexander Chernov <cher@smartdec.ru>
2015-01-02 12:41:29 +0300

    ...

--- include/ejudge/html.h
+++ include/ejudge/html.h
@@ -1,9 +1,8 @@
 /* -*- c -*- */
-/* $Id$ */
 #ifndef __HTML_H__
 #define __HTML_H__
 
-/* Copyright (C) 2000-2014 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2000-2015 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
