54fa114857ac7517e50fe10702e221b9c576c1fd
Alexander Chernov <blackav@gmail.com>
2015-01-20 14:06:38 +0300

    refactored as a plugin

--- include/ejudge/xuser_plugin.h
+++ include/ejudge/xuser_plugin.h
@@ -26,14 +26,72 @@
 /* version of the plugin interface structure */
 #define XUSER_PLUGIN_IFACE_VERSION 1
 
+struct ejudge_cfg;
+struct contest_desc;
+struct section_global_data;
+struct xuser_cnts_state;
+struct team_extra;
+
 struct xuser_plugin_iface
 {
-  struct common_plugin_iface b;
-  int xuser_version;
+    struct common_plugin_iface b;
+    int xuser_version;
+
+    struct xuser_cnts_state * (*open)(
+        struct common_plugin_data *data,
+        const struct ejudge_cfg *config,
+        const struct contest_desc *cnts,
+        const struct section_global_data *global,
+        int flags);
+    struct xuser_cnts_state * (*close)(
+        struct xuser_cnts_state *data);
+    const struct team_extra * (*get_entry)(
+        struct xuser_cnts_state *data,
+        int user_id);
+    int (*get_clar_status)(
+        struct xuser_cnts_state *data,
+        int user_id,
+        int clar_id);
+    int (*set_clar_status)(
+        struct xuser_cnts_state *data,
+        int user_id,
+        int clar_id);
+    void (*flush)(
+        struct xuser_cnts_state *data);
+    int (*append_warning)(
+        struct xuser_cnts_state *data,
+        int user_id,
+        int issuer_id,
+        const ej_ip_t *issuer_ip,
+        time_t issue_date,
+        const unsigned char *txt,
+        const unsigned char *cmt);
+    int (*set_status)(
+        struct xuser_cnts_state *data,
+        int user_id,
+        int status);
+    int (*set_disq_comment)(
+        struct xuser_cnts_state *data,
+        int user_id,
+        const unsigned char *disq_comment);
+    int (*get_run_fields)(
+        struct xuser_cnts_state *data,
+        int user_id);
+    int (*set_run_fields)(
+        struct xuser_cnts_state *data,
+        int user_id,
+        int run_fields);
 };
 
 struct xuser_cnts_state
 {
+    struct xuser_plugin_iface *vt;
 };
 
 #endif /* __CLDB_PLUGIN_H__ */
+
+/*
+ * Local variables:
+ *  c-basic-offset: 4
+ * End:
+ */
