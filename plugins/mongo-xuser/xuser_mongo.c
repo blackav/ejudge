97b4d46ed3667113396f012808bf8ddae2db1e41
Alexander Chernov <blackav@gmail.com>
2015-01-20 08:37:43 +0300

    ...

--- plugins/mongo-xuser/xuser_mongo.c
+++ plugins/mongo-xuser/xuser_mongo.c
@@ -32,6 +32,25 @@
 #include <unistd.h>
 #include <mongo.h>
 
+struct xuser_mongo_state
+{
+  int nref;
+
+  unsigned char *host;
+  int port;
+  mongo_sync_connection *conn;
+};
+
+static struct common_plugin_data *
+init_func(void);
+static int
+finish_func(struct common_plugin_data *data);
+static int
+prepare_func(
+        struct common_plugin_data *data,
+        const struct ejudge_cfg *config,
+        struct xml_tree *tree);
+
 struct xuser_plugin_iface plugin_xuser_mongo =
 {
   {
@@ -42,12 +61,75 @@ struct xuser_plugin_iface plugin_xuser_mongo =
       "mongo",
     },
     COMMON_PLUGIN_IFACE_VERSION,
-    NULL, /* init */
-    NULL, /* finish */
-    NULL, /* prepare */
+    init_func,
+    finish_func,
+    prepare_func,
   },
   XUSER_PLUGIN_IFACE_VERSION,
 };
+
+static struct common_plugin_data *
+init_func(void)
+{
+  struct xuser_mongo_state *state = NULL;
+  XCALLOC(state, 1);
+  return (struct common_plugin_data *) state;
+}
+
+static int
+finish_func(struct common_plugin_data *data)
+{
+  struct xuser_mongo_state *state = (struct xuser_mongo_state *) data;
+
+  if (state) {
+    if (state->nref > 0) {
+      err("xuser_mongo::finish: reference counter > 0");
+      return -1;
+    }
+
+    xfree(state->host);
+    memset(state, 0, sizeof(*state));
+    xfree(state);
+  }
+
+  return 0;
+}
+
+static int
+prepare_func(
+        struct common_plugin_data *data,
+        const struct ejudge_cfg *config,
+        struct xml_tree *tree)
+{
+  struct xuser_mongo_state *state = (struct xuser_mongo_state *) data;
+
+  // this plugin configuration subtree is pointed by 'tree'
+
+  for (struct xml_tree *p = tree->first_down; p; p = p->right) {
+    if (!strcmp(p->name[0], "host")) {
+      if (xml_leaf_elem(p, &state->host, 1, 0) < 0) return -1;
+    } else if (!strcmp(p->name[0], "port")) {
+      if (xml_parse_int(NULL, "", p->line, p->column, p->text, &state->port) < 0) return -1;
+      if (state->port < 0 || state->port > 65535) {
+        xml_err_elem_invalid(p);
+        return -1;
+      }
+    } else {
+      return xml_err_elem_not_allowed(p);
+    }
+  }
+
+  if (!state->host) state->host = xstrdup("localhost");
+  if (state->port <= 0) state->port = 27027;
+
+  state->conn = mongo_sync_connect(state->host, state->port, 0);
+  if (!state->conn) {
+    err("cannot connect to mongodb: %s", os_ErrorMsg());
+    return -1;
+  }
+
+  return 0;
+}
 
 /*
  * Local variables:
