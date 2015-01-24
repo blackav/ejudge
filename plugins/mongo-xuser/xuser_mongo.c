17711213f7df32c56f1c866055ad2e294476d13b
Alexander Chernov <blackav@gmail.com>
2015-01-24 03:11:18 +0300

    progress

--- plugins/mongo-xuser/xuser_mongo.c
+++ plugins/mongo-xuser/xuser_mongo.c
@@ -21,17 +21,19 @@
 #include "ejudge/errlog.h"
 #include "ejudge/xuser_plugin.h"
 #include "ejudge/contests.h"
+#include "ejudge/team_extra.h"
 
 #include "ejudge/xalloc.h"
 #include "ejudge/logger.h"
 #include "ejudge/osdeps.h"
+
+#include <mongo.h>
 
 #include <string.h>
 #include <ctype.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <mongo.h>
 
 struct xuser_mongo_state
 {
@@ -47,6 +49,10 @@ struct xuser_mongo_cnts_state
     struct xuser_cnts_state b;
     struct xuser_mongo_state *plugin_state;
     int contest_id;
+
+    // entry cache
+    int a, u;
+    struct team_extra **v;
 };
 
 static struct common_plugin_data *
@@ -66,6 +72,9 @@ open_func(
         const struct contest_desc *cnts,
         const struct section_global_data *global,
         int flags);
+struct xuser_cnts_state *
+close_func(
+        struct xuser_cnts_state *data);
 
 struct xuser_plugin_iface plugin_xuser_mongo =
 {
@@ -83,6 +92,7 @@ struct xuser_plugin_iface plugin_xuser_mongo =
     },
     XUSER_PLUGIN_IFACE_VERSION,
     open_func,
+    close_func,
 };
 
 static struct common_plugin_data *
@@ -169,6 +179,126 @@ open_func(
 
     return (struct xuser_cnts_state *) state;
 }
+
+struct xuser_cnts_state *close_func(
+        struct xuser_cnts_state *data)
+{
+    struct xuser_mongo_cnts_state *state = (struct xuser_mongo_cnts_state *) data;
+    if (state) {
+        --state->plugin_state->nref;
+        // FIXME: close connection?
+        xfree(state);
+    }
+    return NULL;
+}
+
+/*
+struct team_extra
+{
+  // primary key
+  ej_uuid_t uuid;
+
+  int is_dirty;
+  int user_id;
+
+  int clar_map_size;
+  int clar_map_alloc;
+  unsigned long *clar_map;
+
+  int clar_uuids_size;
+  int clar_uuids_alloc;
+  ej_uuid_t *clar_uuids;
+
+  // disqualification reason
+  unsigned char *disq_comment;
+
+  // warnings
+  int warn_u, warn_a;
+  struct team_warning **warns;
+
+  // status
+  int status;
+
+  // run table fields
+  int run_fields;
+};
+ */
+
+static struct team_extra *
+parse_bson(bson *b)
+{
+    bson_cursor *bc = NULL;
+    struct team_extra *res = NULL;
+
+    if (!b) return NULL;
+
+    XCALLOC(res, 1);
+    bc = bson_cursor_new(b);
+    while (bson_cursor_next(bc)) {
+        const unsigned char *key = bson_cursor_key(bc);
+        if (!strcmp(key, "uuid")) {
+        } else if (!strcmp(key, "contest_id")) {
+        } else if (!strcmp(key, "user_id")) {
+        } else if (!strcmp(key, "viewed_clars")) {
+        } else if (!strcmp(key, "clar_uuids")) {
+        } else if (!strcmp(key, "disc_comment")) {
+        } else if (!strcmp(key, "warnings")) {
+        } else if (!strcmp(key, "status")) {
+        } else if (!strcmp(key, "run_fields")) {
+        }
+    }
+    bson_cursor_free(bc);
+    return res;
+}
+
+const struct team_extra *
+get_entry_func(
+        struct xuser_cnts_state *data,
+        int user_id)
+{
+    struct xuser_mongo_cnts_state *state = (struct xuser_mongo_cnts_state *) data;
+    struct team_extra *res = NULL;
+
+    if (user_id <= 0) return NULL;
+
+    int low = 0, high = state->u, mid;
+    while (low < high) {
+        mid = (low + high) / 2;
+        if (state->v[mid]->user_id == user_id) {
+            return state->v[mid];
+        } else if (state->v[mid]->user_id < user_id) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    mid = low;
+
+    bson *query = bson_new();
+    bson_append_int32(query, "contest_id", state->contest_id);
+    bson_append_int32(query, "user_id", user_id);
+    bson_finish(query);
+    mongo_packet *pkt = mongo_sync_cmd_query(state->plugin_state->conn, "ejudge.xuser",
+                                             0, 0, 1, query, NULL);
+    bson_free(query); query = NULL;
+    if (!pkt) {
+        return NULL;
+    }
+    mongo_sync_cursor *cur = mongo_sync_cursor_new(state->plugin_state->conn, "ejudge.xuser", pkt);
+    if (!cur) {
+        mongo_wire_packet_free(pkt);
+        return NULL;
+    }
+    while (mongo_sync_cursor_next(cur)) {
+        bson *result = mongo_sync_cursor_get_data(cur);
+        res = parse_bson(result);
+        bson_free(result);
+    }
+    mongo_sync_cursor_free(cur);
+
+    return res;
+}
+
 
 /*
  * Local variables:
