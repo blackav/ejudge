9a33e108503be40bfb8df1a238265baa71e3da2a
Alexander Chernov <blackav@gmail.com>
2015-01-30 02:31:56 +0300

    authentification is added

--- plugins/mongo-xuser/xuser_mongo.c
+++ plugins/mongo-xuser/xuser_mongo.c
@@ -45,6 +45,10 @@ struct xuser_mongo_state
 
     unsigned char *host;
     int port;
+    unsigned char *password_file;
+    unsigned char *user;
+    unsigned char *password;
+
     mongo_sync_connection *conn;
 };
 
@@ -185,12 +189,69 @@ finish_func(struct common_plugin_data *data)
             return -1;
         }
 
+        xfree(state->user);
+        xfree(state->password);
+        xfree(state->password_file);
         xfree(state->host);
         memset(state, 0, sizeof(*state));
         xfree(state);
     }
 
     return 0;
+}
+
+static int
+parse_passwd_file(
+        struct xuser_mongo_state *state,
+        const unsigned char *path)
+{
+  FILE *f = 0;
+  const unsigned char *fname = __FUNCTION__;
+  unsigned char buser[1024];
+  unsigned char bpwd[1024];
+  int len, c;
+
+  if (!(f = fopen(path, "r"))) {
+    err("%s: cannot open password file %s", fname, path);
+    goto cleanup;
+  }
+  if (!fgets(buser, sizeof(buser), f)) {
+    err("%s: cannot read the user line from %s", fname, path);
+    goto cleanup;
+  }
+  if ((len = strlen(buser)) > sizeof(buser) - 24) {
+    err("%s: user is too long in %s", fname, path);
+    goto cleanup;
+  }
+  while (len > 0 && isspace(buser[--len]));
+  buser[++len] = 0;
+
+  if (!fgets(bpwd, sizeof(bpwd), f)) {
+    err("%s: cannot read the password line from %s", fname, path);
+    goto cleanup;
+  }
+  if ((len = strlen(bpwd)) > sizeof(bpwd) - 24) {
+    err("%s: password is too long in %s", fname, path);
+    goto cleanup;
+  }
+  while (len > 0 && isspace(bpwd[--len]));
+  bpwd[++len] = 0;
+  while ((c = getc(f)) && isspace(c));
+  if (c != EOF) {
+    err("%s: garbage in %s", fname, path);
+    goto cleanup;
+  }
+  fclose(f); f = 0;
+  state->user = xstrdup(buser);
+  state->password = xstrdup(bpwd);
+
+  // debug
+  //fprintf(stderr, "login: %s\npassword: %s\n", state->user, state->password);
+  return 0;
+
+ cleanup:
+  if (f) fclose(f);
+  return -1;
 }
 
 static int
@@ -212,10 +273,31 @@ prepare_func(
                 xml_err_elem_invalid(p);
                 return -1;
             }
+        } else if (!strcmp(p->name[0], "password_file")) {
+            if (xml_leaf_elem(p, &state->password_file, 1, 0) < 0) return -1;
         } else {
             return xml_err_elem_not_allowed(p);
         }
     }
+
+    if (state->password_file) {
+        unsigned char ppath[PATH_MAX];
+        ppath[0] = 0;
+        if (os_IsAbsolutePath(state->password_file)) {
+            snprintf(ppath, sizeof(ppath), "%s", state->password_file);
+        }
+#if defined EJUDGE_CONF_DIR
+        if (!ppath[0]) {
+            snprintf(ppath, sizeof(ppath), "%s/%s", EJUDGE_CONF_DIR,
+                     state->password_file);
+        }
+#endif
+        if (!ppath[0]) {
+            snprintf(ppath, sizeof(ppath), "%s", state->password_file);
+        }
+        if (parse_passwd_file(state, ppath) < 0) return -1;
+    }
+
 
     if (!state->host) state->host = xstrdup("localhost");
     if (state->port <= 0) state->port = 27017;
@@ -224,6 +306,12 @@ prepare_func(
     if (!state->conn) {
         err("cannot connect to mongodb: %s", os_ErrorMsg());
         return -1;
+    }
+    if (state->user && state->password) {
+        if (!mongo_sync_cmd_authenticate(state->conn, "ejudge", state->user, state->password)) {
+            err("authentification failed: %s", os_ErrorMsg());
+            return -1;
+        }
     }
 
     return 0;
