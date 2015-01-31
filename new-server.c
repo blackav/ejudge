03ab025b56adb3c49dc947a6ffc06498e5c6c911
Alexander Chernov <blackav@gmail.com>
2015-01-31 20:07:27 +0300

    ...

--- new-server.c
+++ new-server.c
@@ -1,7 +1,6 @@
 /* -*- mode: c -*- */
-/* $Id$ */
 
-/* Copyright (C) 2006-2014 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2006-2015 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
