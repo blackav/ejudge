2aa167c6bbad7bf0c241d6a27be00655545fb1c5
Alexander Chernov <blackav@gmail.com>
2015-06-19 22:38:45 +0300

    ...

--- dwarf_parse.c
+++ dwarf_parse.c
@@ -1,6 +1,6 @@
 /* -*- c -*- */
 
-/* Copyright (C) 2015 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2014-2015 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
