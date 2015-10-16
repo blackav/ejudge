92e1b8de31edad9f22c2c9fbdd80f0efb27b14e8
Alexander Chernov <blackav@gmail.com>
2015-10-16 15:08:16 +0300

    fix

--- run_common.c
+++ run_common.c
@@ -2487,7 +2487,7 @@ run_one_test(
   }
 
   if (tst && tst->secure_exec_type_val == SEXEC_TYPE_JAVA && srgp->lang_short_name) {
-    task_FormatEnv(tsk, "EJUDGE_JAVA_COMPILER=%s", srgp->lang_short_name);
+    task_FormatEnv(tsk, "EJUDGE_JAVA_COMPILER", "%s", srgp->lang_short_name);
   }
 
   if (tst && tst->enable_memory_limit_error > 0 && srgp->secure_run > 0 && srgp->detect_violations > 0) {
