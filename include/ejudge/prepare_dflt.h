eb7872aa00b15ee4a032937292d3f89140016e70
Alexander Chernov <blackav@gmail.com>
2015-11-05 15:44:08 +0300

    default rejudge priority adjustment is 4

--- include/ejudge/prepare_dflt.h
+++ include/ejudge/prepare_dflt.h
@@ -1,9 +1,8 @@
 /* -*- c -*- */
-/* $Id$ */
 #ifndef __PREPARE_DFLT_H__
 #define __PREPARE_DFLT_H__
 
-/* Copyright (C) 2005-2013 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2005-2015 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -145,7 +144,7 @@
 #define DFLT_G_SECURE_RUN 0
 #define DFLT_G_ENABLE_MEMORY_LIMIT_ERROR 0
 
-#define DFLT_G_REJUDGE_PRIORITY_ADJUSTMENT 10
+#define DFLT_G_REJUDGE_PRIORITY_ADJUSTMENT 4
 
 #define DFLT_P_INPUT_FILE         "input"
 #define DFLT_P_OUTPUT_FILE        "output"
