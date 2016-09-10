752f6d2fd122230f68665e15495cd259046bb2ab
Alexander Chernov <blackav@gmail.com>
2016-09-11 00:21:34 +0300

    ...

--- new_server_match.c
+++ new_server_match.c
@@ -3416,6 +3416,63 @@ int ns_match_action(const unsigned char *str)
                                   return 0;
                                 }
                               } else {
+                                if (c == 't') {
+                                  c = str[10];
+                                  if (c == 'e') {
+                                    c = str[11];
+                                    if (c == 'l') {
+                                      c = str[12];
+                                      if (c == 'e') {
+                                        c = str[13];
+                                        if (c == 'g') {
+                                          c = str[14];
+                                          if (c == 'r') {
+                                            c = str[15];
+                                            if (c == 'a') {
+                                              c = str[16];
+                                              if (c == 'm') {
+                                                c = str[17];
+                                                if (c == '-') {
+                                                  c = str[18];
+                                                  if (c == 't') {
+                                                    c = str[19];
+                                                    if (c == 'o') {
+                                                      c = str[20];
+                                                      if (c == 'k') {
+                                                        c = str[21];
+                                                        if (c == 'e') {
+                                                          c = str[22];
+                                                          if (c == 'n') {
+                                                            c = str[23];
+                                                            if (!c) return NEW_SRV_ACTION_GENERATE_TELEGRAM_TOKEN;
+                                                            return 0;
+                                                          }
+                                                          return 0;
+                                                        }
+                                                        return 0;
+                                                      }
+                                                      return 0;
+                                                    }
+                                                    return 0;
+                                                  }
+                                                  return 0;
+                                                }
+                                                return 0;
+                                              }
+                                              return 0;
+                                            }
+                                            return 0;
+                                          }
+                                          return 0;
+                                        }
+                                        return 0;
+                                      }
+                                      return 0;
+                                    }
+                                    return 0;
+                                  }
+                                  return 0;
+                                }
                               }
                               return 0;
                             }
