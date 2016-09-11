c5a70c7eabf2c9c59457f9a23ddef03a16f7fcc9
Alexander Chernov <blackav@gmail.com>
2016-09-11 22:06:10 +0300

    telegram_chat added

---
+++ plugins/telegram/telegram_chat.c
@@ -0,0 +1,184 @@
+/* -*- mode: c -*- */
+
+/* Copyright (C) 2016 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "ejudge/bson_utils.h"
+#include "ejudge/xalloc.h"
+#include "ejudge/errlog.h"
+#include "ejudge/osdeps.h"
+
+#include "telegram_chat.h"
+#include "mongo_conn.h"
+
+#include <mongo.h>
+
+#include <errno.h>
+
+#define TELEGRAM_CHATS_TABLE_NAME "telegram_chats"
+
+struct telegram_chat *
+telegram_chat_free(struct telegram_chat *tc)
+{
+    if (tc) {
+        xfree(tc->type);
+        xfree(tc->title);
+        xfree(tc->username);
+        xfree(tc->first_name);
+        xfree(tc->last_name);
+        memset(tc, 0xff, sizeof(*tc));
+        xfree(tc);
+    }
+    return NULL;
+}
+
+struct telegram_chat *
+telegram_chat_create(void)
+{
+    struct telegram_chat *tc = NULL;
+    XCALLOC(tc, 1);
+    return tc;
+}
+
+struct telegram_chat *
+telegram_chat_parse_bson(struct _bson *bson)
+{
+    bson_cursor *bc = NULL;
+    struct telegram_chat *tc = NULL;
+
+    XCALLOC(tc, 1);
+    bc = bson_cursor_new(bson);
+    while (bson_cursor_next(bc)) {
+        const unsigned char *key = bson_cursor_key(bc);
+        if (!strcmp(key, "_id")) {
+            if (ej_bson_parse_int64(bc, "_id", &tc->_id) < 0) goto cleanup;
+        } else if (!strcmp(key, "type")) {
+            if (ej_bson_parse_string(bc, "type", &tc->type) < 0) goto cleanup;
+        } else if (!strcmp(key, "title")) {
+            if (ej_bson_parse_string(bc, "title", &tc->type) < 0) goto cleanup;
+        } else if (!strcmp(key, "username")) {
+            if (ej_bson_parse_string(bc, "username", &tc->type) < 0) goto cleanup;
+        } else if (!strcmp(key, "first_name")) {
+            if (ej_bson_parse_string(bc, "first_name", &tc->type) < 0) goto cleanup;
+        } else if (!strcmp(key, "last_name")) {
+            if (ej_bson_parse_string(bc, "last_name", &tc->type) < 0) goto cleanup;
+        }
+    }
+    bson_cursor_free(bc);
+    return tc;
+
+cleanup:
+    telegram_chat_free(tc);
+    return NULL;
+}
+
+bson *
+telegram_chat_unparse_bson(const struct telegram_chat *tc)
+{
+    if (!tc) return NULL;
+
+    bson *bson = bson_new();
+    if (tc->_id) {
+        bson_append_int64(bson, "_id", tc->_id);
+    }
+    if (tc->type && *tc->type) {
+        bson_append_string(bson, "type", tc->type, strlen(tc->type));
+    }
+    if (tc->title && *tc->title) {
+        bson_append_string(bson, "title", tc->title, strlen(tc->title));
+    }
+    if (tc->username && *tc->username) {
+        bson_append_string(bson, "username", tc->username, strlen(tc->username));
+    }
+    if (tc->first_name && *tc->first_name) {
+        bson_append_string(bson, "first_name", tc->first_name, strlen(tc->first_name));
+    }
+    if (tc->last_name && *tc->last_name) {
+        bson_append_string(bson, "last_name", tc->last_name, strlen(tc->last_name));
+    }
+    bson_finish(bson);
+    return bson;
+}
+
+struct telegram_chat *
+telegram_chat_fetch(struct mongo_conn *conn, long long _id)
+{
+    if (!mongo_conn_open(conn)) return NULL;
+
+    bson *query = NULL;
+    mongo_packet *pkt = NULL;
+    mongo_sync_cursor *cursor = NULL;
+    bson *result = NULL;
+    struct telegram_chat *retval = NULL;
+
+    query = bson_new();
+    bson_append_int64(query, "_id", _id);
+    bson_finish(query);
+
+    pkt = mongo_sync_cmd_query(conn->conn, mongo_conn_ns(conn, TELEGRAM_CHATS_TABLE_NAME), 0, 0, 1, query, NULL);
+    if (!pkt && errno == ENOENT) {
+        goto cleanup;
+    }
+    if (!pkt) {
+        err("mongo query failed: %s", os_ErrorMsg());
+        goto cleanup;
+    }
+    bson_free(query); query = NULL;
+
+    cursor = mongo_sync_cursor_new(conn->conn, conn->ns, pkt);
+    if (!cursor) {
+        err("mongo query failed: cannot create cursor: %s", os_ErrorMsg());
+        goto cleanup;
+    }
+    pkt = NULL;
+    if (mongo_sync_cursor_next(cursor)) {
+        result = mongo_sync_cursor_get_data(cursor);
+        if (result) {
+            retval = telegram_chat_parse_bson(result);
+        }
+    }
+
+cleanup:
+    if (result) bson_free(result);
+    if (cursor) mongo_sync_cursor_free(cursor);
+    if (pkt) mongo_wire_packet_free(pkt);
+    if (query) bson_free(query);
+    return retval;
+}
+
+int
+telegram_chat_save(struct mongo_conn *conn, const struct telegram_chat *tc)
+{
+    if (!mongo_conn_open(conn)) return -1;
+    int retval = -1;
+
+    bson *b = telegram_chat_unparse_bson(tc);
+
+    if (!mongo_sync_cmd_insert(conn->conn, mongo_conn_ns(conn, TELEGRAM_CHATS_TABLE_NAME), b, NULL)) {
+        err("save_token: failed: %s", os_ErrorMsg());
+        goto cleanup;
+    }
+
+    retval = 0;
+
+cleanup:
+    bson_free(b);
+    return retval;
+}
+
+/*
+ * Local variables:
+ *  c-basic-offset: 4
+ * End:
+ */
---
+++ plugins/telegram/telegram_chat.h
@@ -0,0 +1,54 @@
+/* -*- c -*- */
+#ifndef __TELEGRAM_CHAT_H__
+#define __TELEGRAM_CHAT_H__
+
+/* Copyright (C) 2016 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+struct _bson;
+
+/*
+id 	Integer 	Unique identifier for this chat. This number may be greater than 32 bits and some programming languages may have difficulty/silent defects in interpreting it. But it smaller than 52 bits, so a signed 64 bit integer or double-precision float type are safe for storing this identifier.
+type 	String 	Type of chat, can be either “private”, “group”, “supergroup” or “channel”
+title 	String 	Optional. Title, for channels and group chats
+username 	String 	Optional. Username, for private chats, supergroups and channels if available
+first_name 	String 	Optional. First name of the other party in a private chat
+last_name 	String 	Optional. Last name of the other party in a private chat
+ */
+struct telegram_chat
+{
+    long long _id;
+    unsigned char *type;
+    unsigned char *title;
+    unsigned char *username;
+    unsigned char *first_name;
+    unsigned char *last_name;
+};
+
+struct telegram_chat *
+telegram_chat_free(struct telegram_chat *tc);
+struct telegram_chat *
+telegram_chat_create(void);
+struct telegram_chat *
+telegram_chat_parse_bson(struct _bson *bson);
+struct _bson *
+telegram_chat_unparse_bson(const struct telegram_chat *tc);
+
+#endif
+
+/*
+ * Local variables:
+ *  c-basic-offset: 4
+ * End:
+ */
