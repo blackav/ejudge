25860d855d0c909e91afef619725a909e516b211
Alexander Chernov <blackav@gmail.com>
2017-06-30 21:00:05 +0300

    ...

--- plugins/mongo-avatar/mongo_avatar.c
+++ plugins/mongo-avatar/mongo_avatar.c
@@ -16,14 +16,18 @@
 
 #include "ejudge/avatar_plugin.h"
 #include "ejudge/common_mongo_plugin.h"
+#include "ejudge/bson_utils.h"
+#include "ejudge/mime_type.h"
 
 #include "ejudge/xalloc.h"
 #include "ejudge/errlog.h"
 
 #include <string.h>
+#include <mongo.h>
 
 struct avatar_mongo_state
 {
+    struct avatar_plugin_data b;
     struct common_mongo_state *common;
     int nref;
     unsigned char *avatar_table;
@@ -38,6 +42,21 @@ prepare_func(
         struct common_plugin_data *data,
         const struct ejudge_cfg *config,
         struct xml_tree *tree);
+static int
+insert_func(
+        struct avatar_plugin_data *data,
+        int user_id,
+        int contest_id,
+        int is_cropped,
+        int is_temporary,
+        int mime_type,
+        int width,
+        int height,
+        const unsigned char *random_key,
+        time_t create_time,
+        const unsigned char *img_data,
+        size_t img_size,
+        unsigned char **p_id);
 
 struct avatar_plugin_iface plugin_avatar_mongo =
 {
@@ -54,6 +73,7 @@ struct avatar_plugin_iface plugin_avatar_mongo =
         prepare_func,
     },
     AVATAR_PLUGIN_IFACE_VERSION,
+    insert_func,
 };
 
 static struct common_plugin_data *
@@ -99,4 +119,52 @@ prepare_func(
     return 0;
 }
 
+static int
+insert_func(
+        struct avatar_plugin_data *data,
+        int user_id,
+        int contest_id,
+        int is_cropped,
+        int is_temporary,
+        int mime_type,
+        int width,
+        int height,
+        const unsigned char *random_key,
+        time_t create_time,
+        const unsigned char *img_data,
+        size_t img_size,
+        unsigned char **p_id)
+{
+    struct avatar_mongo_state *state = (struct avatar_mongo_state *) data;
 
+    bson *res = bson_new();
+    bson_append_int32(res, "user_id", user_id);
+    bson_append_int32(res, "contest_id", contest_id);
+    bson_append_boolean(res, "is_cropped", is_cropped);
+    bson_append_boolean(res, "is_temporary", is_temporary);
+    const unsigned char *mime_type_str = mime_type_get_type(mime_type);
+    bson_append_string(res, "mime_type", mime_type_str, strlen(mime_type_str));
+    bson_append_int32(res, "width", width);
+    bson_append_int32(res, "height", height);
+    bson_append_string(res, "random_key", random_key, strlen(random_key));
+    bson_append_utc_datetime(res, "create_time", create_time * 1000LL);
+    bson_append_int64(res, "size", (long long) img_size);
+    bson_append_binary(res, "image", BSON_BINARY_SUBTYPE_USER_DEFINED, img_data, img_size);
+    bson_finish(res);
+
+    // FIXME: handle errors
+    state->common->i->insert_and_free(state->common, state->avatar_table, &res);
+    /*
+    if (state->plugin_state->common->i->insert_and_free(state->plugin_state->common, "xuser", &b) < 0) {
+        return -1;
+    }
+     */
+
+    return 0;
+}
+
+/*
+ * Local variables:
+ *  c-basic-offset: 4
+ * End:
+ */
