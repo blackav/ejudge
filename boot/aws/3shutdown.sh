c0113e9f4b8259408612553ff50b2935fdca5e87
Alexander Chernov <blackav@gmail.com>
2017-10-07 11:01:36 +0300

    AWS updated

--- boot/aws/3shutdown.sh
+++ boot/aws/3shutdown.sh
@@ -2,4 +2,4 @@
 fusermount -u /home/judges
 rm -rf /tmp/ejudge/super-run
 rm -rf /var/lib/ejudge/*
-exec ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i ~/.ssh/id_dsa_shutdown fedora@localhost "sudo /sbin/shutdown now"
+exec ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa_shutdown fedora@localhost "sudo /sbin/shutdown now"
