7a3d2be54a954a9709d03e514a5b0a888250f59b
Alexander Chernov <blackav@gmail.com>
2019-03-26 12:34:48 +0300

    mongo connection parameters supported

--- plugins/telegram/telegram.c
+++ plugins/telegram/telegram.c
@@ -22,6 +22,7 @@
 #include "ejudge/xalloc.h"
 #include "ejudge/errlog.h"
 #include "ejudge/logger.h"
+#include "ejudge/osdeps.h"
 
 #include "telegram_data.h"
 #include "telegram_pbs.h"
@@ -108,7 +109,62 @@ struct telegram_plugin_data
 
     struct mongo_conn *conn;
     int curl_verbose_flag;
+    unsigned char *password_file;
 };
+
+static int
+parse_passwd_file(
+        struct telegram_plugin_data *state,
+        const unsigned char *path)
+{
+  FILE *f = 0;
+  const unsigned char *fname = __FUNCTION__;
+  unsigned char buser[1024];
+  unsigned char bpwd[1024];
+  int len, c;
+
+  if (!(f = fopen(path, "r"))) {
+    err("%s: cannot open password file %s", fname, path);
+    goto cleanup;
+  }
+  if (!fgets(buser, sizeof(buser), f)) {
+    err("%s: cannot read the user line from %s", fname, path);
+    goto cleanup;
+  }
+  if ((len = strlen(buser)) > sizeof(buser) - 24) {
+    err("%s: user is too long in %s", fname, path);
+    goto cleanup;
+  }
+  while (len > 0 && isspace(buser[--len]));
+  buser[++len] = 0;
+
+  if (!fgets(bpwd, sizeof(bpwd), f)) {
+    err("%s: cannot read the password line from %s", fname, path);
+    goto cleanup;
+  }
+  if ((len = strlen(bpwd)) > sizeof(bpwd) - 24) {
+    err("%s: password is too long in %s", fname, path);
+    goto cleanup;
+  }
+  while (len > 0 && isspace(bpwd[--len]));
+  bpwd[++len] = 0;
+  while ((c = getc(f)) && isspace(c));
+  if (c != EOF) {
+    err("%s: garbage in %s", fname, path);
+    goto cleanup;
+  }
+  fclose(f); f = 0;
+  state->conn->user = xstrdup(buser);
+  state->conn->password = xstrdup(bpwd);
+
+  // debug
+  //fprintf(stderr, "login: %s\npassword: %s\n", state->user, state->password);
+  return 0;
+
+ cleanup:
+  if (f) fclose(f);
+  return -1;
+}
 
 static struct bot_state *
 add_bot_id(struct telegram_plugin_data *state, const unsigned char *id)
@@ -144,6 +200,7 @@ finish_func(struct common_plugin_data *data)
     struct telegram_plugin_data *state = (struct telegram_plugin_data*) data;
 
     mongo_conn_free(state->conn);
+    xfree(state->password_file);
     memset(state, 0, sizeof(*state));
     xfree(state);
     return 0;
@@ -173,9 +230,41 @@ prepare_func(
                     add_bot_id(state, bot_id);
                 }
             }
+        } else if (!strcmp(p->name[0], "host")) {
+            if (xml_leaf_elem(p, &state->conn->host, 1, 0) < 0) return -1;
+        } else if (!strcmp(p->name[0], "port")) {
+            if (xml_parse_int(NULL, "", p->line, p->column, p->text, &state->conn->port) < 0) return -1;
+            if (state->conn->port < 0 || state->conn->port > 65535) {
+                xml_err_elem_invalid(p);
+                return -1;
+            }
+        } else if (!strcmp(p->name[0], "database")) {
+            if (xml_leaf_elem(p, &state->conn->database, 1, 0) < 0) return -1;
+        } else if (!strcmp(p->name[0], "table_prefix")) {
+            if (xml_leaf_elem(p, &state->conn->table_prefix, 1, 0) < 0) return -1;
+        } else if (!strcmp(p->name[0], "password_file")) {
+            if (xml_leaf_elem(p, &state->password_file, 1, 0) < 0) return -1;
         } else if (!strcmp(p->name[0], "curl_verbose")) {
             state->curl_verbose_flag = 1;
         }
+    }
+
+    if (state->password_file) {
+        unsigned char ppath[PATH_MAX];
+        ppath[0] = 0;
+        if (os_IsAbsolutePath(state->password_file)) {
+            snprintf(ppath, sizeof(ppath), "%s", state->password_file);
+        }
+#if defined EJUDGE_CONF_DIR
+        if (!ppath[0]) {
+            snprintf(ppath, sizeof(ppath), "%s/%s", EJUDGE_CONF_DIR,
+                     state->password_file);
+        }
+#endif
+        if (!ppath[0]) {
+            snprintf(ppath, sizeof(ppath), "%s", state->password_file);
+        }
+        if (parse_passwd_file(state, ppath) < 0) return -1;
     }
 
     ej_jobs_add_handler("telegram", packet_handler_telegram, state);
