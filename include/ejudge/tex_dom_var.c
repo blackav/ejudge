58896b0d4dd4fd31be2732ab869399d2d783b84a
Alexander Chernov <cher@ejudge.ru>
2020-05-23 03:04:02 +0300

    gcc 10 fixes

---
+++ include/ejudge/tex_dom_var.c
@@ -0,0 +1,19 @@
+/* -*- mode:c -*- */
+
+/* Copyright (C) 2004-2020 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "ejudge/tex_dom.h"
+
+int (*(*tex_dom_get_render_func_ptr)(int))(tex_par_t,tex_buffer_t,int) = 0;
