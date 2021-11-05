8fb7e3e69c64f4f3a91ae816da507d0095e1f8ca
Alexander Chernov <blackav@gmail.com>
2021-11-06 01:55:24 +0300

    bind /etc only if shadow copy available

--- bin/ej-suid-container.c
+++ bin/ej-suid-container.c
@@ -540,8 +540,13 @@ reconfigure_fs(void)
     }
     if (!enable_etc) {
         if (snprintf(bind_path, sizeof(bind_path), "%s/etc", safe_dir_path) >= sizeof(bind_path)) abort();
-        if ((r = mount(bind_path, "/etc", NULL, MS_BIND, NULL)) < 0) {
-            ffatal("failed to mount %s as /etc: %s", bind_path, strerror(errno));
+        struct stat stb;
+        if (lstat(bind_path, &stb) >= 0 && S_ISDIR(stb.st_mode)) {
+            if ((r = mount(bind_path, "/etc", NULL, MS_BIND, NULL)) < 0) {
+                ffatal("failed to mount %s as /etc: %s", bind_path, strerror(errno));
+            }
+        } else {
+            // FIXME: report error?
         }
     }
     if (!enable_var) {
