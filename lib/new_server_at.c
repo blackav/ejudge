acfefcdca43fc5b7a035e33e1483b117a787eaa4
Alexander Chernov <blackav@gmail.com>
2022-01-07 15:35:41 +0300

    user_run_header_page added

--- lib/new_server_at.c
+++ lib/new_server_at.c
@@ -320,4 +320,5 @@ const unsigned char * const ns_symbolic_action_table[NEW_SRV_ACTION_LAST] =
   [NEW_SRV_ACTION_OAUTH_LOGIN_2] = "oauth-login-2",
   [NEW_SRV_ACTION_OAUTH_LOGIN_3] = "oauth-login-3",
   [NEW_SRV_ACTION_USER_RUN_HEADERS_PAGE] = "user-run-headers-page",
+  [NEW_SRV_ACTION_USER_RUN_HEADER_PAGE] = "user-run-header-page",
 };
