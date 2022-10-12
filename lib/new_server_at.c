499048667065763dac610e147c5b3017424ecb3f
Alexander Chernov <cher@ejudge.ru>
2022-10-12 15:22:16 +0300

    submit-run-input action added

--- lib/new_server_at.c
+++ lib/new_server_at.c
@@ -327,4 +327,5 @@ const unsigned char * const ns_symbolic_action_table[NEW_SRV_ACTION_LAST] =
   [NEW_SRV_ACTION_DISABLE_VIRTUAL_START] = "disable-virtual-start",
   [NEW_SRV_ACTION_ENABLE_VIRTUAL_START] = "enable-virtual-start",
   [NEW_SRV_ACTION_GITLAB_WEBHOOK] = "gitlab-webhook",
+  [NEW_SRV_ACTION_SUBMIT_RUN_INPUT] = "submit-run-input",
 };
