fa03760d89df7498c8e67192dbe1cffe6e287826
Alexander Chernov <blackav@gmail.com>
2022-10-18 12:21:24 +0300

    vcs plugin added

---
+++ include/ejudge/vcs_plugin.h
@@ -0,0 +1,39 @@
+/* -*- mode: c; c-basic-offset: 4 -*- */
+#ifndef __VCS_PLUGIN_H__
+#define __VCS_PLUGIN_H__
+
+/* Copyright (C) 2022 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "ejudge/common_plugin.h"
+
+#define VCS_PLUGIN_IFACE_VERSION 1
+
+struct vcs_plugin_iface;
+
+// the plugin state
+struct vcs_plugin_data
+{
+    struct common_plugin_data b;
+    struct vcs_plugin_iface *vt;
+};
+
+struct vcs_plugin_iface
+{
+    struct common_plugin_iface b;
+    int vcs_version;
+
+};
+
+#endif /* __VCS_PLUGIN_H__ */
