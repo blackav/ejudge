blackav/ejudge

commit 770643c85e3e7320d785652e8d361118178d6403

telegram_subscription_fetch virtualized

plugins/telegram/telegram.c
@@ -753,7 +753,7 @@ packet_handler_telegram_reviewed(int uid, int argc, char **argv, void *user)
         goto cleanup;
     }
 
-    sub = telegram_subscription_fetch((struct mongo_conn *) state->conn, argv[1], contest_id, user_id);
+    sub = state->conn->vt->subscription_fetch(state->conn, argv[1], contest_id, user_id);
     if (!sub) goto cleanup;
     if (!sub->review_flag) goto cleanup;
     if (!sub->chat_id) {
@@ -833,7 +833,7 @@ packet_handler_telegram_replied(int uid, int argc, char **argv, void *user)
         goto cleanup;
     }
 
-    sub = telegram_subscription_fetch((struct mongo_conn *) state->conn, argv[1], contest_id, user_id);
+    sub = state->conn->vt->subscription_fetch(state->conn, argv[1], contest_id, user_id);
     if (!sub) goto cleanup;
     if (!sub->reply_flag) goto cleanup;
     if (!sub->chat_id) {
@@ -1417,7 +1417,7 @@ handle_incoming_message(
             } else if (!r) {
                 send_result = send_message(state, bs, mc, "Token expired. Operation failed.", NULL, NULL);
             } else {
-                sub = telegram_subscription_fetch((struct mongo_conn *) state->conn, bs->bot_id, token->contest_id, token->user_id);
+                sub = state->conn->vt->subscription_fetch(state->conn, bs->bot_id, token->contest_id, token->user_id);
                 if (!sub && !strcmp(tcs->command, "/unsubscribe")) {
                     send_result = send_message(state, bs, mc, "You have no subscriptions. Nothing to unsubscribe.", NULL, "{ \"hide_keyboard\": true}");
                 } else {

plugins/telegram/generic_conn.h
@@ -75,6 +75,12 @@ struct generic_conn_iface
     int (*chat_state_save)(
         struct generic_conn *gc,
         const struct telegram_chat_state *tcs);
+
+    struct telegram_subscription * (*subscription_fetch)(
+        struct generic_conn *gc,
+        const unsigned char *bot_id,
+        int user_id,
+        int contest_id);
 };
 
 struct generic_conn

plugins/telegram/mongo_conn.c
@@ -96,6 +96,13 @@ chat_state_save_func(
         struct generic_conn *gc,
         const struct telegram_chat_state *tcs);
 
+static struct telegram_subscription *
+subscription_fetch_func(
+        struct generic_conn *gc,
+        const unsigned char *bot_id,
+        int user_id,
+        int contest_id);
+
 static struct generic_conn_iface mongo_iface =
 {
     free_func,
@@ -113,6 +120,7 @@ static struct generic_conn_iface mongo_iface =
     user_save_func,
     chat_state_fetch_func,
     chat_state_save_func,
+    subscription_fetch_func,
 };
 
 struct generic_conn *
@@ -403,4 +411,17 @@ chat_state_save_func(
         const struct telegram_chat_state *tcs)
 {
     return telegram_chat_state_save((struct mongo_conn *) gc, tcs);
+}
+
+struct telegram_subscription *
+telegram_subscription_fetch(struct mongo_conn *conn, const unsigned char *bot_id, int user_id, int contest_id);
+
+static struct telegram_subscription *
+subscription_fetch_func(
+        struct generic_conn *gc,
+        const unsigned char *bot_id,
+        int user_id,
+        int contest_id)
+{
+    return telegram_subscription_fetch((struct mongo_conn *) gc, bot_id, user_id, contest_id);
 }

plugins/telegram/telegram_subscription.h
@@ -1,8 +1,8 @@
-/* -*- c -*- */
+/* -*- mode: c; c-basic-offset: 4 -*- */
 #ifndef __TELEGRAM_SUBSCRIPTION_H__
 #define __TELEGRAM_SUBSCRIPTION_H__
 
-/* Copyright (C) 2016-2019 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2016-2022 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -52,15 +52,7 @@ telegram_subscription_unparse_bson(const struct telegram_subscription *subscript
 
 struct mongo_conn;
 
-struct telegram_subscription *
-telegram_subscription_fetch(struct mongo_conn *conn, const unsigned char *bot_id, int user_id, int contest_id);
 int
 telegram_subscription_save(struct mongo_conn *conn, const struct telegram_subscription *subscription);
 
 #endif
-
-/*
- * Local variables:
- *  c-basic-offset: 4
- * End:
- */

commit 0af5c4c06b18061b8c77ca4462249036bbb70647

Fix subscription_fetch (broken in 770643c85e)

plugins/telegram/telegram.c
@@ -781,7 +781,7 @@ packet_handler_telegram_reviewed(int uid, int argc, char **argv, void *user)
         goto cleanup;
     }
 
-    sub = state->conn->vt->subscription_fetch(state->conn, argv[1], contest_id, user_id);
+    sub = state->conn->vt->subscription_fetch(state->conn, argv[1], user_id, contest_id);
     if (!sub) goto cleanup;
     if (!sub->review_flag) goto cleanup;
     if (!sub->chat_id) {
@@ -861,7 +861,7 @@ packet_handler_telegram_replied(int uid, int argc, char **argv, void *user)
         goto cleanup;
     }
 
-    sub = state->conn->vt->subscription_fetch(state->conn, argv[1], contest_id, user_id);
+    sub = state->conn->vt->subscription_fetch(state->conn, argv[1], user_id, contest_id);
     if (!sub) goto cleanup;
     if (!sub->reply_flag) goto cleanup;
     if (!sub->chat_id) {
@@ -1445,7 +1445,7 @@ handle_incoming_message(
             } else if (!r) {
                 send_result = send_message(state, bs, mc, "Token expired. Operation failed.", NULL, NULL);
             } else {
-                sub = state->conn->vt->subscription_fetch(state->conn, bs->bot_id, token->contest_id, token->user_id);
+                sub = state->conn->vt->subscription_fetch(state->conn, bs->bot_id, token->user_id, token->contest_id);
                 if (!sub && !strcmp(tcs->command, "/unsubscribe")) {
                     send_result = send_message(state, bs, mc, "You have no subscriptions. Nothing to unsubscribe.", NULL, "{ \"hide_keyboard\": true}");
                 } else {

plugins/telegram/telegram_subscription.c
@@ -208,7 +208,7 @@ telegram_subscription_unparse_bson(const struct telegram_subscription *sub)
 }
 
 struct telegram_subscription *
-telegram_subscription_fetch(struct mongo_conn *conn, const unsigned char *bot_id, int contest_id, int user_id)
+telegram_subscription_fetch(struct mongo_conn *conn, const unsigned char *bot_id, int user_id, int contest_id)
 {
 #if HAVE_LIBMONGOC - 0 > 0
     if (!conn->b.vt->open(&conn->b)) return NULL;