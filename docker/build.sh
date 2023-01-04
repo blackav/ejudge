f19c7799ae48e5cc3f4654f6437db1c8b9dc5a79
Alexander Chernov <blackav@gmail.com>
2023-01-04 17:17:40 +0300

    ...

--- docker/build.sh
+++ docker/build.sh
@@ -26,3 +26,4 @@ chmod 755 ejudge-install.sh
 chown ejudge:ejudge ejudge-install.sh
 ./ejudge-setup -u ejudge -g ejudge -b -B
 cp -p ejudge-install.sh /opt/ejudge/bin
+cp docker/httpd.conf /etc/httpd/conf
