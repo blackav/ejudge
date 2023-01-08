03880f4264b381245a2a3a0465251de8038f505c
Denis Kirienko <Denis.Kirienko@gmail.com>
2023-01-08 20:54:13 +0300

    Added solutions on golang and kotlin of the test problem

---
+++ contest-1/problems/A/all_solutions/a_kotlin.kt
@@ -0,0 +1,8 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`);
+    var a: Long = sc.next().toLong();
+    var b: Long = sc.next().toLong();
+    println(a + b);
+}
