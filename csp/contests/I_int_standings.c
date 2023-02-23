97be002f6b32b97ce54e2326a8cd3deeeaaddfd7
Alexander Chernov <blackav@gmail.com>
2023-02-24 01:38:33 +0300

    Fix for #145

--- csp/contests/I_int_standings.c
+++ csp/contests/I_int_standings.c
@@ -1,6 +1,6 @@
 /* -*- c -*- */
 
-/* Copyright (C) 2017-2022 Alexander Chernov <cher@ejudge.ru> */
+/* Copyright (C) 2017-2023 Alexander Chernov <cher@ejudge.ru> */
 
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -630,10 +630,10 @@ process_kirov_run(
 
                 if (!cell->marked_flag || prob->ignore_unmarked <= 0 || pe->is_marked) {
                     cell->marked_flag = pe->is_marked;
-                    if (!cell->full_sol) ++cell->sol_att;
+                    //if (!cell->full_sol) ++cell->sol_att;
                     int score = calc_kirov_score(0, 0, row->start_time,
                                                  pg->separate_user_score, sii->user_mode, token_flags,
-                                                 pe, prob, cell->att_num,
+                                                 pe, prob, cell->sol_att,
                                                  cell->disq_num, cell->ce_num,
                                                  cell->full_sol?RUN_TOO_MANY:col->succ_att,
                                                  0, 0, effective_time);
@@ -665,7 +665,7 @@ process_kirov_run(
                             const struct section_problem_data *dst_prob = cs->probs[pg->p_ind[dst_pind]];
                             StandingsCell *dest_cell = &pg->cells[dst_up_ind];
                             dest_cell->marked_flag = pe->is_marked;
-                            if (!dest_cell->full_sol) ++dest_cell->sol_att;
+                            //if (!dest_cell->full_sol) ++dest_cell->sol_att;
                             score = dst_prob->full_score;
                             /*
                               score = calc_kirov_score(0, 0, start_time,
@@ -694,12 +694,12 @@ process_kirov_run(
             } else if (run_status == RUN_PARTIAL) {
                 if (!cell->marked_flag || prob->ignore_unmarked <= 0 || pe->is_marked) {
                     cell->marked_flag = pe->is_marked;
-                    if (!cell->full_sol) ++cell->sol_att;
                     int score = calc_kirov_score(0, 0, row->start_time,
                                                  pg->separate_user_score, sii->user_mode, token_flags,
-                                                 pe, prob, cell->att_num,
+                                                 pe, prob, cell->sol_att,
                                                  cell->disq_num, cell->ce_num, RUN_TOO_MANY, 0, 0,
                                                  effective_time);
+                    ++cell->sol_att;
                     if (prob->score_latest > 0 || score > cell->score) {
                         cell->score = score;
                     }
@@ -711,12 +711,12 @@ process_kirov_run(
                     pg->last_submit_run = run_id;
                 }
             } else if (run_status == RUN_WRONG_ANSWER_ERR && prob->type != 0) {
-                if (!cell->full_sol) ++cell->sol_att;
                 int score = calc_kirov_score(0, 0, row->start_time,
                                              pg->separate_user_score, sii->user_mode, token_flags,
-                                             pe, prob, cell->att_num,
+                                             pe, prob, cell->sol_att,
                                              cell->disq_num, cell->ce_num, RUN_TOO_MANY, 0, 0,
                                              effective_time);
+                ++cell->sol_att;
                 if (prob->score_latest > 0 || score > cell->score) {
                     cell->score = score;
                 }
@@ -729,13 +729,14 @@ process_kirov_run(
                         ++cell->ce_num;
                     } else {
                         ++cell->att_num;
+                        ++cell->sol_att;
                     }
-                    if (!cell->full_sol) ++cell->sol_att;
+                    //if (!cell->full_sol) ++cell->sol_att;
                     if (!cell->full_sol) ++col->tot_att;
                     pg->last_submit_run = run_id;
                 }
             } else if (run_status == RUN_DISQUALIFIED) {
-                if (!cell->full_sol) ++cell->sol_att;
+                //if (!cell->full_sol) ++cell->sol_att;
                 ++cell->disq_num;
                 ++pg->total_disqualified;
             } else if (run_status == RUN_PENDING) {
