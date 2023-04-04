f3dea5f394c30e9104717642423c31329207ad1c
Alexander Chernov <blackav@gmail.com>
2023-04-05 00:41:33 +0300

    enable execve for tsnode

--- bin/ej-suid-container.c
+++ bin/ej-suid-container.c
@@ -1772,8 +1772,17 @@ apply_language_profiles(void)
             limit_rss_size = limit_vm_size;
             limit_vm_size = -1;
         }
-    } else if (!strcmp(language_name, "node") || !strcmp(language_name, "tsnode")) {
+    } else if (!strcmp(language_name, "node")) {
         enable_sys_fork = 1;
+        limit_processes = 20;
+        limit_stack_size = 1024 * 1024; // 1M
+        if (limit_vm_size > 0 && limit_rss_size <= 0) {
+            limit_rss_size = limit_vm_size;
+            limit_vm_size = -1;
+        }
+    } else if (!strcmp(language_name, "tsnode")) {
+        enable_sys_fork = 1;
+        enable_sys_execve = 1;
         limit_processes = 20;
         limit_stack_size = 1024 * 1024; // 1M
         if (limit_vm_size > 0 && limit_rss_size <= 0) {
