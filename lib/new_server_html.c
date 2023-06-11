87d2188ff69bb028be0d1ef2b814be54235c2fc3
Alexander Chernov <blackav@gmail.com>
2023-06-11 08:33:46 +0300

    submit serializer moved to a separate file

--- lib/new_server_html.c
+++ lib/new_server_html.c
@@ -80,6 +80,7 @@
 #include "ejudge/compile_packet.h"
 #include "ejudge/run_packet.h"
 #include "ejudge/notify_plugin.h"
+#include "ejudge/json_serializers.h"
 
 #include "ejudge/xalloc.h"
 #include "ejudge/logger.h"
@@ -13129,75 +13130,7 @@ ns_get_submit(
     }
   }
 
-  cJSON *jrr = cJSON_CreateObject();
-  cJSON_AddNumberToObject(jrr, "submit_id", se.serial_id);
-  cJSON_AddNumberToObject(jrr, "contest_id", se.contest_id);
-  cJSON_AddNumberToObject(jrr, "user_id", se.user_id);
-  cJSON_AddNumberToObject(jrr, "prob_id", se.prob_id);
-  cJSON_AddNumberToObject(jrr, "lang_id", se.lang_id);
-  cJSON_AddNumberToObject(jrr, "status", se.status);
-  cJSON_AddStringToObject(jrr, "status_str",
-                          run_status_str(se.status, NULL, 0, 0, 0));
-  if (se.ext_user_kind > 0 && se.ext_user_kind < MIXED_ID_LAST) {
-    unsigned char buf[64];
-    cJSON_AddStringToObject(jrr, "ext_user_kind",
-                            mixed_id_unparse_kind(se.ext_user_kind));
-    cJSON_AddStringToObject(jrr, "ext_user",
-                            mixed_id_marshall(buf, se.ext_user_kind,
-                                              &se.ext_user));
-  }
-  if (se.notify_driver > 0
-      && se.notify_kind > 0 && se.notify_kind < MIXED_ID_LAST) {
-    unsigned char buf[64];
-    cJSON_AddNumberToObject(jrr, "notify_driver", se.notify_driver);
-    cJSON_AddStringToObject(jrr, "notify_kind",
-                            mixed_id_unparse_kind(se.notify_kind));
-    cJSON_AddStringToObject(jrr, "notify_queue",
-                            mixed_id_marshall(buf, se.notify_kind,
-                                              &se.notify_queue));
-  }
-  if (tr) {
-    if (tr->compiler_output && *tr->compiler_output) {
-      cJSON_AddStringToObject(jrr, "compiler_output", tr->compiler_output);
-    }
-    if (tr->run_tests > 0) {
-      if (tr->tests && tr->tests[0]) {
-        struct testing_report_test *ttr = tr->tests[0];
-        if (ttr) {
-          cJSON_AddNumberToObject(jrr, "time", ttr->time);
-          if (ttr->real_time > 0) {
-            cJSON_AddNumberToObject(jrr, "real_time", ttr->real_time);
-          }
-          if (ttr->exit_code >= 0) {
-            cJSON_AddNumberToObject(jrr, "exit_code", ttr->exit_code);
-          }
-          if (ttr->term_signal > 0) {
-            cJSON_AddNumberToObject(jrr, "term_signal", ttr->term_signal);
-          }
-          if (ttr->max_memory_used > 0) {
-            cJSON_AddNumberToObject(jrr, "max_memory_used", ttr->max_memory_used);
-          }
-          if (ttr->max_rss > 0) {
-            cJSON_AddNumberToObject(jrr, "max_rss", ttr->max_rss);
-          }
-          if (ttr->input.size > 0) {
-            cJSON_AddStringToObject(jrr, "input", ttr->input.data);
-          }
-          if (ttr->output.size > 0) {
-            cJSON_AddStringToObject(jrr, "output", ttr->output.data);
-          }
-          if (ttr->error.size > 0) {
-            cJSON_AddStringToObject(jrr, "error", ttr->error.data);
-          }
-          if (ttr->test_checker.size > 0) {
-            cJSON_AddStringToObject(jrr, "test_checker", ttr->test_checker.data);
-          }
-        }
-      }
-    }
-  }
-
-  cJSON_AddItemToObject(jr, "result", jrr);
+  cJSON_AddItemToObject(jr, "result", json_serialize_submit(&se, tr));
   ok = 1;
 
 done:;
