ee2ac525a6e47fdb0fd97a6b86c7a883913b5097
Alexander Chernov <blackav@gmail.com>
2023-09-21 20:16:15 +0300

    postgres-exec implemented

--- bin/ej-postgres-exec.c
+++ bin/ej-postgres-exec.c
@@ -29,6 +29,7 @@
 #include <sys/wait.h>
 #include <sys/mman.h>
 #include <signal.h>
+#include <stdarg.h>
 
 static const char *program_name;
 
@@ -51,13 +52,253 @@ die(const char *format, ...)
     exit(1);
 }
 
+static int
+run_psql(
+        const char *host,
+        const char *port,
+        const char *user,
+        const char *password,
+        const char *database,
+        ...)
+{
+    int pid = fork();
+    if (pid < 0) {
+        fprintf(stderr, "%s: fork failed: %s\n", program_name,
+                strerror(errno));
+        return -1;
+    }
+    if (!pid) {
+        const char *args[64];
+        int i = 0;
+
+        setenv("PGPASSWORD", password, 1);
+        args[i++] = "psql";
+        args[i++] = "-h";
+        args[i++] = host;
+        args[i++] = "-p";
+        args[i++] = port;
+        args[i++] = "-U";
+        args[i++] = user;
+        if (database) {
+            args[i++] = "-d";
+            args[i++] = database;
+        }
+        args[i++] = "-X";
+        args[i++] = "-w";
+        args[i++] = "--csv";
+
+        va_list lst;
+        va_start(lst, database);
+        const char *ptr = NULL;
+        while ((ptr = va_arg(lst, const char *))) {
+            args[i++] = ptr;
+        }
+        va_end(lst);
+        args[i] = NULL;
+
+        execvp(args[0], (char**) args);
+        fprintf(stderr, "%s: exec failed: %s\n", program_name,
+                strerror(errno));
+        _exit(1);
+    }
+
+    int status = 0;
+    waitpid(pid, &status, 0);
+    if (WIFSIGNALED(status)) {
+        fprintf(stderr, "%s: psql terminated with signal %d\n",
+                program_name, WTERMSIG(status));
+        return 256 + WTERMSIG(status);
+    }
+    if (!WIFEXITED(status)) {
+        abort();
+    }
+    if (WEXITSTATUS(status) != 0) {
+        fprintf(stderr, "%s: psql exited with status %d\n",
+                program_name, WEXITSTATUS(status));
+        return WEXITSTATUS(status);
+    }
+    return 0;
+}
+
+static int
+create_database(
+        const char *host,
+        const char *port,
+        const char *root_user,
+        const char *root_password,
+        const char *database,
+        const char *other_user)
+{
+    char *cmd_s = NULL;
+    size_t cmd_z = 0;
+    FILE *cmd_f = open_memstream(&cmd_s, &cmd_z);
+    fprintf(cmd_f, "CREATE DATABASE %s", database);
+    if (other_user) {
+        fprintf(cmd_f, " OWNER %s", other_user);
+    }
+    fprintf(cmd_f, ";");
+    fclose(cmd_f); cmd_f = NULL;
+
+    int r = run_psql(host, port, root_user, root_password, NULL,
+                     "-c", cmd_s, NULL);
+    free(cmd_s); cmd_s = NULL;
+
+    return r;
+}
+
+/*
+ * Environment to handle:
+ *   PGHOST
+ *   PGPORT
+ *   PG_BASE_PREFIX
+ *   PG_ROOT_USER
+ *   PG_ROOT_PASSWORD
+ *   PG_USER_PREFIX
+ *   PG_PASSWORDS
+ *   EJUDGE_SUPER_RUN_SERIAL
+ *   EJUDGE_TEST_RANDOM_VALUE
+ */
 int
 main(int argc, char *argv[])
 {
-    char *s = strrchr(argv[0], '/');
+    const char *s = strrchr(argv[0], '/');
     if (s) {
         program_name = s + 1;
     } else {
         program_name = argv[0];
     }
+
+    if (argc != 2) {
+        die("wrong number of arguments");
+    }
+    const char *exec_file = argv[1];
+
+    const char *host = getenv("PGHOST");
+    if (!host || !*host) {
+        die("PGHOST env var is not set");
+    }
+    unsetenv("PGHOST");
+    const char *port = getenv("PGPORT");
+    if (!port || !*port) {
+        die("PGPORT env var is not set");
+    }
+    unsetenv("PGPORT");
+    s = getenv("EJUDGE_TEST_RANDOM_VALUE");
+    if (!s || !*s) {
+        die("EJUDGE_TEST_RANDOM_VALUE env var is not set");
+    }
+    unsetenv("EJUDGE_TEST_RANDOM_VALUE");
+    unsigned long long random_value = 0;
+    {
+        char *eptr = NULL;
+        errno = 0;
+        random_value = strtoull(s, &eptr, 16);
+        if (errno || *eptr || eptr == s) {
+            die("invalid EJUDGE_TEST_RANDOM_VALUE: %s", s);
+        }
+    }
+    const char *base_prefix = getenv("PG_BASE_PREFIX");
+    if (!base_prefix || !*base_prefix) {
+        die("PG_BASE_PREFIX env var is not set");
+    }
+    unsetenv("PG_BASE_PREFIX");
+    const char *root_user = getenv("PG_ROOT_USER");
+    if (!root_user || !*root_user) {
+        die("PG_ROOT_USER env var is not set");
+    }
+    unsetenv("PG_ROOT_USER");
+    const char *root_password = getenv("PG_ROOT_PASSWORD");
+    if (!root_password || !*root_password) {
+        die("PG_ROOT_PASSWORD env var is not set");
+    }
+    unsetenv("PG_ROOT_PASSWORD");
+    const char *user_prefix = getenv("PG_USER_PREFIX");
+    if (user_prefix && !*user_prefix) {
+        user_prefix = NULL;
+    }
+    unsetenv("PG_USER_PREFIX");
+
+    int run_serial = 0;
+    if (user_prefix) {
+        s = getenv("EJUDGE_SUPER_RUN_SERIAL");
+        if (s && *s) {
+            char *eptr = NULL;
+            errno = 0;
+            long v = strtol(s, &eptr, 10);
+            if (errno || *eptr || s == eptr || v < 0 || v > 100) {
+                die("invalid EJUDGE_SUPER_RUN_SERIAL: %s", s);
+            }
+            run_serial = (int) v;
+        }
+    }
+    unsetenv("EJUDGE_SUPER_RUN_SERIAL");
+
+    char *user_password = NULL;
+    if (user_prefix) {
+        s = getenv("PG_PASSWORDS");
+        if (!s || !*s) {
+            die("PG_PASSWORDS env var is not set");
+        }
+        const char *cur = s;
+        const char *next = strchr(cur, ':');
+        for (int i = 0; i < run_serial; ++i) {
+            if (!next) {
+                die("PG_PASSWORDS does not contain password for serial %d",
+                    run_serial);
+            }
+            cur = next + 1;
+            next = strchr(cur, ':');
+        }
+        user_password = strdup(cur);
+        if (next) {
+            user_password[next - cur] = 0;
+        }
+    }
+    unsetenv("PG_PASSWORDS");
+
+    unsigned r32 = (unsigned) ((random_value >> 32) ^ random_value);
+    char database[128];
+    int r = snprintf(database, sizeof(database), "%s%x", base_prefix, r32);
+    if (r >= (int) sizeof(database)) {
+        die("PG_BASE_PREFIX is too long");
+    }
+
+    char *user_name = NULL;
+    if (user_prefix) {
+        if (!run_serial) {
+            user_name = strdup(user_prefix);
+        } else {
+            __attribute__((unused)) int _;
+            _ = asprintf(&user_name, "%s%d", user_prefix, run_serial);
+        }
+    }
+
+    const char *input_file = getenv("INPUT_FILE");
+    if (input_file && !*input_file) {
+        input_file = NULL;
+    }
+    if (!input_file) {
+        input_file = "input";
+    }
+
+    r = create_database(host, port, root_user, root_password,
+                        database, user_name);
+    if (r != 0) {
+        die("database creation failed");
+    }
+
+    const char *exec_user = root_user;
+    const char *exec_password = root_password;
+    if (user_name) {
+        exec_user = user_name;
+        exec_password = user_password;
+    }
+
+    r = run_psql(host, port, exec_user, exec_password, database,
+                 "-f", input_file, "-f", exec_file, NULL);
+    if (r != 0) {
+        die("postgres execution failed");
+    }
+
+    return 0;
 }
