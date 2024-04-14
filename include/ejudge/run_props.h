792007edb9e81e0eb765d001081566c84fc69f32
Alexander Chernov <blackav@gmail.com>
2024-04-14 08:14:33 +0300

    add run_props.[ch]

---
+++ include/ejudge/run_props.h
@@ -0,0 +1,21 @@
+/* -*- mode: c; c-basic-offset: 4 -*- */
+#ifndef __RUN_PROPS_H__
+#define __RUN_PROPS_H__
+
+/* Copyright (C) 2024 Alexander Chernov <cher@ejudge.ru> */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+
+#endif /* __RUN_PROPS_H__ */
+
