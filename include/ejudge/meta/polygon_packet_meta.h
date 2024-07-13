4be7a1905b5ea2cb844796a45c9db563e2252f87
Alexander Chernov <blackav@gmail.com>
2024-07-13 10:36:08 +0300

    regenerated

--- include/ejudge/meta/polygon_packet_meta.h
+++ include/ejudge/meta/polygon_packet_meta.h
@@ -18,6 +18,7 @@ enum
   META_POLYGON_PACKET_enable_api,
   META_POLYGON_PACKET_verbose,
   META_POLYGON_PACKET_ignore_main_solution,
+  META_POLYGON_PACKET_enable_rss_limit,
   META_POLYGON_PACKET_polygon_url,
   META_POLYGON_PACKET_login,
   META_POLYGON_PACKET_password,
