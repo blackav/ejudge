6b0492829a90567bfa3cc5876bf031c7460530e8
Alexander Chernov <blackav@gmail.com>
2025-09-28 14:18:51 +0300

    refactor

--- include/ejudge/ejudge_problem_impl.h
+++ include/ejudge/ejudge_problem_impl.h
@@ -16,4 +16,85 @@
  * GNU General Public License for more details.
  */
 
- #endif /* __EJUDGE_PROBLEM_IMPL_H__ */
+#include <stdio.h>
+
+struct problem_state;
+struct ppxml_problem;
+
+struct problem_state *
+ejudge_problem_new_state();
+
+void
+ejudge_problem_set_ppxml(
+        struct problem_state *ps,
+        struct ppxml_problem *ppxml,
+        const unsigned char *problem_xml_file);
+
+int
+ejudge_problem_read_full_file(
+        FILE *log_f,
+        const unsigned char *path,
+        unsigned char **p_str,
+        size_t *p_size);
+
+int
+ejudge_problem_collect_dependencies(
+        FILE *log_f,
+        struct problem_state *ps,
+        int no_norm_check);
+
+int
+ejudge_problem_build(
+        FILE *log_f,
+        struct problem_state *ps,
+        char *args[]);
+
+int
+ejudge_problem_clean(
+        FILE *log_f,
+        struct problem_state *ps,
+        char *args[]);
+
+int
+ejudge_problem_print_makefile(
+        FILE *log_f,
+        struct problem_state *ps,
+        char *args[]);
+
+int
+ejudge_problem_print_topological(
+        FILE *log_f,
+        struct problem_state *ps,
+        char *args[]);
+
+int
+ejudge_problem_print_build_info(
+        FILE *log_f,
+        struct problem_state *ps,
+        char *args[]);
+
+int
+ejudge_problem_print_hash(
+        FILE *log_f,
+        struct problem_state *ps,
+        char *args[]);
+
+int
+ejudge_problem_print_source_files(
+        FILE *log_f,
+        struct problem_state *ps,
+        char *args[]);
+
+int
+ejudge_problem_normalize_tests(
+        FILE *log_f,
+        struct problem_state *ps,
+        char *args[]);
+
+int
+ejudge_problem_create_zip_tool(
+        FILE *log_f,
+        struct problem_state *ps,
+        char *args[]);
+
+#endif /* __EJUDGE_PROBLEM_IMPL_H__ */
