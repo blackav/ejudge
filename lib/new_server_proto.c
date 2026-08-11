593c4f851ad41bb39699633335a917c86a6b491e
Alexander Chernov <blackav@gmail.com>
2026-08-12 02:24:05 +0300

    add ERR_NO_AFFECTED_ROWS

--- lib/new_server_proto.c
+++ lib/new_server_proto.c
@@ -197,6 +197,7 @@ static const unsigned char * const ns_error_messages[NEW_SRV_ERR_LAST]=
   [NEW_SRV_ERR_USER_BANNED] = __("User is banned"),
   [NEW_SRV_ERR_USER_LOCKED] = __("User is locked"),
   [NEW_SRV_ERR_NO_CONTESTS] = __("No contests"),
+  [NEW_SRV_ERR_NO_AFFECTED_ROWS] = __("No affected rows"),
 };
 
 static const unsigned char * const ns_error_titles[NEW_SRV_ERR_LAST]=
@@ -366,6 +367,7 @@ static const unsigned char * const ns_error_titles[NEW_SRV_ERR_LAST]=
   [NEW_SRV_ERR_USER_BANNED] = __("User is banned"),
   [NEW_SRV_ERR_USER_LOCKED] = __("User is locked"),
   [NEW_SRV_ERR_NO_CONTESTS] = __("No contests"),
+  [NEW_SRV_ERR_NO_AFFECTED_ROWS] = __("No affected rows"),
 };
 
 static const unsigned char * const ns_error_symbols[NEW_SRV_ERR_LAST]=
@@ -534,6 +536,7 @@ static const unsigned char * const ns_error_symbols[NEW_SRV_ERR_LAST]=
   [NEW_SRV_ERR_USER_BANNED] = "ERR_USER_BANNED",
   [NEW_SRV_ERR_USER_LOCKED] = "ERR_USER_LOCKED",
   [NEW_SRV_ERR_NO_CONTESTS] = "ERR_NO_CONTESTS",
+  [NEW_SRV_ERR_NO_AFFECTED_ROWS] = "ERR_NO_AFFECTED_ROWS",
 };
 
 const unsigned char *
