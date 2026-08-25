ae92ddb37425300e84aeed3dceacf40920828031
Alexander Chernov <blackav@gmail.com>
2026-08-25 12:13:35 +0300

    add run-review-incomplete error

--- lib/new_server_proto.c
+++ lib/new_server_proto.c
@@ -200,6 +200,7 @@ static const unsigned char * const ns_error_messages[NEW_SRV_ERR_LAST]=
   [NEW_SRV_ERR_NO_AFFECTED_ROWS] = __("No affected rows"),
   [NEW_SRV_ERR_RUN_REVIEW_QUOTA_EXCEEDED] = __("Review quota exceeded"),
   [NEW_SRV_ERR_RUN_ALREADY_REVIEWED] = __("Run already reviewed"),
+  [NEW_SRV_ERR_RUN_REVIEW_INCOMPLETE] = __("Previous run review not finished"),
 };
 
 static const unsigned char * const ns_error_titles[NEW_SRV_ERR_LAST]=
@@ -372,6 +373,7 @@ static const unsigned char * const ns_error_titles[NEW_SRV_ERR_LAST]=
   [NEW_SRV_ERR_NO_AFFECTED_ROWS] = __("No affected rows"),
   [NEW_SRV_ERR_RUN_REVIEW_QUOTA_EXCEEDED] = __("Review quota exceeded"),
   [NEW_SRV_ERR_RUN_ALREADY_REVIEWED] = __("Run already reviewed"),
+  [NEW_SRV_ERR_RUN_REVIEW_INCOMPLETE] = __("Previous run review not finished"),
 };
 
 static const unsigned char * const ns_error_symbols[NEW_SRV_ERR_LAST]=
@@ -543,6 +545,7 @@ static const unsigned char * const ns_error_symbols[NEW_SRV_ERR_LAST]=
   [NEW_SRV_ERR_NO_AFFECTED_ROWS] = "ERR_NO_AFFECTED_ROWS",
   [NEW_SRV_ERR_RUN_REVIEW_QUOTA_EXCEEDED] = "ERR_REVIEW_QUOTA_EXCEEDED",
   [NEW_SRV_ERR_RUN_ALREADY_REVIEWED] = "ERR_ALREADY_REVIEWED",
+  [NEW_SRV_ERR_RUN_REVIEW_INCOMPLETE] = "ERR_RUN_REVIEW_INCOMPLETE",
 };
 
 const unsigned char *
